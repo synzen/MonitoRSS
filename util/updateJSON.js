15af8a8ee5b5579c5738de8d96732cab686e8ace
debenco <michael994664@gmail.com>
2017-01-10 16:10:02 -0500

    async filewrite

--- util/updateJSON.js
+++ util/updateJSON.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 
 
 module.exports = function (realFile, inFile) {
-
-  fs.writeFileSync(realFile, JSON.stringify(inFile, null, 2));
+  fs.writeFile(realFile, JSON.stringify(inFile, null, 2), function (err) {
+    if (err) return console.log(err);
+  });
+  //fs.writeFileSync(realFile, JSON.stringify(inFile, null, 2));
 
 }
