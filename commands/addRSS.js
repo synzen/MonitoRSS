18084d7e095d4beee2f60a26c200561292c6b695
debenco <michael994664@gmail.com>
2017-01-23 13:35:11 -0500

    added request loop for adding rss on req errors

--- commands/addRSS.js
+++ commands/addRSS.js
@@ -28,40 +28,44 @@ module.exports = function (bot, message) {
   if (!rssLink.startsWith("http")) return message.channel.sendMessage("Unable to add feed. Make sure it is a link, and there are no odd characters before your feed link.")
   message.channel.startTyping()
 
-  request(rssLink, function (error, response, body) {
+  var attempts = 0;
 
-    if (!error && response.statusCode == 200){
-
-      for (var x in rssList) {
-        if ( rssList[x].link == rssLink && isCurrentChannel(rssList[x].channel) ) {
-          message.channel.stopTyping();
-          return message.channel.sendMessage("This feed already exists for this channel.");
+  (function checkValidLink() {
+    request(rssLink, function (error, response, body) {
+      if (!error && response.statusCode == 200){
+        for (var x in rssList) {
+          if ( rssList[x].link == rssLink && isCurrentChannel(rssList[x].channel) ) {
+            message.channel.stopTyping();
+            return message.channel.sendMessage("This feed already exists for this channel.");
+          }
         }
-      }
 
-      if (rssConfig.maxFeeds == 0 || rssList.length < rssConfig.maxFeeds) {
-        var con = sqlConnect(init);
-        function init() {
-          initializeRSS(con, rssLink, message.channel, function() {
-            sqlCmds.end(con, function(err) {
-              if (err) throw err;
+        if (rssConfig.maxFeeds == 0 || rssList.length < rssConfig.maxFeeds) {
+          if (attempts > 0) console.log(`sucessful connection on attempt #${attempts+1}`)
+          var con = sqlConnect(init);
+          function init() {
+            initializeRSS(con, rssLink, message.channel, function() {
+              sqlCmds.end(con, function(err) {
+                if (err) throw err;
+              });
             });
-          });
+          }
+        }
+        else {
+          message.channel.stopTyping();
+          console.log(`RSS Info: (${message.guild.id}, ${message.guild.name}) => Unable to add feed ${rssLink} due to limit of ${rssConfig.maxFeeds} feeds.`)
+          return message.channel.sendMessage(`Unable to add feed. The server has reached the limit of: \`${rssConfig.maxFeeds}\` feeds.`)
         }
       }
+
       else {
         message.channel.stopTyping();
-        console.log(`RSS Info: (${message.guild.id}, ${message.guild.name}) => Unable to add feed ${rssLink} due to limit of ${rssConfig.maxFeeds} feeds.`)
-        return message.channel.sendMessage(`Unable to add feed. The server has reached the limit of: \`${rssConfig.maxFeeds}\` feeds.`)
+        console.log(`RSS Info: (${message.guild.id}, ${message.guild.name}) => Unable to add feed ${rssLink} due to invalid response.`)
+        if (attempts < 2) setTimeout(checkValidLink, 500);
+        else return message.channel.sendMessage(`Unable to add feed, could not connect to <${rssLink}>.`);
       }
-    }
+    });
 
-    else {
-      message.channel.stopTyping();
-      console.log(`RSS Info: (${message.guild.id}, ${message.guild.name}) => Unable to add feed ${rssLink} due to invalid response.`)
-      return message.channel.sendMessage(`Unable to add feed, could not connect to <${rssLink}>.`);
-    }
-
-  });
+  })()
 
 }
