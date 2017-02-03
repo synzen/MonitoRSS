453b7c969d2da7b5f9b533fc2164a20611829d21
synzen <michael994664@gmail.com>
2017-02-02 22:55:07 -0500

    added missing file

---
+++ cmdServer.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const eventHandler = (evnt) => require(`./events/${evnt}.js`)
+const config = require('./config.json')
+
+if (config.logDates) require('./util/logDates.js')();
+
+
+bot.on('ready', function() {
+  console.log("I am online.")
+})
+
+bot.on('message', function (message) {
+  eventHandler('message')(bot, message)
+})
+
+bot.on('guildCreate', function (guild) {
+  eventHandler('guildCreate')(bot, guild)
+})
+
+bot.on('guildDelete', function (guild) {
+  eventHandler('guildDelete')(bot, guild)
+})
+
+bot.on('channelDelete', function (channel) {
+  eventHandler('channelDelete')(channel)
+})
+
+bot.on('roleDelete', function (role) {
+  eventHandler('roleDelete')(bot, role)
+})
+
+bot.login(config.token)
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log('Unhandled Rejection at: Promise', promise, 'reason:', err);
+})
