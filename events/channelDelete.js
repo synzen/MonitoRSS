d76f7d94ac061850fd5a936d6d44e4b32e23d922
synzen <michael994664@gmail.com>
2017-02-09 19:22:07 -0500

    fixed unnecessary channel deletion logging

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -3,10 +3,16 @@ const removeRSS = require('../commands/rssRemove.js')
 module.exports = function (channel) {
   var rssList = require(`../sources/${channel.guild.id}.json`).sources
 
+  var indexList = []
+  for (let rssIndex in rssList) {
+    if (rssList[rssIndex].channel === channel.id) indexList.push(rssIndex);
+  }
+  if (indexList.length === 0) return;
+
   console.log(`Guild Info: (${channel.guild.id}, ${channel.guild.name}) => Channel (${channel.id}, ${channel.name}) deleted.`)
 
-  for (let rssIndex in rssList) {
-    if (rssList[rssIndex].channel == channel.id) removeRSS(channel, rssIndex);
+  for (let index in indexList) {
+    removeRSS(channel, index);
   }
 
 }
