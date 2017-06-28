0919e976ff93fb32d6f588a661d27729f4956bbf
synzen <michael994664@gmail.com>
2017-06-28 09:29:27 -0400

    Greatly improved SQL performance

--- rss/sql/commands.js
+++ rss/sql/commands.js
@@ -27,13 +27,13 @@ exports.cleanTable = function (con, table, articleArray) {
 }
 
 exports.selectId = function (con, table, articleId, callback) {
-  if (sqlType === 'mysql') return con.query(`select ID from \`${table}\` where ID = ?`, [articleId], callback)
-  else return con.all(`select * from "${table}" where ID = ?`, articleId, callback)
+  if (sqlType === 'mysql') return con.query(`select ID from \`${table}\` where ID = ? limit 1`, [articleId], callback)
+  else return con.all(`select ID from "${table}" where ID = ? limit 1`, articleId, callback)
 }
 
 exports.selectTitle = function (con, table, articleTitle, callback) {
-  if (sqlType === 'mysql') return con.query(`select TITLE from \`${table}\` where TITLE = ?`, [articleTitle], callback)
-  else return con.all(`select * from "${table}" where TITLE = ?`, articleTitle, callback)
+  if (sqlType === 'mysql') return con.query(`select TITLE from \`${table}\` where TITLE = ? limit 1`, [articleTitle], callback)
+  else return con.all(`select TITLE from "${table}" where TITLE = ? limit 1`, articleTitle, callback)
 }
 
 exports.insert = function (con, table, articleInfo, callback) {
