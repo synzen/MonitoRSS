0c159f6ba5d0346dc6d05ce91bdf3dab10bc9e74
synzen <michael994664@gmail.com>
2017-07-22 12:40:03 -0400

    Updated help text

--- commands/rsshelp.js
+++ commands/rsshelp.js
@@ -6,6 +6,6 @@ module.exports = function (bot, message, command) {
   for (var cmd in commands) {
     if (commands[cmd].description) msg += `\`${config.botSettings.prefix}${cmd}\`\n${commands[cmd].description}\n\n`
   }
-  message.channel.send(msg + 'Note that this is an **experimental bot**. Should you need it, some support can be found at a server I made at https://discord.gg/WPWRyxK')
+  message.channel.send(msg + 'Support can be found at https://discord.gg/WPWRyxK')
   .catch(err => console.log(`Commands Warning: (${message.guild.id}, ${message.guild.name}) => Could not send help menu. (${err})`))
 }
