c05251c6bd076162c0ea19a76fc07fa6a2419d0f
synzen <michael994664@gmail.com>
2017-08-10 08:41:27 -0400

    Updated filters. NOT filters and non-NOT filters are now ANDED. Please redo filters as necessary to test if you are using NOT filters.

--- rss/translator/filters.js
+++ rss/translator/filters.js
@@ -5,17 +5,20 @@ function escapeRegExp (str) {
 
 function findFilterWords (filterType, content, isTestMessage) {
   // filterType is array of title, description, summary, or author
-  if (!content) return false
-  if (isTestMessage) var matches = []
+  if (!content) return {passed: false, inverted: false}
+  if (isTestMessage) {
+    var matches = []
+    var invertedMatches = []
+  }
+  const results = []
 
-  let invertedFilter = false // Inverted results = NOT filters found
   if (typeof filterType === 'object' && filterType.length && filterType.length !== 0) {
     // For title, descriptions, summary, and author
     if (typeof content === 'string') {
       content = content.toLowerCase()
       for (var word in filterType) {
+        let invertedFilter = false // Inverted results = NOT filters found
         let searchTerm = filterType[word].toLowerCase()
-
         if (searchTerm.startsWith('!')) {
           invertedFilter = true
           searchTerm = searchTerm.slice(1, searchTerm.length)
@@ -24,23 +27,24 @@ function findFilterWords (filterType, content, isTestMessage) {
         if (searchTerm.startsWith('~')) { // Broad filters, for phrases/words found anywhere
           searchTerm = searchTerm.slice(1, searchTerm.length)
           if (content.includes(searchTerm)) {
-            if (isTestMessage) matches.push(filterType[word])
-            else if (!invertedFilter) return true
-            else return false
-          }
+            if (isTestMessage && !invertedFilter) matches.push(filterType[word])
+            else if (isTestMessage && invertedFilter) invertedMatches.push(filterType[word])
+            results.push({passed: true, inverted: invertedFilter})
+          } else results.push({passed: false, inverted: invertedFilter})
         } else { // Specific filters, for phrases/words with spaces around them
           searchTerm = (searchTerm.startsWith('\\~')) ? searchTerm.slice(1, searchTerm.length) : searchTerm.startsWith('\!') ? searchTerm.slice(1, searchTerm.length) : searchTerm // A \~ or \! will just read as a ~ or !
           let expression = new RegExp(`(\\s|^)${escapeRegExp(searchTerm)}(\\s|$)`, 'gi')
           if (content.search(expression) !== -1) {
-            if (isTestMessage) matches.push(filterType[word])
-            else if (!invertedFilter) return true
-            else return false
-          }
+            if (isTestMessage && !invertedFilter) matches.push(filterType[word])
+            else if (isTestMessage && invertedFilter) invertedMatches.push(filterType[word])
+            results.push({passed: true, inverted: invertedFilter})
+          } else results.push({passed: false, inverted: invertedFilter})
         }
       }
     } else if (typeof content === 'object') { // For tags
       for (var item in content) {
         for (var w in filterType) {
+          let invertedFilter = false // Inverted results = NOT filters found
           let searchTerm = filterType[w]
 
           if (searchTerm.startsWith('!')) {
@@ -51,31 +55,58 @@ function findFilterWords (filterType, content, isTestMessage) {
           if (searchTerm.startsWith('~')) { // Broad filters, for phrases/words found anywhere
             searchTerm = searchTerm.slice(1, searchTerm.length)
             if (content[item].includes(searchTerm)) {
-              if (isTestMessage) matches.push(filterType[w])
-              else if (!invertedFilter) return true
-              else return false
-            }
+              if (isTestMessage && !invertedFilter) matches.push(filterType[w])
+              else if (isTestMessage && invertedFilter) invertedMatches.push(filterType[word])
+              results.push({passed: true, inverted: invertedFilter})
+            } else results.push({passed: false, inverted: invertedFilter})
           } else { // Specific filters, for phrases/words with spaces around them
             searchTerm = (searchTerm.startsWith('\\~')) ? searchTerm.slice(1, searchTerm.length) : searchTerm.startsWith('\!') ? searchTerm.slice(1, searchTerm.length) : searchTerm // A \~ or \! will just read as a ~ or !
             let expression = new RegExp(`(\\s|^)${escapeRegExp(searchTerm)}(\\s|$)`, 'gi')
             if (content[item].search(expression) !== -1) {
-              if (isTestMessage) matches.push(filterType[w])
-              else if (!invertedFilter) return true
-              else return false
-            }
+              if (isTestMessage && !invertedFilter) matches.push(filterType[w])
+              else if (isTestMessage && invertedFilter) invertedMatches.push(filterType[word])
+              results.push({passed: true, inverted: invertedFilter})
+            } else results.push({passed: false, inverted: invertedFilter})
           }
         }
       }
     }
-  } else return false
+  }
 
   if (isTestMessage) {
     return {
-      inverted: invertedFilter,
-      matches: matches.length > 0 ? matches : null
+      resultsList: results,
+      matches: matches.length > 0 ? matches : null,
+      invertedMatches: invertedMatches.length > 0 ? invertedMatches : null
     }
-  } else if (invertedFilter) return true
-  else return false
+  }
+  else return {resultsList: results}
+}
+
+function FilterResults() {
+  this.matches = {}
+  this.invertedMatches = {}
+
+  this.add = function(type, matches, inverted) {
+    if (inverted) this.invertedMatches[type] = matches
+    else this.matches[type] = matches
+  }
+
+  this.listMatches = function(inverted) {
+    const matchList = inverted ? this.invertedMatches : this.matches
+    let str = ''
+    for (var type in matchList) {
+      let list = ''
+      const typeMatches = matchList[type]
+      for (var x in typeMatches) {
+        list += ` ${typeMatches[x]}`
+        if (parseInt(x, 10) !== typeMatches.length - 1) list += ' |'
+      }
+      str += `\n${type}:${list}`
+    }
+    return str
+  }
+
 }
 
 module.exports = function (rssList, rssName, article, isTestMessage) {
@@ -84,7 +115,7 @@ module.exports = function (rssList, rssName, article, isTestMessage) {
       user: rssList[rssName].filters.Title,
       ref: article.title
     },
-    'Descriptipn': {
+    'Description': {
       user: rssList[rssName].filters.Description,
       ref: article.rawDescrip
     },
@@ -102,28 +133,43 @@ module.exports = function (rssList, rssName, article, isTestMessage) {
     }
   }
 
-  let passedFilters = false
+  // Inverted and regular filters are ultimately calculated together with AND
+  const passed = {
+    invertedFilters: true, // If an inverted filter is found, invertedFilters is set to false for the AND operation
+    regularFilters: true // If a regular filter is not found, regularFilters is set to true
+  }
+  let regularFiltersHasTrueInstance = false // Used for OR operation between regularFilters
+
   let filterMatches = ''
+  let invertedFilterMatches = ''
+  const filterResults = new FilterResults()
   for (var type in filterTypes) {
-    let results = findFilterWords(filterTypes[type].user, filterTypes[type].ref, isTestMessage)
-    if (isTestMessage) {
-      if (results.matches) {
-        var list = ''
-        for (var x in results.matches) {
-          list += ` ${results.matches[x]}`
-          if (parseInt(x, 10) !== results.matches.length - 1) list += ' |'
-        }
-        filterMatches += `\n${type}:${list}`
-        if (!results.inverted) passedFilters = true
+    const allResults = findFilterWords(filterTypes[type].user, filterTypes[type].ref, isTestMessage)
+    // Get match words for test messages
+    if (isTestMessage && allResults.matches) filterResults.add(type, allResults.matches, false)
+    if (isTestMessage && allResults.invertedMatches) filterResults.add(type, allResults.invertedMatches, true)
+
+    // Decide whether it passes for each filter
+    for (var i in allResults.resultsList) {
+      const results = allResults.resultsList[i]
+      if (results.length === 0) continue
+
+      // AND operation saved for future reference
+      // if (results.inverted && results.passed === true) passed.invertedFilters = false
+      // else if (!results.inverted && results.passed === false) passed.regularFilters = false
+
+      // OR operation
+      if (results.inverted && results.passed === true) passed.invertedFilters = false // Maintain the first line from the AND operation between NOT nad non-NOT
+      else if (!results.inverted) {
+        if (results.passed === false) passed.regularFilters = false // Only account for false since the default is true
+        else if (results.passed === true) regularFiltersHasTrueInstance = true
       }
-      if (results.inverted && !results.matches) passedFilters = true
-    } else if (results === true) passedFilters = true
+    }
   }
 
-  if (isTestMessage) {
-    return {
-      passedFilters: passedFilters,
-      filterMatches: filterMatches
-    }
-  } else return passedFilters
+  if (regularFiltersHasTrueInstance) passed.regularFilters = true
+
+  filterResults.passedFilters = passed.invertedFilters && passed.regularFilters
+  if (isTestMessage) return filterResults
+  else return passed.invertedFilters && passed.regularFilters
 }
