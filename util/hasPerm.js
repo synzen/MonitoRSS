cb9c8fac971551166f61b454b092038e1a591b17
synzen <michael994664@gmail.com>
2018-02-27 21:07:08 -0500

    Added error message for missing embed link permission

--- util/hasPerm.js
+++ util/hasPerm.js
@@ -1,4 +1,5 @@
-const validPerms = [
+const log = require('./logger.js')
+const PERMISSIONS = [
   'CREATE_INSTANT_INVITE',
   'KICK_MEMBERS',
   'BAN_MEMBERS',
@@ -29,22 +30,25 @@ const validPerms = [
 ]
 
 exports.bot = (bot, message, permission) => {
-  if (!permission || !validPerms.includes(permission)) return true
+  if (!permission || !PERMISSIONS.includes(permission)) return true
   const channel = message.channel
   const guild = bot.guilds.get(channel.guild.id)
   const guildBot = guild.members.get(bot.user.id)
   const hasPerm = guildBot.permissionsIn(channel).has(permission)
 
   if (permission === 'MANAGE_ROLES_OR_PERMISSIONS' && !hasPerm) {
-    console.log(`Commands Warning: (${channel.guild.id}, ${channel.guild.name}) => Self subscription disabled due to missing permission.`)
-    message.channel.send('Function disabled due to missing `Manage Roles` permission.').then(m => m.delete(3000))
+    log.command.warning(`Self subscription disabled due to missing permission`, message.guild)
+    message.channel.send('Function disabled due to missing `Manage Roles` permission.').then(m => m.delete(3000)).catch(err => log.command.warning(`hasperm 1`, message.guild, err))
+  } else if (permission === 'EMBED_LINKS' && !hasPerm) {
+    log.command.warning(`Menu commands disabled due to missing permission`, message.guild)
+    message.channel.send('Menu commands disabled due to missing `Embed Links` permission.').then(m => m.delete(3000)).catch(err => log.command.warning(`hasperm 2`, message.guild, err))
   }
 
   return hasPerm
 }
 
 exports.user = (message, permission) => {
-  if (!permission || !validPerms.includes(permission)) return true
+  if (!permission || !PERMISSIONS.includes(permission)) return true
 
   const serverPerm = message.member.hasPermission(permission)
   const channelPerm = message.member.permissionsIn(message.channel).has(permission)
