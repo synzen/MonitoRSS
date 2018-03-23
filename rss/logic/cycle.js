a752c66274a387e310e1a2632d920188b1ca2f80
synzen <michael994664@gmail.com>
2018-03-22 23:30:04 -0400

    Optimized and fixed critical logic errors

--- rss/logic/cycle.js
+++ rss/logic/cycle.js
@@ -23,119 +23,76 @@ function getArticleId (articleList, article) {
 module.exports = function (data, callback) {
   const { rssList, articleList, debugFeeds, link, shardId } = data
   let sourcesCompleted = 0
+  const totalArticles = articleList.length
+  const dbIds = []
+  const dbTitles = []
   const toInsert = []
   const Feed = FeedModel(link, shardId)
 
   dbCmds.findAll(Feed, (err, docs) => {
     if (err) return callback(new Error(`Database error: Unable to findAll articles for link ${link}`, err.message || err), { status: 'failed', link: link, rssList: rssList })
-    const oldIds = []
 
-    for (var d = 0; d < docs.length; ++d) oldIds.push(docs[d].id)
+    for (var d = 0; d < docs.length; ++d) {
+      dbIds.push(docs[d].id)
+      dbTitles.push(docs[d].title)
+    }
 
     for (var a = 0; a < articleList.length; ++a) {
       const article = articleList[a]
       article._id = getArticleId(articleList, article)
-      if (!oldIds.includes(article._id)) toInsert.push(article)
+      if (!dbIds.includes(article._id)) toInsert.push(article)
     }
 
     dbCmds.bulkInsert(Feed, toInsert, (err, res) => {
       if (err) return callback(new Error(`Database Error: Unable to bulk insert articles for link ${link}`, err.message || err), { status: 'failed', link: link, rssList: rssList })
-      if (oldIds.length > 0) for (var rssName in rssList) processSource(rssName, docs)
+      if (dbIds.length > 0) for (var rssName in rssList) processSource(rssName, docs)
       else callback(null, { status: 'success', link: link })
     })
   })
 
   function processSource (rssName, docs) {
-    const channelId = rssList[rssName].channel
+    const source = rssList[rssName]
+    const channelId = source.channel
 
     let processedArticles = 0
-    let totalArticles = 0
-    const olderArticles = []
-    const newerArticles = []
     if (debugFeeds.includes(rssName)) log.debug.info(`${rssName}: Processing collection. Total article list length: ${articleList.length}`)
 
-    const feedLength = articleList.length - 1
     const cycleMaxAge = config.feeds.cycleMaxAge
+
     const globalDateCheck = config.feeds.checkDates != null ? config.feeds.checkDates : defaultConfigs.feeds.checkDates.default
+    const localDateCheck = source.checkDates
+    const checkDate = typeof localDateCheck !== 'boolean' ? globalDateCheck : localDateCheck
 
-    for (var x = feedLength; x >= 0; x--) {
-      const article = articleList[x]
-
-      if (article.pubdate && article.pubdate > moment().subtract(cycleMaxAge, 'days')) newerArticles.push(article)
-      else {
-        let checkDate = globalDateCheck
-        const localDateSetting = rssList[rssName].checkDates
-        checkDate = typeof localDateSetting !== 'boolean' ? checkDate : localDateSetting
-
-        if (checkDate) {
-          olderArticles.push(article) // Mark as old if date checking is enabled
-          article._old = true
-        } else newerArticles.push(article) // Otherwise mark as new
-      }
-
-      ++totalArticles
-    }
-
-    let checkTitle = config.feeds.checkTitles != null ? config.feeds.checkTitles : defaultConfigs.feeds.checkTitles.default
-    const localTitleSetting = rssList[rssName].checkTitles
-    checkTitle = typeof localTitleSetting !== 'boolean' ? checkTitle : localTitleSetting
-    const allIds = []
-    const allTitles = []
-    const newerIds = []
-    for (var o = 0; o < olderArticles.length; ++o) {
-      const article = olderArticles[o]
-      allIds.push(article._id)
-      if (checkTitle) allTitles.push(article.title)
-    }
-    for (var n = 0; n < newerArticles.length; ++n) {
-      const article = newerArticles[n]
-      allIds.push(article._id)
-      newerIds.push(article._id)
-      if (checkTitle) allTitles.push(article.title)
-    }
-    const foundIds = []
-    const foundTitles = []
-
-    for (var d = 0; d < docs.length; ++d) {
-      const item = docs[d]
-      foundIds.push(item.id)
-      foundTitles.push(item.title)
-    }
+    const globalTitleCheck = config.feeds.checkTitles != null ? config.feeds.checkTitles : defaultConfigs.feeds.checkTitles.default
+    const localTitleCheck = source.checkTitles
+    const checkTitle = typeof globalTitleCheck !== 'boolean' ? globalTitleCheck : localTitleCheck
 
     for (var a = 0; a < articleList.length; ++a) {
       const article = articleList[a]
-      if (foundIds.length === 0 && articleList.length !== 1) { // Only skip if the articleList length is !== 1, otherwise a feed with only 1 article to send since it may have been the first item added
-        if (debugFeeds.includes(rssName)) log.debug.info(`${rssName}: Not sending article (ID: ${article._id}, TITLE: ${article.title}) due to empty collection. Initializing.`)
-        seenArticle(false, article, true) // If the collection was uninitialized, initialize all articles without sending
-      } else if (foundIds.includes(article._id)) {
+      if (dbIds.length === 0 && articleList.length !== 1) { // Only skip if the articleList length is !== 1, otherwise a feed with only 1 article to send since it may have been the first item added
+        if (debugFeeds.includes(rssName)) log.debug.info(`${rssName}: Not sending article (ID: ${article._id}, TITLE: ${article.title}) due to empty collection.`)
+        seenArticle(true)
+      } else if (dbIds.includes(article._id)) {
         if (debugFeeds.includes(rssName)) log.debug.info(`${rssName}: Not sending article (ID: ${article._id}, TITLE: ${article.title}), ID was matched.`)
-        seenArticle(true, article)
-      } else if (foundTitles.includes(article.title)) {
-        if (debugFeeds.includes(rssName)) log.debug.warning(`${rssName}: Not sending article (ID: ${article._id}, TITLE: ${article.title}), Title was matched but not ID. Inserting into collection.`)
-        seenArticle(false, article, true) // Don't send to Discord but still insert into collection because it has a unique ID
-      } else if (article._old) {
-        if (debugFeeds.includes(rssName)) log.debug.warning(`${rssName}: Not sending article (ID: ${article._id}, TITLE: ${article.title}), due to date check. Inserting into collection.`)
-        seenArticle(false, article, true)
+        seenArticle(true)
+      } else if (checkTitle && dbTitles.includes(article.title)) {
+        if (debugFeeds.includes(rssName)) log.debug.warning(`${rssName}: Not sending article (ID: ${article._id}, TITLE: ${article.title}), Title was matched but not ID.`)
+        seenArticle(true)
+      } else if (checkDate && (!article.pubdate || article.pubdate.toString() === 'Invalid Date' || article.pubdate <= moment().subtract(cycleMaxAge, 'days'))) {
+        if (debugFeeds.includes(rssName)) log.debug.warning(`${rssName}: Not sending article (ID: ${article._id}, TITLE: ${article.title}), due to date check.`)
+        seenArticle(true)
       } else {
         if (debugFeeds.includes(rssName)) log.debug.warning(`${rssName}: Sending article (ID: ${article._id}, TITLE: ${article.title}) to sendToDiscord.`)
         seenArticle(false, article)
       }
     }
-    // })
 
-    function seenArticle (seen, article, doNotSend) {
-      if (seen) return incrementProgress() // Stops here if it already exists in table, AKA "seen"
-
-      if (newerIds.includes(article._id) && !doNotSend) {
-        article.rssName = rssName
-        article.discordChannelId = channelId
-        callback(null, { status: 'article', article: article })
-      }
-      incrementProgress()
-    }
-
-    function incrementProgress () {
-      if (++processedArticles === totalArticles) finishSource()
+    function seenArticle (seen, article) {
+      if (seen) return ++processedArticles === totalArticles ? finishSource() : null // Stops here if it already exists in table, AKA "seen"
+      article.rssName = rssName
+      article.discordChannelId = channelId
+      callback(null, { status: 'article', article: article })
+      return ++processedArticles === totalArticles ? finishSource() : null
     }
   }
 
--- rss/logic/initialization.js
+++ rss/logic/initialization.js
@@ -21,24 +21,29 @@ module.exports = function (data, callback) {
   const { rssList, articleList, link, shardId } = data
   const totalArticles = articleList.length
   let sourcesCompleted = 0
+  const dbIds = []
+  const dbTitles = []
   const toInsert = []
   const Feed = FeedModel(link, shardId)
 
   dbCmds.findAll(Feed, (err, docs) => {
-    if (err) throw err // return callback(err)
-    const oldIds = []
+    if (err) throw err
+    const dbIds = []
 
-    for (var d = 0; d < docs.length; ++d) oldIds.push(docs[d].id)
+    for (var d = 0; d < docs.length; ++d) {
+      dbIds.push(docs[d].id)
+      dbTitles.push(docs[d].title)
+    }
 
     for (var a = 0; a < articleList.length; ++a) {
       const article = articleList[a]
       article._id = getArticleId(articleList, article)
-      if (!oldIds.includes(article._id)) toInsert.push(article)
+      if (!dbIds.includes(article._id)) toInsert.push(article)
     }
 
     dbCmds.bulkInsert(Feed, toInsert, (err, res) => {
-      if (err) throw err// return callback(err)
-      if (oldIds.length > 0) for (var rssName in rssList) processSource(rssName, docs)
+      if (err) throw err
+      if (dbIds.length > 0) for (var rssName in rssList) processSource(rssName, docs)
       else callback(null, { status: 'success', link: link })
     })
   })
@@ -46,76 +51,34 @@ module.exports = function (data, callback) {
   function processSource (rssName, docs) {
     const source = rssList[rssName]
     const channelId = source.channel
-    const olderArticles = []
-    const newerArticles = []
     let processedArticles = 0
 
-    const feedLength = articleList.length - 1
     const defaultMaxAge = config.feeds.defaultMaxAge && !isNaN(parseInt(config.feeds.defaultMaxAge, 10)) ? parseInt(config.feeds.defaultMaxAge, 10) : 1
+    const cutoffDay = source.maxAge ? moment().subtract(source.maxAge, 'days') : moment().subtract(defaultMaxAge, 'days')
+
     const globalDateCheck = config.feeds.checkDates != null ? config.feeds.checkDates : defaultConfigs.feeds.checkDates.default
+    const localDateCheck = source.checkDates
+    const checkDate = typeof localDateCheck !== 'boolean' ? globalDateCheck : localDateCheck
 
-    for (var x = feedLength; x >= 0; x--) { // Get feeds starting from oldest, ending with newest.
-      const article = articleList[x]
-      const cutoffDay = source.maxAge ? moment().subtract(source.maxAge, 'days') : moment().subtract(defaultMaxAge, 'days')
+    const globalTitleCheck = config.feeds.checkTitles != null ? config.feeds.checkTitles : defaultConfigs.feeds.checkTitles.default
+    const localTitleCheck = source.checkTitles
+    const checkTitle = typeof localTitleCheck !== 'boolean' ? globalTitleCheck : localTitleCheck
 
-      if (article.pubdate >= cutoffDay) newerArticles.push(article)
-      else if (article.pubdate < cutoffDay) {
-        olderArticles.push(article)
-      } else if (article.pubdate.toString() === 'Invalid Date') {
-        let checkDate = globalDateCheck
-        const feedSet = source.checkDates
-        checkDate = typeof feedSet !== 'boolean' ? checkDate : feedSet
-
-        if (checkDate) olderArticles.push(article) // Mark as old if date checking is enabled
-        else newerArticles.push(article) // Otherwise mark it new
-      } else olderArticles.push(article) // for all other cases
-    }
-
-    let checkTitle = config.feeds.checkTitles != null ? config.feeds.checkTitles : defaultConfigs.feeds.checkTitles.default
-    const feedSet = source.checkTitles
-    checkTitle = typeof feedSet !== 'boolean' ? checkTitle : feedSet
-    const allIds = []
-    const allTitles = []
-    const newerIds = []
-    for (var o = 0; o < olderArticles.length; ++o) {
-      const article = olderArticles[o]
-      allIds.push(article._id)
-      if (checkTitle) allTitles.push(article.title)
-    }
-    for (var n = 0; n < newerArticles.length; ++n) {
-      const article = newerArticles[n]
-      allIds.push(article._id)
-      newerIds.push(article._id)
-      if (checkTitle) allTitles.push(article.title)
-    }
-    const foundIds = []
-    const foundTitles = []
-
-    for (var d = 0; d < docs.length; ++d) {
-      const item = docs[d]
-      foundIds.push(item.id)
-      foundTitles.push(item.title)
-    }
     for (var a = 0; a < articleList.length; ++a) {
       const article = articleList[a]
-      if (foundIds.length === 0) seenArticle(false, article, true) // If the collection was uninitialized, initialize all articles without sending
-      else if (foundIds.includes(article._id)) seenArticle(true, article)
-      else if (foundTitles.includes(article.title)) seenArticle(false, article, true)
+      if (dbIds.length === 0 && dbIds.length !== 1) seenArticle(true) // If the collection was uninitialized, initialize all articles without sending. If it only has 1 article, it's likely that it's the first new article
+      else if (dbIds.includes(article._id)) seenArticle(true)
+      else if (checkTitle && dbTitles.includes(article.title)) seenArticle(true)
+      else if (checkDate && (!article.pubdate || article.pubdate.toString() === 'Invalid Date' || article.pubdate < cutoffDay)) seenArticle(true)
       else seenArticle(false, article)
     }
 
-    function seenArticle (seen, article, doNotSend) {
-      if (seen) return incrementProgress() // Stops here if it already exists in table, AKA "seen"
-      if (config.feeds.sendOldMessages === true && newerIds.includes(article._id) && !doNotSend) {
-        article.rssName = rssName
-        article.discordChannelId = channelId
-        callback(null, { status: 'article', article: article })
-      }
-      incrementProgress()
-    }
-
-    function incrementProgress () {
-      if (++processedArticles === totalArticles) finishSource()
+    function seenArticle (seen, article) {
+      if (seen || config.feeds.sendOldMessages !== true) return ++processedArticles === totalArticles ? finishSource() : null // Stops here if it already exists in table, AKA "seen"
+      article.rssName = rssName
+      article.discordChannelId = channelId
+      callback(null, { status: 'article', article: article })
+      return ++processedArticles === totalArticles ? finishSource() : null
     }
   }
 
