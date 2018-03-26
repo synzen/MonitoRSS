8b178824cbbdeac57a9d28cbce10b690ca6111a4
synzen <michael994664@gmail.com>
2018-03-26 19:40:54 -0400

    Check for undefined db.connection config

--- rss/db/connect.js
+++ rss/db/connect.js
@@ -2,13 +2,15 @@ const fs = require('fs')
 const dbSettings = require('../../config.json').database
 const mongoose = require('mongoose')
 const BUFFER_CONFIGS = ['sslCA', 'sslCRL', 'sslCert', 'sslKey']
-const CON_SETTINGS = dbSettings.connection
+const CON_SETTINGS = typeof dbSettings.connection === 'object' ? dbSettings.connection : {}
 
 module.exports = callback => {
   const buffers = {}
-  for (var x = 0; x < BUFFER_CONFIGS.length; ++x) {
-    const name = BUFFER_CONFIGS[x]
-    if (CON_SETTINGS[name]) buffers[name] = fs.readFileSync(CON_SETTINGS[name])
+  if (Object.keys(CON_SETTINGS).length > 0) {
+    for (var x = 0; x < BUFFER_CONFIGS.length; ++x) {
+      const name = BUFFER_CONFIGS[x]
+      if (CON_SETTINGS[name]) buffers[name] = fs.readFileSync(CON_SETTINGS[name])
+    }
   }
   mongoose.connect(dbSettings.uri, { keepAlive: 120, ...CON_SETTINGS, ...buffers })
   mongoose.connection.on('error', callback)
