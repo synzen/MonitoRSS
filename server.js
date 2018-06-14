5ca46ac977b0a08fdca6af01e907640f6c412ad4
synzen <michael994664@gmail.com>
2018-06-14 14:32:17 -0400

    Update server.js for config override

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const DiscordRSS = require('./index.js')
 const config = require('./config.json')
 
 const drss = new DiscordRSS.Client({ readFileSchedules: true, setPresence: true })
-drss.login(config.bot.token)
+let token = config.bot.token
+
+try {
+  const override = require('./settings/configOverride.json')
+  token = override ? override.bot.token : token
+} catch (err) {}
+
+drss.login(token)
