9a452594d410761502fa94477ac09eee6f796beb
synzen <michael994664@gmail.com>
2018-07-14 18:41:54 -0400

    Update mongo login options to prevent deprecation warning

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /usr/src/discord-rss
 COPY package*.json ./
 RUN npm install
 COPY . .
-ENV DRSS_BOT_TOKEN='drss_docker_token' DRSS_DATABASE_URI='mongodb://mongo/rss'
+ENV DRSS_BOT_TOKEN='drss_docker_token' DRSS_DATABASE_URI='mongodb://mongo:27017/rss'
 CMD ["node", "server.js"]
--- rss/db/connect.js
+++ rss/db/connect.js
@@ -19,7 +19,7 @@ module.exports = callback => {
 
   (function connect () {
     // Do not callback on .then here since the promise never gets resolved for some reason
-    mongoose.connect(uri, { keepAlive: 120, ...CON_SETTINGS, ...buffers }) // Environment variable in Docker container if available
+    mongoose.connect(uri, { keepAlive: 120, useNewUrlParser: true, ...CON_SETTINGS, ...buffers }) // Environment variable in Docker container if available
     .catch(err => {
       log.general.error('Failed to connect to database, retrying in 30 seconds...', err)
       setTimeout(connect, 30000)
