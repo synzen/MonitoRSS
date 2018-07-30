99ddf75c0ab522e61653eb41caae8da205fb9246
synzen <michael994664@gmail.com>
2018-07-30 17:02:12 -0400

    Replace console.log with logger function, resolved #61

--- util/listeners.js
+++ util/listeners.js
@@ -17,14 +17,14 @@ const EVENT_HANDLERS = {
 }
 let cmdsExtension
 if (fs.existsSync('./settings/commands.js')) {
-  try { cmdsExtension = require('../settings/commands.js') } catch (e) { console.log(`Error: Unable to load commands extension file. Reason:\n`, e) }
+  try { cmdsExtension = require('../settings/commands.js') } catch (e) { log.general.error(`Unable to load commands extension file`, e) }
   fs.watchFile('./settings/commands.js', (cur, prev) => {
     delete require.cache[require.resolve('../settings/commands.js')]
     try {
       cmdsExtension = require('../settings/commands.js')
-      console.log(`Commands extension file has been updated`)
+      log.general.success(`Commands extension file has been updated`)
     } catch (e) {
-      console.log(`Commands extension file was changed, but could not be updated. Reason:\n`, e)
+      log.general.error(`Commands extension file was changed, but could not be updated`, e)
     }
   })
 }
