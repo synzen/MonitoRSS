835e511ac0fdfd8f418af76e77c268e705abec01
synzen <michael994664@gmail.com>
2018-08-24 12:32:48 -0400

    Replaced needle dependency with node-fetch for better performance Use npm install again to install replacement dependency node-fetch

--- rss/request.js
+++ rss/request.js
@@ -1,43 +1,37 @@
-const needle = require('needle')
-const cloudscraper = require('cloudscraper') // For cloudflare
+const fetch = require('node-fetch')
+// const cloudscraper = require('cloudscraper') // For cloudflare
 
-module.exports = (link, cookies, feedparser) => {
+module.exports = async (link, cookies, feedparser) => {
   const options = {
     timeout: 20000,
-    read_timeout: 17000,
-    follow_max: 5,
-    follow_set_cookies: true,
-    rejectUnauthorized: true,
-    // GoogleBot to access explicit Tumblr feeds
-    headers: {'user-agent': `Mozilla/5.0 ${link.includes('.tumblr.com') ? 'GoogleBot' : ''} (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36`},
-    decode: false,
-    parse: false
+    follow: 5,
+    headers: {'user-agent': `Mozilla/5.0 ${link.includes('.tumblr.com') ? 'GoogleBot' : ''} (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36`}
   }
   if (cookies) options.cookies = cookies
+  let endStatus
 
-  return new Promise((resolve, reject) => {
-    ;(function connectToLink () {
-      const request = needle.get(link, options)
+  const res = await fetch(link, options)
+  endStatus = res.status
 
-      request.on('header', (statusCode, headers) => {
-        if (statusCode === 200) {
-          resolve(request)
-          // if (feedparser) request.pipe(feedparser)
-        } else if (headers.server && headers.server.includes('cloudflare')) {
-          cloudscraper.get(link, (err, res, body) => { // For cloudflare
-            if (err || res.statusCode !== 200) return reject(err || new Error(`Bad status code (${res.statusCode})` + `${cookies ? ' (cookies detected)' : ''}`))
-            if (!feedparser) return resolve()
-            let Readable = require('stream').Readable
-            let feedStream = new Readable()
-            feedStream.push(body)
-            feedStream.push(null)
-            resolve(feedStream)
-            // feedStream.pipe(feedparser)
-          })
-        } else request.emit('err', new Error(`Bad status code (${statusCode})`))
-      })
+  if (res.status === 200) return res.body
+  if (res.status === 403 || res.status === 400) {
+    delete options.headers
+    const res2 = await fetch(link, options)
+    endStatus = res2.status
+    if (res2.status === 200) return res2.body
+  }
 
-      request.on('err', err => reject(new Error(err.message || err + `${cookies ? ' (cookies detected)' : ''}`)))
-    })()
-  })
+  const serverHeaders = res.headers.get('server')
+  if (!serverHeaders || !serverHeaders.includes('cloudflare')) throw new Error(`Bad status code (${endStatus})`)
+  else throw new Error(`Bad Cloudflare status code (${endStatus})`)
+  // cloudscraper.get(link, (err, res, body) => { // For cloudflare
+  //   if (err || res.statusCode !== 200) return reject(err || new Error(`Bad Cloudflare status code (${res.statusCode})` + `${cookies ? ' (cookies detected)' : ''}`))
+  //   if (!feedparser) return resolve()
+  //   let Readable = require('stream').Readable
+  //   let feedStream = new Readable()
+  //   feedStream.push(body)
+  //   feedStream.push(null)
+  //   resolve(feedStream)
+  //   // feedStream.pipe(feedparser)
+  // })
 }
