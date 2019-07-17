3ef41c814c541ac9a52e49f557c643653f5c4477
synzen <michael994664@gmail.com>
2019-07-17 09:45:07 -0400

    Add sharded version of removeguild

--- commands/controller/removeguild.js
+++ commands/controller/removeguild.js
@@ -8,11 +8,33 @@ exports.normal = async (bot, message) => {
     if (!guild) return await message.channel.send('No such guild found.')
     await guild.leave()
     log.controller.success(`Guild ${content[1]} (${guild.name}) has been removed`, message.author)
-    return await message.channel.send('Guild removed.')
+    return await message.channel.send(`Guild ${content[1]} (${guild.name}) removed.`)
   } catch (err) {
     log.controller.warning('leaveguild', err)
     if (err.code !== 50013) message.channel.send(err.message).catch(err => log.controller.warning('leaveguild 1a', message.guild, err))
   }
 }
 
-exports.sharded = exports.normal
+exports.sharded = async (bot, message) => {
+  const content = message.content.split(' ')
+  try {
+    if (content.length !== 2) return await message.channel.send('You must specify the guild ID as the first argument.')
+    const results = await bot.shard.broadcastEval(`
+    const guild = this.guilds.get('${content[1]}')
+    const obj = {}
+    if (guild) {
+      guild.leave().catch(console.log)
+      obj.name = guild.name
+      obj.id = guild.id
+      obj
+    }
+    `)
+    const removed = results.filter(kicked => kicked)
+    if (removed.length === 0) return await message.channel.send('No such guild found.')
+    log.controller.success(`Guild ${content[1]} (${removed[0].name}) has been removed`, message.author)
+    return await message.channel.send(`Guild ${content[1]} (${removed[0].name}) was found - see console for whether the removal was successful.`)
+  } catch (err) {
+    log.controller.warning('leaveguild', message.author, err)
+    if (err.code !== 50013) message.channel.send(err.message).catch(err => log.controller.warning('leaveguild 1b', message.guild, err))
+  }
+}
