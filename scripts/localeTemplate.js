91f24cb13dcc41a21fd1d647f14e70dfc3e294a6
synzen <michael994664@gmail.com>
2019-08-03 23:11:36 -0400

    Add script to generate locale template

---
+++ scripts/localeTemplate.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+const defaultLocale = require('../src/config.js').bot.locale
+const localeData = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'src', 'locales', `${defaultLocale}.json`)))
+
+const obj = {}
+
+function traverse (object, reference) {
+  for (const key in object) {
+    const value = object[key]
+    if (typeof value === 'object') {
+      reference[key] = {}
+      traverse(value, reference[key])
+    } else {
+      reference[key] = ''
+    }
+  }
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Type the locale to generate a template: ', name => {
+  traverse(localeData, obj)
+  const file = path.join(__dirname, '..', 'src', 'locales', `${name}.json`)
+  fs.writeFileSync(file, JSON.stringify(obj, null, 2))
+  console.log(`Created at ${file}`)
+  rl.close()
+})
