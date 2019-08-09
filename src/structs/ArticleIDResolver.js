25c12910b6272a254076b89edb8a05542b351a17
synzen <michael994664@gmail.com>
2019-08-09 17:48:52 -0400

    Improve algorithm for article ID resolution

--- src/structs/ArticleIDResolver.js
+++ src/structs/ArticleIDResolver.js
@@ -3,47 +3,74 @@ const idTypeNames = ['guid', 'pubdate', 'title']
 class ArticleIDResolver {
   constructor () {
     this.idsRecorded = {}
-    this.useIdTypes = {}
-    this.mergedTypeNames = [] // An extension of idTypeNames - must be an array to maintain order
+    this.useIdTypes = []
     this.failedTypeNames = [] // Keep the order in which they are discarded - in case ALL id types fail, use the last one that failed
+    this.failedTypeNamesSet = new Set() // For O(1) access
+    this.mergedTypeNames = new Set()
 
+    // Push the merged names first - using multiple properties is more reliable
     for (let i = 0; i < idTypeNames.length; ++i) {
       const idType = idTypeNames[i]
       this.idsRecorded[idType] = {}
-      this.useIdTypes[idType] = true
       for (let j = i + 1; j < idTypeNames.length; ++j) {
         const nextIdType = idTypeNames[j]
         const mergedName = `${idType},${nextIdType}`
         this.idsRecorded[mergedName] = {}
-        this.useIdTypes[mergedName] = true
-        this.mergedTypeNames.push(mergedName)
+        this.useIdTypes.push(mergedName)
+        this.mergedTypeNames.add(mergedName)
       }
+    }
+
+    // Push the single id types at the end
+    for (const name of idTypeNames) {
+      this.useIdTypes.push(name)
     }
   }
 
   recordArticle (article) {
     const { useIdTypes, idsRecorded } = this
-    for (const idType in useIdTypes) {
-      if (!useIdTypes[idType]) continue
+    // Start from the end, since that's where the non-merged id types are.
+    // The single-property id types should be marked failed first to check the merged id types
+    for (let i = useIdTypes.length - 1; i >= 0; --i) {
+      const idType = useIdTypes[i]
+      if (this.failedTypeNamesSet.has(idType)) continue
+
+      if (this.mergedTypeNames.has(idType)) {
+        const individualIDTypes = idType.split(',')
+        for (const singleIDType of individualIDTypes) {
+          if (this.failedTypeNamesSet.has(singleIDType) && !this.failedTypeNamesSet.has(idType)) {
+            this.failedTypeNames.push(idType)
+            this.failedTypeNamesSet.add(idType)
+          }
+        }
+        if (this.failedTypeNamesSet.has(idType)) continue
+      }
+
       const articleValue = ArticleIDResolver.getIDTypeValue(article, idType)
       if (!articleValue || idsRecorded[idType][articleValue]) {
-        useIdTypes[idType] = false
+        // If the article value doesn't exist, or was already seen for this id type, then don't use it
         this.failedTypeNames.push(idType)
-      } else idsRecorded[idType][articleValue] = true
+        this.failedTypeNamesSet.add(idType)
+      } else {
+        idsRecorded[idType][articleValue] = true
+      }
     }
   }
 
   getIDType () {
-    const idTypes = idTypeNames.concat(this.mergedTypeNames)
-    for (const idType of idTypes) {
-      if (this.useIdTypes[idType]) return idType
+    for (const idType of this.useIdTypes) {
+      if (!this.failedTypeNamesSet.has(idType)) return idType
     }
-    return this.failedTypeNames[this.failedTypeNames.length - 1]
+    for (let i = this.failedTypeNames.length - 1; i >= 0; --i) {
+      const idType = this.failedTypeNames[i]
+      if (this.mergedTypeNames.has(idType)) return idType
+    }
+    // return this.failedTypeNames[this.failedTypeNames.length - 1]
   }
 
   static getIDTypeValue (article, idType) {
     const properties = idType.split(',')
-    return properties.map(property => article[property]).join('')
+    return properties.map(property => article[property] || '').join('')
   }
 }
 
