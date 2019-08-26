a71854982043eae73b473d474e2bb0cf7cd3083f
synzen <michael994664@gmail.com>
2019-08-26 17:28:25 -0400

    Remove useless file

--- server-client.js
+++
@@ -1,6 +0,0 @@
-// Create a single client
-
-const DiscordRSS = require('./src/index.js')
-const drss = new DiscordRSS.Client() // Override default config values here
-
-drss.login(require('./src/config.js').bot.token, true)
