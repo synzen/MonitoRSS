1c84af636ae819e6f51a88318c6472c6c98a4cb2
synzen <michael994664@gmail.com>
2019-09-25 08:55:39 -0400

    Document storage.js

--- src/util/storage.js
+++ src/util/storage.js
@@ -1,12 +1,46 @@
-/*
-*   Used to store data for various operations across multiple files
-*/
+// Used to store data for various operations across multiple files
 
+/**
+ * @type {import('discord.js').Client}
+ */
 exports.bot = undefined
+
+/**
+ * @type {import('redis').RedisClient}
+ */
 exports.redisClient = undefined
-exports.prefixes = {} // Guild prefixes
-exports.initialized = 0 // Different levels dictate what commands may be used while the bot is booting up. 0 = While all shards not initialized, 1 = While shard is initialized, 2 = While all shards initialized
-exports.deletedFeeds = [] // Any deleted rssNames to check during article sending to see if it was deleted during a cycle
+
+/**
+ * Guild command prefixes
+ * @type {Object<string, string>}
+ */
+exports.prefixes = {}
+
+/**
+ * Different levels dictate what commands may be used while the bot is booting up. 0 = While all shards not initialized, 1 = While shard is initialized, 2 = While all shards initialized
+ * @type {0|1|2}
+ */
+exports.initialized = 0
+
+/**
+ * Any deleted rssNames to check during article sending to see if it was deleted during a cycle
+ * @type {string[]}
+ */
+exports.deletedFeeds = []
+
+/**
+ * @type {import('../structs/ScheduleManager.js')}
+ */
 exports.scheduleManager = undefined
+
+/**
+ * List of IDs
+ * @type {string[]}
+ */
 exports.blacklistUsers = []
+
+/**
+ * List of IDs
+ * @type {string[]}
+ */
 exports.blacklistGuilds = []
