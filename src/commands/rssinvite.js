7ff8fb841a1a22bef420e7b8425e7385a1f5457e
synzen <michael994664@gmail.com>
2019-11-11 00:12:49 -0500

    Fix rssinvite links

--- src/commands/rssinvite.js
+++ src/commands/rssinvite.js
@@ -5,7 +5,7 @@ const Translator = require('../structs/Translator.js')
 module.exports = async (bot, message, automatic) => { // automatic indicates invokation by the bot
   try {
     const guildRss = await dbOpsGuilds.get(message.guild.id)
-    await message.channel.send(Translator.translate('commands.rssinvite.text', guildRss ? guildRss.locale : undefined))
+    await message.channel.send(Translator.translate('commands.rssinvite.text', guildRss ? guildRss.locale : undefined, { id: bot.user.id }))
   } catch (err) {
     log.command.warning('rssinvite', err)
     if (err.code !== 50013) message.channel.send(err.message).catch(err => log.command.warning('rssinvite 1', message.guild, err))
