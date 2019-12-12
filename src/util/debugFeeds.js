2abca93bf952f16350dd42142f3c70b676fb4ce4
synzen <michael994664@gmail.com>
2019-12-12 11:00:04 -0500

    Preload debug data from json

--- src/util/debugFeeds.js
+++ src/util/debugFeeds.js
@@ -1,3 +1,13 @@
-const debugRssNames = []
+const fs = require('fs')
+const path = require('path')
+const log = require('./logger.js')
+const debugPath = path.join(__dirname, '..', '..', 'settings', 'debug.json')
+
+let debugRssNames = []
+
+if (fs.existsSync(debugPath)) {
+  log.debug.info('Preloaded debug data from settings/debug.json')
+  debugRssNames = JSON.parse(fs.readFileSync(debugPath))
+}
 
 exports.list = debugRssNames
