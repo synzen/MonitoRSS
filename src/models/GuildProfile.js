38ebd9c8c0f0922057de3547af1a3b1e0dd6bebe
synzen <michael994664@gmail.com>
2019-12-23 11:30:15 -0500

    Update GuildProfile model

--- src/models/GuildProfile.js
+++ src/models/GuildProfile.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+const middleware = require('./middleware/GuildProfile.js')
+const path = require('path')
+const fs = require('fs')
+const packageVersion = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'package.json'))).version
 
-const schema = mongoose.Schema({
-  id: {
-    type: String,
-    unique: true
-  },
+const schema = new mongoose.Schema({
+  _id: String,
   name: String,
   sendAlertsTo: {
     type: [String],
@@ -14,11 +15,20 @@ const schema = mongoose.Schema({
   dateFormat: String,
   dateLanguage: String,
   timezone: String,
-  vip: Object,
   prefix: String,
   locale: String,
-  version: String
+  version: {
+    type: String,
+    default: packageVersion
+  },
+  feeds: [{
+    type: mongoose.Types.ObjectId,
+    ref: 'Feed'
+  }]
 })
 
+schema.pre('findOneAndUpdate', middleware.findOneAndUpdate)
+schema.pre('remove', middleware.remove)
+
 exports.schema = schema
-exports.model = () => mongoose.model('guilds', schema)
+exports.model = mongoose.model('Guild', schema)
