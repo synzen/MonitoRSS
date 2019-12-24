c205ac107f0c794927dddc0ab3002a20b2b83b40
synzen <michael994664@gmail.com>
2019-12-24 15:11:11 -0500

    Refactor rsssplit to new models

--- src/commands/rsssplit.js
+++ src/commands/rsssplit.js
@@ -4,6 +4,7 @@ const log = require('../util/logger.js')
 const MenuUtils = require('../structs/MenuUtils.js')
 const FeedSelector = require('../structs/FeedSelector.js')
 const Translator = require('../structs/Translator.js')
+const GuildProfile = require('../structs/db/GuildProfile.js')
 function escapeBackticks (str) {
   return str.replace('`', '​`') // Replace backticks with zero-width space and backtick to escape
 }
@@ -14,13 +15,16 @@ const defAppendCharStr = defPrependCharStr
 const defMaxLenStr = translate => translate('commands.rsssplit.defaultIsValue', { value: '1950' })
 
 async function feedSelectorFn (m, data) {
-  const { guildRss, rssName, translate } = data
-  const source = guildRss.sources[rssName]
-  const splitOptions = source.splitMessage // Show toggle if it is disabled
+  const { feed, translate } = data
+  const splitOptions = feed.split // Show toggle if it is disabled
 
   const nextMenu = new MenuUtils.Menu(m, splitOptions ? selectSetting : enable)
     .setAuthor(translate('commands.rsssplit.messageSplittingOptions'))
-    .setDescription(translate('commands.rsssplit.description', { title: source.title, link: source.link, currently: splitOptions ? translate('generics.enabledLower') : translate('generics.disabledLower') }))
+    .setDescription(translate('commands.rsssplit.description', {
+      title: feed.title,
+      link: feed.url,
+      currently: splitOptions ? translate('generics.enabledLower') : translate('generics.disabledLower')
+    }))
 
   const currentSplitChar = splitOptions && splitOptions.char ? splitOptions.char : ''
   const currentSplitPrepend = splitOptions && splitOptions.prepend ? splitOptions.prepend : ''
@@ -52,18 +56,21 @@ async function feedSelectorFn (m, data) {
 
 async function enable (m, data) {
   // This function only triggers if it is initially disabled
-  const { guildRss, rssName, translate } = data
-  const source = guildRss.sources[rssName]
-  if (m.content !== '1') throw new MenuUtils.MenuOptionError()
+  const { feed, translate } = data
+  if (m.content !== '1') {
+    throw new MenuUtils.MenuOptionError()
+  }
 
-  source.splitMessage = { enabled: true }
+  feed.split = {
+    enabled: true
+  }
 
-  await dbOpsGuilds.update(guildRss)
-  log.command.info(`Enabled message splitting for ${source.link}`, m.channel.guild)
+  await feed.save()
+  log.command.info(`Enabled message splitting for ${feed.url}`, m.channel.guild)
 
   const nextMenu = new MenuUtils.Menu(m, selectSetting)
     .setAuthor(translate('commands.rsssplit.messageSplittingOptions'))
-    .setDescription(translate('commands.rsssplit.enabledDescription', { title: source.title, link: source.link }))
+    .setDescription(translate('commands.rsssplit.enabledDescription', { title: feed.title, link: feed.url }))
     .addOption(translate('commands.rsssplit.optionSetSplitChar'), `${translate('commands.rsssplit.optionSetSplitCharDescription')} ${defSplitCharStr(translate)}`)
     .addOption(translate('commands.rsssplit.optionSetPrependChar'), `${translate('commands.rsssplit.optionSetPrependCharDescription')} ${defPrependCharStr(translate)}`)
     .addOption(translate('commands.rsssplit.optionSetAppendChar'), `${translate('commands.rsssplit.optionSetAppendCharDescription')} ${defAppendCharStr(translate)}`)
@@ -77,24 +84,29 @@ async function enable (m, data) {
 }
 
 async function selectSetting (m, data) {
-  const { guildRss, rssName, translate } = data
-  const source = guildRss.sources[rssName]
+  const { feed, translate } = data
   const selected = m.content
 
   if (selected === '5') {
-    delete source.splitMessage
-    await dbOpsGuilds.update(guildRss)
-    log.command.info(`Disabled message splitting for ${source.link}`, m.channel.guild)
-    await m.channel.send(translate('commands.rsssplit.disabledSuccess', { link: source.link }))
+    feed.split = undefined
+    await feed.save()
+    log.command.info(`Disabled message splitting for ${feed.url}`, m.channel.guild)
+    await m.channel.send(translate('commands.rsssplit.disabledSuccess', { link: feed.url }))
     return data
   }
 
   let nextText = ''
-  if (selected === '1') nextText = translate('commands.rsssplit.promptSplitChar')
-  else if (selected === '2') nextText = translate('commands.rsssplit.promptPrependChar')
-  else if (selected === '3') nextText = translate('commands.rsssplit.promptAppendChar')
-  else if (selected === '4') nextText = translate('commands.rsssplit.promptMaxLen')
-  else throw new MenuUtils.MenuOptionError()
+  if (selected === '1') {
+    nextText = translate('commands.rsssplit.promptSplitChar')
+  } else if (selected === '2') {
+    nextText = translate('commands.rsssplit.promptPrependChar')
+  } else if (selected === '3') {
+    nextText = translate('commands.rsssplit.promptAppendChar')
+  } else if (selected === '4') {
+    nextText = translate('commands.rsssplit.promptMaxLen')
+  } else {
+    throw new MenuUtils.MenuOptionError()
+  }
 
   return { ...data,
     selected: selected,
@@ -105,69 +117,69 @@ async function selectSetting (m, data) {
 }
 
 async function setSetting (m, data) {
-  const { guildRss, rssName, selected, translate } = data
-  const source = guildRss.sources[rssName]
+  const { feed, profile, selected, translate } = data
 
   let successText = ''
 
-  const translateArg = { link: source.link }
+  const translateArg = { link: feed.url }
   if (selected === '1') {
     if (m.content === 'reset') {
-      delete source.splitMessage.char
+      delete feed.split.char
       successText = translate('commands.rsssplit.resetSplitChar', translateArg)
-      log.command.info(`Message splitting split character for ${source.link} resetting`, m.channel.guild)
-    } else if (m.content === '\\n' && source.splitMessage.char === undefined) throw new MenuUtils.MenuOptionError(translate('commands.rsssplit.setSplitCharDefault'))
+      log.command.info(`Message splitting split character for ${feed.url} resetting`, m.channel.guild)
+    } else if (m.content === '\\n' && feed.split.char === undefined) throw new MenuUtils.MenuOptionError(translate('commands.rsssplit.setSplitCharDefault'))
     else {
-      source.splitMessage.char = m.content
-      successText = translate('commands.rsssplit.setSplitChar', { link: source.link, content: m.content })
-      log.command.info(`Message splitting split character for ${source.link} setting to ${m.content}`, m.channel.guild)
+      feed.split.char = m.content
+      successText = translate('commands.rsssplit.setSplitChar', { link: feed.url, content: m.content })
+      log.command.info(`Message splitting split character for ${feed.url} setting to ${m.content}`, m.channel.guild)
     }
   } else if (selected === '2') {
     if (m.content === 'reset') {
-      delete source.splitMessage.prepend
+      delete feed.split.prepend
       successText = translate('commands.rsssplit.resetPrependChar', translateArg)
-      log.command.info(`Message splitting prepend character for ${source.link} has been reset`, m.channel.guild)
+      log.command.info(`Message splitting prepend character for ${feed.url} has been reset`, m.channel.guild)
     } else {
-      source.splitMessage.prepend = m.content
-      successText = translate('commands.rsssplit.setPrependChar', { link: source.link, content: escapeBackticks(m.content) })
-      log.command.info(`Message splitting prepend character for ${source.link} setting to ${m.content}`, m.channel.guild)
+      feed.split.prepend = m.content
+      successText = translate('commands.rsssplit.setPrependChar', { link: feed.url, content: escapeBackticks(m.content) })
+      log.command.info(`Message splitting prepend character for ${feed.url} setting to ${m.content}`, m.channel.guild)
     }
   } else if (selected === '3') {
     if (m.content === 'reset') {
-      delete source.splitMessage.append
+      delete feed.split.append
       successText = translate('commands.rsssplit.resetAppendChar', translateArg)
-      log.command.info(`Message splitting append character for ${source.link} resetting`, m.channel.guild)
+      log.command.info(`Message splitting append character for ${feed.url} resetting`, m.channel.guild)
     } else {
-      source.splitMessage.append = m.content
-      successText = translate('commands.rsssplit.setAppendChar', { link: source.link, content: escapeBackticks(m.content) })
-      log.command.info(`Message splitting append character for ${source.link} setting to ${m.content}`, m.channel.guild)
+      feed.split.append = m.content
+      successText = translate('commands.rsssplit.setAppendChar', { link: feed.url, content: escapeBackticks(m.content) })
+      log.command.info(`Message splitting append character for ${feed.url} setting to ${m.content}`, m.channel.guild)
     }
   } else if (selected === '4') {
     const num = parseInt(m.content, 10)
     if (m.content === 'reset') {
-      delete source.splitMessage.maxLength
+      delete feed.split.maxLength
       successText = translate('commands.rsssplit.resetMaxLen', translateArg)
-      log.command.info(`Message splitting max length for ${source.link} resetting`, m.channel.guild)
+      log.command.info(`Message splitting max length for ${feed.url} resetting`, m.channel.guild)
     } else if (!/^\d+$/.test(m.content) || num < 500 || num > 1950) {
       throw new MenuUtils.MenuOptionError(translate('commands.rsssplit.setInvalidMaxLen'))
     } else {
-      source.splitMessage.maxLength = num
-      successText = translate('commands.rsssplit.setMaxLen', { link: source.link, num })
-      log.command.info(`Message splitting max length for ${source.link} setting to ${m.content}`, m.channel.guild)
+      feed.split.maxLength = num
+      successText = translate('commands.rsssplit.setMaxLen', { link: feed.url, num })
+      log.command.info(`Message splitting max length for ${feed.url} setting to ${m.content}`, m.channel.guild)
     }
   }
 
-  await dbOpsGuilds.update(guildRss)
-  await m.channel.send(`${successText} ${translate('generics.backupReminder', { prefix: guildRss.prefix || config.bot.prefix })}`)
+  await feed.save()
+  await m.channel.send(`${successText} ${translate('generics.backupReminder', { prefix: profile.prefix || config.bot.prefix })}`)
   return data
 }
 
 module.exports = async (bot, message, command) => {
   try {
-    const guildRss = await dbOpsGuilds.get(message.guild.id)
-    const guildLocale = guildRss ? guildRss.locale : undefined
-    const feedSelector = new FeedSelector(message, feedSelectorFn, { command }, guildRss)
-    await new MenuUtils.MenuSeries(message, [feedSelector], { locale: guildLocale, translate: Translator.createLocaleTranslator(guildLocale) }).start()
+    const profile = await GuildProfile.get(message.guild.id)
+    const guildLocale = profile ? profile.locale : undefined
+    const feeds = profile ? await profile.getFeeds() : []
+    const feedSelector = new FeedSelector(message, feedSelectorFn, { command }, feeds)
+    await new MenuUtils.MenuSeries(message, [feedSelector], { profile, locale: guildLocale, translate: Translator.createLocaleTranslator(guildLocale) }).start()
   } catch (err) {
     log.command.warning(`rsssplit`, message.guild, err)
     if (err.code !== 50013) message.channel.send(err.message).catch(err => log.command.warning('rsssplit 1', message.guild, err))
