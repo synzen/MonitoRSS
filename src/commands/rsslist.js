2de3585c7c947b4f30aebc1c76ce8e0ad9ccc235
synzen <michael994664@gmail.com>
2019-12-23 21:01:55 -0500

    Rewrite rsslist to use new data model

--- src/commands/rsslist.js
+++ src/commands/rsslist.js
@@ -2,24 +2,25 @@ const config = require('../config.js')
 const log = require('../util/logger.js')
 const MenuUtils = require('../structs/MenuUtils.js')
 const moment = require('moment')
-const dbOpsGuilds = require('../util/db/guilds.js')
 const dbOpsFailedLinks = require('../util/db/failedLinks.js')
 const dbOpsSchedules = require('../util/db/schedules.js')
 const serverLimit = require('../util/serverLimit.js')
 const storage = require('../util/storage.js')
 const Translator = require('../structs/Translator.js')
+const GuildProfile = require('../structs/db/GuildProfile.js')
 const FAIL_LIMIT = config.feeds.failLimit
 
 module.exports = async (bot, message, command) => {
   try {
-    const [ guildRss, serverLimitData ] = await Promise.all([ dbOpsGuilds.get(message.guild.id), serverLimit(message.guild.id) ])
-    const translate = Translator.createLocaleTranslator(guildRss ? guildRss.locale : undefined)
-    if (!guildRss || !guildRss.sources || Object.keys(guildRss.sources).length === 0) {
+    const [ profile, serverLimitData ] = await Promise.all([ GuildProfile.get(message.guild.id), serverLimit(message.guild.id) ])
+    const translate = Translator.createLocaleTranslator(profile ? profile.locale : undefined)
+    if (!profile || profile.feeds.length === 0) {
       return await message.channel.send(translate('commands.rsslist.noFeeds'))
     }
 
     const failedLinks = {}
-    const rssList = guildRss.sources
+    // const rssList = guildRss.sources
+    const feeds = await profile.getFeeds()
     let failedFeedCount = 0
 
     const vipUser = serverLimitData.vipUser
@@ -30,18 +31,17 @@ module.exports = async (bot, message, command) => {
     const failedLinksToCheck = []
     const schedulesToFetch = []
     const schedulesByFeedIDs = {}
-    for (const rssName in rssList) {
-      const feed = rssList[rssName]
-      schedulesToFetch.push(dbOpsSchedules.assignedSchedules.get(rssName))
+    for (const feed of feeds) {
+      schedulesToFetch.push(dbOpsSchedules.assignedSchedules.get(feed.id))
       let o = {
-        id: rssName,
-        link: feed.link,
+        id: feed.id,
+        url: feed.url,
         title: feed.title,
         webhook: feed.webhook ? feed.webhook.id : undefined,
         channel: bot.channels.get(feed.channel) ? bot.channels.get(feed.channel).name : undefined,
-        titleChecks: feed.titleChecks === true ? translate('commands.rsslist.titleChecksEnabled') : null
+        checkTitles: feed.checkTitles === true ? translate('commands.rsslist.titleChecksEnabled') : null
       }
-      failedLinksToCheck.push(feed.link)
+      failedLinksToCheck.push(feed.url)
       if (feed.disabled) {
         o.status = translate('commands.rsslist.statusDisabled', { reason: feed.disabled })
         o.isDisabled = true
@@ -77,8 +77,8 @@ module.exports = async (bot, message, command) => {
       } else vipDetails += 'Ongoing\n'
     } else vipDetails = '\n'
 
-    let desc = maxFeedsAllowed === 0 ? `${vipDetails}\u200b\n` : `${vipDetails}**${translate('commands.rsslist.serverLimit')}:** ${Object.keys(rssList).length}/${maxFeedsAllowed} [＋](https://www.patreon.com/discordrss)\n\n\u200b`
-    desc += failedFeedCount > 0 ? translate('commands.rsslist.failAlert', { failLimit: FAIL_LIMIT, prefix: guildRss.prefix || config.bot.prefix }) : ''
+    let desc = maxFeedsAllowed === 0 ? `${vipDetails}\u200b\n` : `${vipDetails}**${translate('commands.rsslist.serverLimit')}:** ${profile.feeds.length}/${maxFeedsAllowed} [＋](https://www.patreon.com/discordrss)\n\n\u200b`
+    desc += failedFeedCount > 0 ? translate('commands.rsslist.failAlert', { failLimit: FAIL_LIMIT, prefix: profile.prefix || config.bot.prefix }) : ''
 
     const list = new MenuUtils.Menu(message)
       .setAuthor(translate('commands.rsslist.currentActiveFeeds'))
@@ -87,7 +87,7 @@ module.exports = async (bot, message, command) => {
     if (vipUser) list.setFooter(`Patronage backed by ${vipUser.name} (${vipUser.id})`)
 
     currentRSSList.forEach(item => {
-      const link = item.link
+      const url = item.url
       const title = item.title
       const channelName = item.channel
       const status = item.status
@@ -96,7 +96,7 @@ module.exports = async (bot, message, command) => {
       const schedule = storage.scheduleManager.getSchedule(schedulesByFeedIDs[item.id].schedule)
       let refreshRate = schedule ? schedule.refreshRate < 1 ? `${schedule.refreshRate * 60} ${translate('commands.rsslist.seconds')}` : `${schedule.refreshRate} ${translate('commands.rsslist.minutes')}` : translate('commands.rsslist.unknown')
       if (config._vip === true && !vipUser) refreshRate += ' [－](https://www.patreon.com/discordrss)'
-      list.addOption(`${title.length > 200 ? title.slice(0, 200) + '[...]' : title}`, `${titleChecks || ''}${status || ''}${translate('generics.channelUpper')}: #${channelName}\n${translate('commands.rsslist.refreshRate')}: ${refreshRate}\n${webhook ? `${translate('commands.rsslist.webhook')}: ${webhook}\n` : ''}${translate('commands.rsslist.link')}: ${link.length > 500 ? translate('commands.rsslist.exceeds500Characters') : link}`)
+      list.addOption(`${title.length > 200 ? title.slice(0, 200) + '[...]' : title}`, `${titleChecks || ''}${status || ''}${translate('generics.channelUpper')}: #${channelName}\n${translate('commands.rsslist.refreshRate')}: ${refreshRate}\n${webhook ? `${translate('commands.rsslist.webhook')}: ${webhook}\n` : ''}${translate('commands.rsslist.link')}: ${url.length > 500 ? translate('commands.rsslist.exceeds500Characters') : url}`)
     })
 
     await list.send()
