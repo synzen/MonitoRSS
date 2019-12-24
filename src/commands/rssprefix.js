f52be297b901d9559507f1787736768c0c74b91e
synzen <michael994664@gmail.com>
2019-12-24 12:16:33 -0500

    Refactor rssprefix to use new models

--- src/commands/rssprefix.js
+++ src/commands/rssprefix.js
@@ -1,26 +1,26 @@
 const log = require('../util/logger.js')
 const config = require('../config.js')
-const dbOpsGuilds = require('../util/db/guilds.js')
 const storage = require('../util/storage.js')
 const Translator = require('../structs/Translator.js')
+const GuildProfile = require('../structs/db/GuildProfile.js')
 
 module.exports = async (bot, message) => {
   const prefix = message.content.split(' ')[1]
   try {
-    let guildRss = await dbOpsGuilds.get(message.guild.id)
-    const translate = Translator.createLocaleTranslator(guildRss ? guildRss.locale : undefined)
+    const profile = await GuildProfile.get(message.guild.id)
+    const translate = Translator.createLocaleTranslator(profile ? profile.locale : undefined)
 
     if (!prefix) {
       return await message.channel.send(translate('commands.rssprefix.helpText'))
     }
     // Reset
     if (prefix === 'reset') {
-      if (!guildRss || !guildRss.prefix) {
+      if (!profile || !profile.prefix) {
         return await message.channel.send(translate('commands.rssprefix.resetNone'))
       }
-      delete guildRss.prefix
-      delete storage.prefixes[guildRss.id]
-      await dbOpsGuilds.update(guildRss, true)
+      profile.prefix = undefined
+      delete storage.prefixes[profile.id]
+      await profile.save()
       return await message.channel.send(translate('commands.rssprefix.resetSuccess', { prefix: config.bot.prefix }))
     }
     if (prefix.length > 4) {
@@ -30,12 +30,21 @@ module.exports = async (bot, message) => {
       return await message.channel.send(translate('commands.rssprefix.cannotUseDefault'))
     }
 
-    if (!guildRss) guildRss = { id: message.guild.id, name: message.guild.name, prefix }
-    else guildRss.prefix = prefix
+    if (!profile) {
+      const data = {
+        _id: message.guild.id,
+        name: message.guild.name,
+        prefix
+      }
+      const newProfile = new GuildProfile(data)
+      await newProfile.save()
+    } else {
+      profile.prefix = prefix
+      await profile.save()
+    }
 
-    await dbOpsGuilds.update(guildRss)
     await message.channel.send(translate('commands.rssprefix.setSuccess', { prefix }))
-    storage.prefixes[guildRss.id] = prefix
+    storage.prefixes[profile.id] = prefix
   } catch (err) {
     log.command.warning(`rssprefix`, message.guild, err)
     if (err.code !== 50013) message.channel.send(err.message).catch(err => log.command.warning('rssprefix 1', message.guild, err))
