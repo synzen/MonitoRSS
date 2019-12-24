61b8ec15a9a2c06378de3f606fa18f051cee1754
synzen <michael994664@gmail.com>
2019-12-24 15:56:08 -0500

    Refactor rsswebhook to use new models

--- src/commands/rsswebhook.js
+++ src/commands/rsswebhook.js
@@ -1,28 +1,30 @@
-const dbOpsGuilds = require('../util/db/guilds.js')
 const dbOpsVips = require('../util/db/vips.js')
 const MenuUtils = require('../structs/MenuUtils.js')
 const FeedSelector = require('../structs/FeedSelector.js')
 const Translator = require('../structs/Translator.js')
+const GuildProfile = require('../structs/db/GuildProfile.js')
 const log = require('../util/logger.js')
 
 async function feedSelectorFn (m, data) {
-  const { guildRss, rssName, translate } = data
-  const existingWebhook = guildRss.sources[rssName].webhook
+  const { feed, translate } = data
+  const webhook = feed.webhook
 
-  const text = `${typeof existingWebhook === 'object' ? translate('commands.rsswebhook.existingFound', { name: existingWebhook.name }) : ''}${translate('commands.rsswebhook.prompt')}`
+  const text = `${webhook ? translate('commands.rsswebhook.existingFound', { name: webhook.name }) : ''}${translate('commands.rsswebhook.prompt')}`
 
   return { ...data,
-    existingWebhook: existingWebhook,
+    existingWebhook: webhook,
     next: {
       text: text,
       embed: null
     } }
 }
 
-async function collectWebhook (m, data) {
+async function collectWebhookFn (m, data) {
   const { hooks, translate } = data
   const webhookName = m.content
-  if (webhookName === '{remove}') return { ...data, webhookName: webhookName }
+  if (webhookName === '{remove}') {
+    return { ...data, webhookName }
+  }
 
   const nameRegex = /--name="(((?!(--name|--avatar)).)*)"/
   const avatarRegex = /--avatar="(((?!(--name|--avatar)).)*)"/
@@ -40,51 +42,66 @@ async function collectWebhook (m, data) {
       throw new MenuUtils.MenuOptionError(translate('commands.rsswebhook.tooLong'))
     }
   }
-  if (customAvatarSrch) customAvatarSrch = customAvatarSrch[1]
-  return { ...data, webhook: hook, customAvatarSrch: customAvatarSrch, customNameSrch: customNameSrch }
+  if (customAvatarSrch) {
+    customAvatarSrch = customAvatarSrch[1]
+  }
+  return { ...data, webhook: hook, customAvatarSrch, customNameSrch }
 }
 
 module.exports = async (bot, message, command) => {
   try {
-    const [ guildRss, isVipServer ] = await Promise.all([ dbOpsGuilds.get(message.guild.id), dbOpsVips.isVipServer(message.guild.id) ])
-    const guildLocale = guildRss ? guildRss.locale : undefined
+    const [ profile, isVipServer ] = await Promise.all([ GuildProfile.get(message.guild.id), dbOpsVips.isVipServer(message.guild.id) ])
+    const guildLocale = profile ? profile.locale : undefined
     const translate = Translator.createLocaleTranslator(guildLocale)
     if (!isVipServer) {
       log.command.info(`Unauthorized attempt to access webhooks`, message.guild, message.author)
       return await message.channel.send(`Only servers with patron backing have access to webhooks.`)
     }
-    if (!message.guild.me.permissionsIn(message.channel).has('MANAGE_WEBHOOKS')) return await message.channel.send(translate('commands.rsswebhook.noPermission'))
+    if (!message.guild.me.permissionsIn(message.channel).has('MANAGE_WEBHOOKS')) {
+      return await message.channel.send(translate('commands.rsswebhook.noPermission'))
+    }
 
     const hooks = await message.channel.fetchWebhooks()
-    const feedSelector = new FeedSelector(message, feedSelectorFn, { command: command }, guildRss)
-    const webhookSelector = new MenuUtils.Menu(message, collectWebhook)
+    const feeds = profile ? await profile.getFeeds() : []
+    const feedSelector = new FeedSelector(message, feedSelectorFn, { command: command }, feeds)
+    const collectWebhook = new MenuUtils.Menu(message, collectWebhookFn)
 
-    const data = await new MenuUtils.MenuSeries(message, [feedSelector, webhookSelector], { hooks, locale: guildLocale, translate }).start()
-    if (!data) return
-    const { rssName, existingWebhook, webhookName, webhook, customAvatarSrch, customNameSrch } = data
-    const source = guildRss.sources[rssName]
+    const data = await new MenuUtils.MenuSeries(message, [feedSelector, collectWebhook], { hooks, locale: guildLocale, translate }).start()
+    if (!data) {
+      return
+    }
+    const { feed, existingWebhook, webhookName, webhook, customAvatarSrch, customNameSrch } = data
+
     if (webhookName === '{remove}') {
       if (typeof existingWebhook !== 'object') {
         await message.channel.send(translate('commands.rsswebhook.noneAssigned'))
       } else {
-        delete source.webhook
-        await message.channel.send(translate('commands.rsswebhook.removeSuccess', { link: source.link }))
+        feed.webhook = undefined
+        await feed.save()
+        await message.channel.send(translate('commands.rsswebhook.removeSuccess', { link: feed.url }))
       }
-    } else {
-      source.webhook = {
-        id: webhook.id
-      }
-
-      if (customNameSrch) source.webhook.name = customNameSrch
-      if (customAvatarSrch) source.webhook.avatar = customAvatarSrch
-      log.command.info(`Webhook ID ${webhook.id} (${webhook.name}) connecting to feed ${source.link}`, message.guild, message.channel)
-      const connected = translate('commands.rsswebhook.connected', { botUser: bot.user, link: source.link })
-      webhook.send(connected, { username: customNameSrch, avatarURL: customAvatarSrch })
-        .catch(err => {
-          if (err.message.includes('avatar_url')) return webhook.send(connected, { username: customNameSrch }).catch(err => log.comamnd.warning(`rsswebhook 2`, message.guild, err)) // This may be a placeholder
-        })
+      return
     }
-    await dbOpsGuilds.update(guildRss)
+
+    feed.webhook = {
+      id: webhook.id
+    }
+
+    if (customNameSrch) {
+      feed.webhook.name = customNameSrch
+    }
+    if (customAvatarSrch) {
+      feed.webhook.avatar = customAvatarSrch
+    }
+    log.command.info(`Webhook ID ${webhook.id} (${webhook.name}) connecting to feed ${feed.url}`, message.guild, message.channel)
+    const connected = translate('commands.rsswebhook.connected', { botUser: bot.user, link: feed.url })
+    await feed.save()
+    webhook.send(connected, { username: customNameSrch, avatarURL: customAvatarSrch })
+      .catch(err => {
+        if (err.message.includes('avatar_url')) {
+          return webhook.send(connected, { username: customNameSrch }).catch(err => log.comamnd.warning(`rsswebhook 2`, message.guild, err)) // This may be a placeholder
+        }
+      })
   } catch (err) {
     log.command.warning(`rsswebhook`, message.guild, err)
     if (err.code !== 50013) message.channel.send(err.message).catch(err => log.command.warning('rsswebhook 1', message.guild, err))
