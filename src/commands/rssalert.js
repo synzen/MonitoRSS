8afc2e03e9028cf93d35476b313abcdb1b52d1d7
synzen <michael994664@gmail.com>
2019-12-24 09:48:49 -0500

    Refactor rssalert to use new models

--- src/commands/rssalert.js
+++ src/commands/rssalert.js
@@ -1,50 +1,54 @@
 const log = require('../util/logger.js')
 const config = require('../config.js')
-const dbOpsGuilds = require('../util/db/guilds.js')
 const Translator = require('../structs/Translator.js')
+const GuildProfile = require('../structs/db/GuildProfile.js')
 
 module.exports = async (bot, message, automatic) => { // automatic indicates invokation by the bot
   try {
-    const guildRss = await dbOpsGuilds.get(message.guild.id)
-    const translate = Translator.createLocaleTranslator(guildRss ? guildRss.locale : undefined)
-    if (!guildRss || !guildRss.sources || Object.keys(guildRss.sources).length === 0) return await message.channel.send(Translator.translate('commands.rssalert.noFeeds'))
+    const profile = await GuildProfile.get(message.guild.id)
+    const translate = Translator.createLocaleTranslator(profile ? profile.locale : undefined)
+    if (!profile || profile.feeds.length === 0) {
+      return await message.channel.send(Translator.translate('commands.rssalert.noFeeds'))
+    }
     const contentArray = message.content.split(' ').map(item => item.trim())
-    const guildID = guildRss.id
-    const guildName = guildRss.name
-    const prefix = guildRss.prefix || config.bot.prefix
+    const guildID = profile.id
+    const guildName = profile.name
+    const prefix = profile.prefix || config.bot.prefix
     switch (contentArray[1]) {
       case 'add':
       case 'remove':
-        if (!contentArray[2]) return await message.channel.send(translate('commands.rssalert.info', { prefix }))
+        if (!contentArray[2]) {
+          return await message.channel.send(translate('commands.rssalert.info', { prefix }))
+        }
         const userMention = message.mentions.users.first()
         const member = userMention || await message.guild.members.get(contentArray[2] === 'me' ? message.author.id : contentArray[2])
         if (!member) return await message.channel.send(translate('commands.rssalert.notFound', { user: userMention || `\`${contentArray[2]}\`` }))
         if (contentArray[1] === 'add') {
-          if (!Array.isArray(guildRss.sendAlertsTo)) guildRss.sendAlertsTo = [ member.id ]
-          else {
-            if (guildRss.sendAlertsTo.includes(member.id)) return await message.channel.send(translate('commands.rssalert.alreadyEnabled'))
-            guildRss.sendAlertsTo.push(member.id)
+          if (profile.alert.includes(member.id)) {
+            return await message.channel.send(translate('commands.rssalert.alreadyEnabled'))
           }
+          profile.alert.push(member.id)
           log.command.info(`Added user to alerts`, message.guild, userMention || member.user)
           await member.send(translate('commands.rssalert.successDM', { member, guildName, guildID }))
           await message.channel.send(translate('commands.rssalert.success', { member }))
         } else {
-          if (Array.isArray(guildRss.sendAlertsTo)) {
-            const removeIndex = guildRss.sendAlertsTo.indexOf(member.id)
-            if (removeIndex === -1) return await message.channel.send(translate('commands.rssalert.removeFail'))
-            guildRss.sendAlertsTo.splice(removeIndex, 1)
-            if (guildRss.sendAlertsTo.length === 0) guildRss.sendAlertsTo = undefined
-            log.command.info(`Removed user from alerts`, message.guild, userMention || member.user)
-            await member.send(translate('commands.rssalert.removedDM', { member, guildName, guildID }))
-            await message.channel.send(translate('commands.rssalert.removed', { member }))
-          } else return await message.channel.send(translate('commands.rssalert.removeFail'))
+          const removeIndex = profile.alert.indexOf(member.id)
+          if (removeIndex === -1) {
+            return await message.channel.send(translate('commands.rssalert.removeFail'))
+          }
+          profile.alert.splice(removeIndex, 1)
+          log.command.info(`Removed user from alerts`, message.guild, userMention || member.user)
+          await member.send(translate('commands.rssalert.removedDM', { member, guildName, guildID }))
+          await message.channel.send(translate('commands.rssalert.removed', { member }))
         }
-        await dbOpsGuilds.update(guildRss, true)
+        await profile.save()
         break
       case 'list':
-        if (!Array.isArray(guildRss.sendAlertsTo) || guildRss.sendAlertsTo.length === 0) return await message.channel.send(translate('commands.rssalert.listEmpty'))
+        if (profile.alert.length === 0) {
+          return await message.channel.send(translate('commands.rssalert.listEmpty'))
+        }
         let msg = translate('commands.rssalert.list')
-        for (const id of guildRss.sendAlertsTo) {
+        for (const id of profile.alert) {
           msg += `${await message.guild.members.get(id)}\n`
         }
         await message.channel.send(msg)
