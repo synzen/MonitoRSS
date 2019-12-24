fc9e496c434d5a1d15b9499f6c9530d9a6d6787b
synzen <michael994664@gmail.com>
2019-12-24 15:44:06 -0500

    Refactor rssrefresh to use new models

--- src/commands/rssrefresh.js
+++ src/commands/rssrefresh.js
@@ -1,34 +1,37 @@
 const config = require('../config.js')
 const log = require('../util/logger.js')
-const dbOpsGuilds = require('../util/db/guilds.js')
 const dbOpsFailedLinks = require('../util/db/failedLinks.js')
 const channelTracker = require('../util/channelTracker.js')
 const FeedFetcher = require('../util/FeedFetcher.js')
 const Translator = require('../structs/Translator.js')
+const GuildProfile = require('../structs/db/GuildProfile.js')
 const FAIL_LIMIT = config.feeds.failLimit
 
 module.exports = async (bot, message, command) => {
   try {
-    const guildRss = await dbOpsGuilds.get(message.guild.id)
-    const translate = Translator.createLocaleTranslator(guildRss ? guildRss.locale : undefined)
-    if (!guildRss || !guildRss.sources || Object.keys(guildRss.sources).length === 0) {
+    const profile = await GuildProfile.get(message.guild.id)
+    const translate = Translator.createLocaleTranslator(profile ? profile.locale : undefined)
+    if (!profile || profile.feeds.length === 0) {
       return await message.channel.send(translate('commands.rsslist.noFeeds'))
     }
 
     if (FAIL_LIMIT === 0) {
       return await message.channel.send(translate('commands.rssrefresh.noFailLimit'))
     }
-    const rssList = guildRss.sources
-    if (!rssList || Object.keys(rssList).length === 0) {
-      return await message.channel.send(translate('commands.rssrefresh.noFailedFeeds'))
+
+    const feeds = await profile.getFeeds()
+    if (!feeds || feeds.length === 0) {
+      return await message.channel.send(translate('commands.rssrefresh.noFeeds'))
     }
+
     let toRefresh = []
     channelTracker.add(message.channel.id)
-    for (const rssName in rssList) {
-      const source = rssList[rssName]
-      const failedLinkStatus = await dbOpsFailedLinks.get(source.link)
-      if (!failedLinkStatus || !failedLinkStatus.failed) continue
-      toRefresh.push(source.link)
+    for (const feed of feeds) {
+      const failedLinkStatus = await dbOpsFailedLinks.get(feed.url)
+      if (!failedLinkStatus || !failedLinkStatus.failed) {
+        continue
+      }
+      toRefresh.push(feed.url)
     }
     if (toRefresh.length === 0) {
       channelTracker.remove(message.channel.id)
@@ -50,13 +53,20 @@ module.exports = async (bot, message, command) => {
     let successfulLinks = ''
     let failedLinks = ''
     for (const link of toRefresh) {
-      if (!failedReasons[link]) successfulLinks += `${link}\n`
-      else failedLinks += `${link} (${failedReasons[link]})`
+      if (!failedReasons[link]) {
+        successfulLinks += `${link}\n`
+      } else {
+        failedLinks += `${link} (${failedReasons[link]})`
+      }
     }
 
     let reply = ''
-    if (successfulLinks) reply += translate('commands.rssrefresh.success') + '\n```' + successfulLinks + '```\n\n'
-    if (failedLinks) reply += translate('commands.rssrefresh.failed') + '\n```' + failedLinks + '```'
+    if (successfulLinks) {
+      reply += translate('commands.rssrefresh.success') + '\n```' + successfulLinks + '```\n\n'
+    }
+    if (failedLinks) {
+      reply += translate('commands.rssrefresh.failed') + '\n```' + failedLinks + '```'
+    }
     channelTracker.remove(message.channel.id)
     await processing.edit(reply)
   } catch (err) {
