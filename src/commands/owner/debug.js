3cb9b70d13054ea463f914977a90620329406b1e
synzen <michael994664@gmail.com>
2019-12-31 18:00:00 -0500

    Refactor debug command for new models

--- src/commands/owner/debug.js
+++ src/commands/owner/debug.js
@@ -1,5 +1,5 @@
 const debug = require('../../util/debugFeeds.js')
-const dbOpsGuilds = require('../../util/db/guilds.js')
+const Feed = require('../../structs/db/Feed.js')
 const log = require('../../util/logger.js')
 
 exports.normal = async (bot, message) => {
@@ -9,21 +9,16 @@ exports.normal = async (bot, message) => {
   const rssName = content[1]
 
   try {
-    const guildRssList = await dbOpsGuilds.getAll()
-    for (const guildRss of guildRssList) {
-      const rssList = guildRss.sources
-      for (const name in rssList) {
-        if (rssName === name) {
-          if (!debug.feeds.has(rssName)) {
-            debug.feeds.add(rssName)
-          }
-          await message.channel.send(`Added ${rssName} to debug`)
-          return true
-        }
-      }
+    const feed = await Feed.get(rssName)
+    if (!feed) {
+      await message.channel.send(`Unable to add ${rssName} to debugging list, not found in any guild sources.`)
+      return false
     }
-    await message.channel.send(`Unable to add ${rssName} to debugging list, not found in any guild sources.`)
-    return false
+    if (!debug.feeds.has(rssName)) {
+      debug.feeds.add(rssName)
+    }
+    await message.channel.send(`Added ${rssName} to debug`)
+    return true
   } catch (err) {
     log.owner.warning('debug', err)
     if (err.code !== 50013) message.channel.send(err.message).catch(err => log.owner.warning('debug 1', message.guild, err))
