09eecace9cf3fa1bb58c45b8713809aeea3485eb
synzen <michael994664@gmail.com>
2020-01-05 16:41:58 -0500

    Rewrite collectionid to not use AssignedSchedule

--- src/commands/owner/collectionid.js
+++ src/commands/owner/collectionid.js
@@ -1,26 +1,53 @@
-const AssignedSchedule = require('../../structs/db/AssignedSchedule.js')
+const Feed = require('../../structs/db/Feed.js')
 const ArticleModel = require('../../models/Article.js')
 const log = require('../../util/logger.js')
 
+async function getFeed (bot, message) {
+  const args = message.content.split(' ')
+  const feedID = args[1]
+  if (!feedID) {
+    return null
+  }
+  const feed = await Feed.get(feedID)
+  if (!feed) {
+    return null
+  }
+  return feed
+}
+
 exports.normal = async (bot, message) => {
   try {
-    const args = message.content.split(' ')
-    const feedID = args[1]
-    if (!feedID) {
-      return await message.channel.send('No feed ID argument')
+    const feed = await getFeed(bot, message)
+    if (!feed) {
+      return await message.channel.send('No such feed found.')
     }
-    const assigned = await AssignedSchedule.getByFeedAndShard(feedID, -1)
-    if (!assigned) {
-      return await message.channel.send('No assigned schedule')
-    }
-    const link = assigned.link
-    const shardID = assigned.shard
-    const scheduleName = assigned.schedule
-    await message.channel.send(ArticleModel.getCollectionID(link, shardID, scheduleName))
+    const schedule = await feed.determineSchedule()
+    await message.channel.send(ArticleModel.getCollectionID(feed.url, -1, schedule.name))
   } catch (err) {
     log.owner.warning('collectionid', err)
     if (err.code !== 50013) message.channel.send(err.message).catch(err => log.owner.warning('collectionid 1a', message.guild, err))
   }
 }
 
-exports.sharded = exports.normal
+exports.sharded = async (bot, message) => {
+  try {
+    const feed = await getFeed(bot, message)
+    if (!feed) {
+      return await message.channel.send('No such feed found.')
+    }
+    const guild = feed.guild
+    const schedule = await feed.determineSchedule()
+    const res = await bot.shard.broadcastEval(`
+      const guild = this.guilds.get('${guild}');
+      guild ? this.shard.id : null
+    `)
+    const shard = res.find(result => result !== null)
+    if (shard === undefined) {
+      return await message.channel.send(`Guild ${guild} was not found within the bot.`)
+    }
+    await message.channel.send(ArticleModel.getCollectionID(feed.url, shard, schedule.name))
+  } catch (err) {
+    log.owner.warning('collectionid sharded', err)
+    if (err.code !== 50013) message.channel.send(err.message).catch(err => log.owner.warning('collectionid 2a', message.guild, err))
+  }
+}
