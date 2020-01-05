dc9807e15be4882149ec1a785a10023bbc5ff3bf
synzen <michael994664@gmail.com>
2020-01-04 22:28:37 -0500

    Add checkPermissions maintenance task

---
+++ src/tests/util/maintenance/unit_checkPermissions.test.js
@@ -0,0 +1,146 @@
+process.env.TEST_ENV = true
+const checkPermissions = require('../../../util/maintenance/checkPermissions.js')
+
+describe('Unit::util/maintenance/checkPermission', function () {
+  const permissionsIn = jest.fn()
+  const bot = {
+    channels: {
+      get: () => ({
+        guild: {
+          me: {
+            permissionsIn
+          }
+        }
+      })
+    }
+  }
+  beforeEach(function () {
+    permissionsIn.mockReset()
+  })
+  it('calls disable for undisabled feed for missing view channel message', async function () {
+    const feed = {
+      disabled: undefined,
+      disable: jest.fn(() => Promise.resolve())
+    }
+    const format = {
+      embeds: []
+    }
+    permissionsIn.mockReturnValue(new Set(['SEND_MESSAGES', 'EMBED_LINKS']))
+    const res = checkPermissions(feed, format, bot)
+    expect(feed.disable).toHaveBeenCalledTimes(1)
+    expect(feed.disable).toHaveBeenCalledWith('Missing permissions VIEW_CHANNEL')
+    expect(res).toEqual(true)
+  })
+  it('calls disable for undisabled feed for missing send messages', async function () {
+    const feed = {
+      disabled: undefined,
+      disable: jest.fn(() => Promise.resolve())
+    }
+    const format = {
+      embeds: []
+    }
+    permissionsIn.mockReturnValue(new Set(['VIEW_CHANNEL', 'EMBED_LINKS']))
+    const res = checkPermissions(feed, format, bot)
+    expect(feed.disable).toHaveBeenCalledTimes(1)
+    expect(feed.disable).toHaveBeenCalledWith('Missing permissions SEND_MESSAGES')
+    expect(res).toEqual(true)
+  })
+  it('calls disable for undisabled feed for missing embed links with embed', async function () {
+    const feed = {
+      disabled: undefined,
+      disable: jest.fn(() => Promise.resolve())
+    }
+    const format = {
+      embeds: [{}]
+    }
+    permissionsIn.mockReturnValue(new Set(['VIEW_CHANNEL', 'SEND_MESSAGES']))
+    const res = checkPermissions(feed, format, bot)
+    expect(feed.disable).toHaveBeenCalledTimes(1)
+    expect(feed.disable).toHaveBeenCalledWith('Missing permissions EMBED_LINKS')
+    expect(res).toEqual(true)
+  })
+  it('does not call disable for undisabled feed for missing embed links with no embed', async function () {
+    const feed = {
+      disabled: undefined,
+      disable: jest.fn(() => Promise.resolve())
+    }
+    const format = {
+      embeds: []
+    }
+    permissionsIn.mockReturnValue(new Set(['VIEW_CHANNEL', 'SEND_MESSAGES']))
+    const res = checkPermissions(feed, format, bot)
+    expect(feed.disable).not.toHaveBeenCalled()
+    expect(res).toEqual(false)
+  })
+  it('calls disable for undisabled feed for multiple permissions missing', async function () {
+    const feed = {
+      disabled: undefined,
+      disable: jest.fn(() => Promise.resolve())
+    }
+    const format = {
+      embeds: []
+    }
+    permissionsIn.mockReturnValue(new Set(['EMBED_LINKS']))
+    const res = checkPermissions(feed, format, bot)
+    expect(feed.disable).toHaveBeenCalledTimes(1)
+    expect(feed.disable).toHaveBeenCalledWith(`Missing permissions SEND_MESSAGES, VIEW_CHANNEL`)
+    expect(res).toEqual(true)
+  })
+  it('changes the disable reason if one of the permission status changes', async function () {
+    const feed = {
+      disabled: 'Missing permissions VIEW_CHANNEL',
+      disable: jest.fn(() => Promise.resolve())
+    }
+    const format = {
+      embeds: []
+    }
+    permissionsIn.mockReturnValue(new Set(['EMBED_LINKS']))
+    const res = checkPermissions(feed, format, bot)
+    expect(feed.disable).toHaveBeenCalledTimes(1)
+    expect(feed.disable).toHaveBeenCalledWith(`Missing permissions SEND_MESSAGES, VIEW_CHANNEL`)
+    expect(res).toEqual(true)
+  })
+  it('does not call disable if feed is already disabled unrelated to permissions', async function () {
+    const feed = {
+      disabled: 'hook hok',
+      disable: jest.fn(() => Promise.resolve())
+    }
+    const format = {
+      embeds: []
+    }
+    permissionsIn.mockReturnValue(new Set())
+    const res = checkPermissions(feed, format, bot)
+    expect(feed.disable).not.toHaveBeenCalled()
+    expect(res).toEqual(true)
+  })
+  it('enables the feed if all permissions found', async function () {
+    const feed = {
+      disabled: 'Missing permissions VIEW_CHANNEL',
+      disable: jest.fn(() => Promise.resolve()),
+      enable: jest.fn(() => Promise.resolve())
+    }
+    const format = {
+      embeds: []
+    }
+    permissionsIn.mockReturnValue(new Set(['EMBED_LINKS', 'VIEW_CHANNEL', 'SEND_MESSAGES']))
+    const res = checkPermissions(feed, format, bot)
+    expect(feed.disable).not.toHaveBeenCalled()
+    expect(feed.enable).toHaveBeenCalledTimes(1)
+    expect(res).toEqual(false)
+  })
+  it('does not enable the feed if the disable reason is unrelated', async function () {
+    const feed = {
+      disabled: 'whodat',
+      disable: jest.fn(() => Promise.resolve()),
+      enable: jest.fn(() => Promise.resolve())
+    }
+    const format = {
+      embeds: []
+    }
+    permissionsIn.mockReturnValue(new Set(['EMBED_LINKS', 'VIEW_CHANNEL', 'SEND_MESSAGES']))
+    const res = checkPermissions(feed, format, bot)
+    expect(feed.disable).not.toHaveBeenCalled()
+    expect(feed.enable).not.toHaveBeenCalled()
+    expect(res).toEqual(true)
+  })
+})
---
+++ src/util/maintenance/checkPermissions.js
@@ -0,0 +1,47 @@
+const log = require('../logger.js')
+
+/**
+ * Precondition: The feed's guild belongs to the bot, or the
+ * shard if it is sharded.
+ * @param {import('../../structs/db/Feed.js')} feed - The feed
+ * @param {import('../../structs/db/Format.js')} format - The feed's format
+ * @param {import('discord.js').Client} bot
+ * @returns {boolean} - The feed's disabled status
+ */
+function checkPermissions (feed, format, bot) {
+  if (feed.disabled && !feed.disabled.startsWith('Missing permissions')) {
+    // The feed is disabled for a separate reason - skip all checks
+    return true
+  }
+  const channel = bot.channels.get(feed.channel)
+  const guild = channel.guild
+  const permissions = guild.me.permissionsIn(channel)
+  const allowView = permissions.has('VIEW_CHANNEL')
+  const allowSendMessages = permissions.has('SEND_MESSAGES')
+  const allowEmbedLinks = format.embeds.length === 0 ? true : permissions.has('EMBED_LINKS')
+  if (!allowSendMessages || !allowEmbedLinks || !allowView) {
+    let reasons = []
+    if (!allowSendMessages) reasons.push('SEND_MESSAGES')
+    if (!allowEmbedLinks) reasons.push('EMBED_LINKS')
+    if (!allowView) reasons.push('VIEW_CHANNEL')
+    const reason = `Missing permissions ${reasons.join(', ')}`
+    if (!feed.disabled) {
+      feed.disable(reason)
+        .then(() => log.general.info(`Disabled feed ${feed._id} (${reason})`, guild, channel))
+        .catch(err => log.general.error(`Failed to disable feed ${feed._id} (${reason})`, guild, err))
+    } else if (feed.disabled.startsWith('Missing permissions') && feed.disabled !== reason) {
+      feed.disable(reason)
+        .then(() => log.general.info(`Updated disabled feed ${feed._id} (${reason})`, guild, channel))
+        .catch(err => log.general.error(`Failed to update disabled reason for feed ${feed._id} (${reason})`, guild, err))
+    }
+    return true
+  } else if (feed.disabled && feed.disabled.startsWith('Missing permissions')) {
+    feed.enable()
+      .then(() => log.general.info(`Enabled feed ${feed._id} for found permissions`, guild, channel))
+      .catch(err => log.general.error(`Failed to enable feed ${feed._id} after channel permissions found`, guild, err))
+    return false
+  }
+  return !!feed.disabled
+}
+
+module.exports = checkPermissions
