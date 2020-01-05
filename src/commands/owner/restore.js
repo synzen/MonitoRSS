61a5994ffd842f7a622612839665f9d43d91f74d
synzen <michael994664@gmail.com>
2020-01-05 17:00:52 -0500

    Rewrite rssbackup and restore to use new models

--- src/commands/owner/restore.js
+++ src/commands/owner/restore.js
@@ -1,37 +1,36 @@
 const fetch = require('node-fetch')
-const dbOpsGuilds = require('../../util/db/guilds.js')
+const GuildData = require('../../structs/GuildData.js')
 const log = require('../../util/logger.js')
 
 async function getID (message) {
   const arr = message.content.split(' ')
   const id = arr[1]
-  if (!id) throw new Error('No ID found. You must pass the server ID as an argument to this command.')
+  if (!id) {
+    throw new Error('No ID found. You must pass the server ID as an argument to this command.')
+  }
   const attachment = message.attachments.first()
   const url = attachment ? attachment.url : undefined
-  if (!url) throw new Error('No attachment found')
-  const res = await fetch(url)
-  if (res.status !== 200) throw new Error(`Non-200 status code: `, res.status)
-  const file = await res.json()
-  delete file._id
-  delete file.__v
-  if (!file.id) throw new Error('No ID found in file')
-  if (file.id !== id) throw new Error('File ID does not match input ID')
-  const sources = file.sources
-  if (sources && Object.keys(sources).length > 0) {
-    for (var n in sources) {
-      if (!sources[n].addedOn) sources[n].addedOn = new Date()
-    }
+  if (!url) {
+    throw new Error('No attachment found')
   }
+  const res = await fetch(url)
+  if (res.status !== 200) {
+    throw new Error(`Non-200 status code: `, res.status)
+  }
+  const file = await res.json()
   return file
 }
 
 exports.normal = async (bot, message) => {
   try {
     const file = await getID(message)
-    const id = file.id
+    const guildData = new GuildData(file)
+    const id = guildData.id
     const guild = bot.guilds.get(id)
-    if (!guild) return await message.chanel.send(`Unable to restore server, ID ${id} was not found in cache.`)
-    await dbOpsGuilds.update(file)
+    if (!guild) {
+      return await message.chanel.send(`Unable to restore server, ID ${id} was not found in cache.`)
+    }
+    await guildData.restore()
     log.owner.success(`Server (ID: ${id}, Name: ${guild.name}) has been restored`, message.author)
     await message.channel.send(`Server (ID: ${id}, Name: ${guild.name}) has been restored.`)
   } catch (err) {
@@ -43,17 +42,19 @@ exports.normal = async (bot, message) => {
 exports.sharded = async (bot, message) => {
   try {
     const file = await getID(message)
-    const id = file.id
+    const guildData = new GuildData(file)
+    const id = guildData.id
     const res = await bot.shard.broadcastEval(`
       const guild = this.guilds.get('${id}');
       guild ? guild.name : null
     `)
     for (var i = 0; i < res.length; ++i) {
       if (!res[i]) continue
-      await dbOpsGuilds.update(file)
+      await guildData.restore()
       log.owner.success(`Server (ID: ${id}, Name: ${res[i]}) has been restored`, message.author)
       await message.channel.send(`Server (ID: ${id}, Name: ${res[i]}) has been restored.`)
     }
+    await message.chanel.send(`Unable to restore server, guild ${id} was not found in cache.`)
   } catch (err) {
     log.owner.warning(`restore`, message.author, err)
     if (err.code !== 50013) message.channel.send(err.message).catch(err => log.owner.warning('restore 1b', message.guild, err))
--- src/commands/rssbackup.js
+++ src/commands/rssbackup.js
@@ -1,19 +1,24 @@
+const GuildData = require('../structs/GuildData.js')
 const Attachment = require('discord.js').Attachment
-const log = require('../util/logger.js')
-const dbOpsGuilds = require('../util/db/guilds')
 const Translator = require('../structs/Translator.js')
+const log = require('../util/logger.js')
 
 module.exports = async (bot, message, automatic) => { // automatic indicates invokation by the bot
   try {
-    const guildRss = await dbOpsGuilds.get(message.guild.id)
-    const translate = Translator.createLocaleTranslator(guildRss ? guildRss.locale : undefined)
-    if (!guildRss && !automatic) await message.channel.send(translate('commands.rssbackup.noProfile'))
-    if (!guildRss) return
-    const backup = JSON.parse(JSON.stringify(guildRss, null, 2))
-    delete backup._id
-    delete backup.__v
-    if (message.guild.me.permissionsIn(message.channel).has('ATTACH_FILES')) await message.channel.send(new Attachment(Buffer.from(JSON.stringify(backup, null, 2)), message.guild.id + '.json'))
-    else await message.channel.send(translate('commands.rssbackup.noPermission'))
+    const guildId = message.guild.id
+    const guildData = await GuildData.get(guildId)
+    const locale = guildData.profile ? guildData.profile.locale : undefined
+    const translate = Translator.createLocaleTranslator(locale)
+    if (guildData.isEmpty() && !automatic) {
+      return await message.channel.send(translate('commands.rssbackup.noProfile'))
+    }
+    if (message.guild.me.permissionsIn(message.channel).has('ATTACH_FILES')) {
+      const data = Buffer.from(JSON.stringify(guildData.toJSON(), null, 2))
+      const attachment = new Attachment(data, guildId + '.json')
+      await message.channel.send(attachment)
+    } else {
+      await message.channel.send(translate('commands.rssbackup.noPermission'))
+    }
   } catch (err) {
     log.command.warning('rssbackup', err)
     if (err.code !== 50013) message.channel.send(err.message).catch(err => log.command.warning('rssbackup 1', message.guild, err))
