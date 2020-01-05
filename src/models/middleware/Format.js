90f12da91ca6b7a3f4782da9925b6ef7221fd564
synzen <michael994664@gmail.com>
2020-01-05 12:23:14 -0500

    Fix typo

--- src/models/middleware/Format.js
+++ src/models/middleware/Format.js
@@ -4,7 +4,7 @@
 async function validate () {
   const feed = await this.model('Feed').findById(this.feed).exec()
   if (!feed) {
-    throw new Error(`Subscriber's specified feed ${this.feed} was not found`)
+    throw new Error(`Format's specified feed ${this.feed} was not found`)
   }
   const current = await this.model('Format').findById(this._id).exec()
   // If current doesn't exist, then it's a new subscriber
