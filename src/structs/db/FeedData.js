8f1c80f2f7e4f8d15bc51b2c58a02c8a266fa3fe
synzen <michael994664@gmail.com>
2020-01-21 22:52:57 -0500

    Add FeedData that fetches combines relevant data

---
+++ src/structs/db/FeedData.js
@@ -0,0 +1,104 @@
+const Profile = require('./Profile.js')
+const Feed = require('./Feed.js')
+
+class FeedData extends Feed {
+  constructor (data, _saved) {
+    super(data, _saved)
+
+    /**
+     * @type {import('./Profile.js')}
+     */
+    this.profile = undefined
+
+    /**
+     * @type {import('./Format.js')}
+     */
+    this.format = undefined
+
+    /**
+     * @type {import('./Subscriber.js')[]}
+     */
+    this.subscribers = []
+
+    /**
+     * @type {import('./FilteredFormat.js')[]}
+     */
+    this.filteredFormats = []
+  }
+
+  async _populate () {
+    const [
+      profile,
+      format,
+      subscribers,
+      filteredFormats
+    ] = await Promise.all([
+      Profile.get(this.guild),
+      this.getFormat(),
+      this.getSubscribers(),
+      this.getFilteredFormats()
+    ])
+    this.profile = profile
+    this.format = format
+    this.subscribers = subscribers
+    this.filteredFormats = filteredFormats
+  }
+
+  toObject () {
+    return {
+      ...super.toObject(),
+      profile: this.profile ? this.profile.toObject() : undefined,
+      format: this.format ? this.format.toObject() : undefined,
+      subscribers: this.subscribers.map(s => s.toObject()),
+      filteredFormats: this.filteredFormats.map(f => f.toObject())
+    }
+  }
+
+  toJSON () {
+    return {
+      ...super.toJSON(),
+      profile: this.profile ? this.profile.toJSON() : undefined,
+      format: this.format ? this.format.toJSON() : undefined,
+      subscribers: this.subscribers.map(s => s.toJSON()),
+      filteredFormats: this.filteredFormats.map(f => f.toJSON())
+    }
+  }
+
+  static async get (id) {
+    const found = await super.get(id)
+    if (found) {
+      await found._populate()
+    }
+    return found
+  }
+
+  static async getByQuery (data) {
+    const found = await super.getByQuery(data)
+    if (found) {
+      await found._populate()
+    }
+    return found
+  }
+
+  static async getManyByQuery (data) {
+    const found = await super.getManyByQuery(data)
+    const promises = []
+    for (const f of found) {
+      promises.push(f._populate())
+    }
+    await Promise.all(promises)
+    return found
+  }
+
+  static async getAll () {
+    const found = await super.getAll()
+    const promises = []
+    for (const f of found) {
+      promises.push(f._populate())
+    }
+    await Promise.all(promises)
+    return found
+  }
+}
+
+module.exports = FeedData
