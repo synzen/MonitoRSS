b7839b841b276767943078c37d88b58e37899d3e
synzen <michael994664@gmail.com>
2020-01-22 14:28:36 -0500

    Add rate limit and url check for getting feeds

--- src/web/routes/api/feeds/index.js
+++ src/web/routes/api/feeds/index.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const feedsAPI = express.Router()
+const rateLimit = require('express-rate-limit')
+const validate = require('../../../middleware/validator.js')
+const createError = require('../../../util/createError.js')
+const {
+  param
+} = require('express-validator')
 
-// Remember for stricter rate limit for unauthenticated getFeed
+if (process.env.NODE_ENV !== 'test') {
+  feedsAPI.use(rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 20, // 20 requests per 1 minute
+    message: createError(429, 'Too many requests')
+  }))
+}
 
-feedsAPI.get('/:url', require('../../../controllers/api/feeds/getFeed.js')())
+feedsAPI.get('/:url', validate([
+  param('url', 'Not a valid URL').isURL({
+    protocols: ['http', 'https'],
+    require_protocol: true
+  })
+]), require('../../../controllers/api/feeds/getFeed.js')())
 
 module.exports = feedsAPI
