09b9144f7920ddd3d098a87e1c5414ceb25f280f
synzen <michael994664@gmail.com>
2020-01-23 12:20:48 -0500

    Add PATCH guilds/:guildID/feeds/:feedID route Also fix feedID validation order

--- src/web/routes/api/guilds/feeds/index.js
+++ src/web/routes/api/guilds/feeds/index.js
@@ -3,6 +3,7 @@ const guildFeedsAPI = express.Router({ mergeParams: true })
 const validate = require('../../../../middleware/validator.js')
 const guildHasFeed = require('../../../../middleware/guildHasFeed.js')
 const guildHasChannel = require('../../../../middleware/guildHasChannel.js')
+const guildHasChannelOptional = require('../../../../middleware/guildHasChannelOptional.js')
 const {
   isMongoID
 } = require('../../../../util/validators/index.js')
@@ -11,20 +12,37 @@ const {
   body
 } = require('express-validator')
 
+guildFeedsAPI.post('/', [
+  validate([
+    body(['url', 'channel'])
+      .isString().withMessage('Must be a string')
+      .notEmpty().withMessage('Cannot be empty'),
+  ]),
+  guildHasChannel
+], require('../../../../controllers/api/guilds/feeds/createFeed.js'))
+
+// Make sure feedID exists before proceeding
 guildFeedsAPI.use('/:feedID', validate([
   param('feedID')
     .custom(isMongoID).withMessage('Invalid resource ID')
 ]))
 
-guildFeedsAPI.post('/', [
-  validate([
-    body(['url', 'channel'])
-      .exists({ checkFalsy: true }).withMessage('Must exist')
-      .isString().withMessage('Must be a string'),
-  ]),
-  guildHasChannel
-], require('../../../../controllers/api/guilds/feeds/createFeed.js'))
-
+// Make sure the guild has this feed
 guildFeedsAPI.use('/:feedID', guildHasFeed)
+
+// Edit the feed
+guildFeedsAPI.patch('/:feedID', [
+  validate([
+    body('channel')
+      .if(val => !!val)
+      .isString().withMessage('Must be a string')
+      .notEmpty().withMessage('Cannot be empty'),
+    body('title')
+      .if(val => !!val)
+      .isString().withMessage('Must be a string')
+      .notEmpty().withMessage('Cannot be empty')
+  ]),
+  guildHasChannelOptional
+], require('../../../../controllers/api/guilds/feeds/editFeed.js'))
 
 module.exports = guildsAPI
