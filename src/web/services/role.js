cfd5c84c7c8573582c67d454fbbcb3acc17a9e80
synzen <michael994664@gmail.com>
2020-01-24 11:25:15 -0500

    Add isRoleOfGuild service, add null check to getRole

--- src/web/services/role.js
+++ src/web/services/role.js
@@ -1,10 +1,16 @@
 const RedisRole = require('../../structs/db/Redis/Role.js')
 
+/**
+ * @param {string} roleID
+ */
 async function getRole (roleID) {
   const role = await RedisRole.fetch(roleID)
-  return role.toJSON()
+  return role ? role.toJSON() : null
 }
 
+/**
+ * @param {Object<string, any>} roleData 
+ */
 async function formatRole (roleData) {
   return {
     ...roleData,
@@ -25,13 +31,30 @@ async function getRoles (roleIDs) {
     .sort((a, b) => b.position - a.position)
 }
 
+/**
+ * @param {string} roleID 
+ * @param {string} guildID 
+ */
 async function isManagerOfGuild (roleID, guildID) {
   return RedisRole.utils.isManagerOfGuild(roleID, guildID)
+}
+
+/**
+ * @param {string} roleID 
+ * @param {string} guildID 
+ */
+async function isRoleOfGuild (roleID, guildID) {
+  const role = await getRole(roleID)
+  if (!role) {
+    return false
+  }
+  return role.guildID === guildID
 }
 
 module.exports = {
   getRole,
   getRoles,
   formatRole,
-  isManagerOfGuild
+  isManagerOfGuild,
+  isRoleOfGuild
 }
