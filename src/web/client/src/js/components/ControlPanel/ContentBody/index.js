90926e2a9a077fc45072bd277bd9cbf2b65a81d2
synzen <michael994664@gmail.com>
2020-02-02 20:52:10 -0500

    Convert ContentBody index to hooks

--- src/web/client/src/js/components/ControlPanel/ContentBody/index.js
+++ src/web/client/src/js/components/ControlPanel/ContentBody/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import React from 'react'
+import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 import Home from './Main/Home/index'
 import Feeds from './Server/Feeds/index'
@@ -10,18 +10,10 @@ import Subscriptions from './Feed/Subscriptions/index'
 import MiscOptions from './Feed/MiscOptions/index'
 import Debugger from './Feed/Debugger/index'
 import { Switch, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { setActiveGuild } from '../../../actions/index-actions'
+import { useDispatch } from 'react-redux'
 import pages from '../../../constants/pages';
-import { changePage } from '../../../actions/index-actions'
 import { Scrollbars } from 'react-custom-scrollbars';
-
-const mapDispatchToProps = dispatch => {
-  return {
-    setActiveGuild: guildId => dispatch(setActiveGuild(guildId)),
-    changePage: page => dispatch(changePage(page))
-  }
-}
+import { changePage } from 'js/actions/page'
 
 const Body = styled.div`
   height: 100%;
@@ -31,41 +23,35 @@ const Body = styled.div`
   /* scrollbar-width: thin; */
 `
 
-class ContentBody extends Component {
-  constructor () {
-    super()
-    this.state = {
 
-    }
+function ContentBody () {
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+  function redirect (page) {
+    dispatch(changePage(page))
+    history.push(page)
   }
 
-  redirect = page => {
-    const { changePage } = this.props
-    changePage(page)
-    this.props.history.push(page)
-  }
-
-  render () {
-    return (
-      <Body>
-        {/* <Notice>Hello!</Notice> */}
-        <Scrollbars>
-          <Switch>
-            
-            <Route exact path={pages.DASHBOARD} render={routerProps => <Home redirect={this.redirect} {...routerProps} />}/>
-            <Route exact path={pages.FEEDS} render={routerProps => <Feeds redirect={this.redirect} {...routerProps} />} />
-            <Route exact path={pages.SERVER_SETTINGS} render={routerProps => <Settings {...routerProps} />} />
-            <Route exact path={pages.MESSAGE} render={routerProps => <Message {...routerProps} />} />
-            <Route exact path={pages.FILTERS} render={routerProps => <Filters {...routerProps} />} />
-            <Route exact path={pages.SUBSCRIPTIONS} render={routerProps => <Subscriptions {...routerProps} />} />
-            <Route exact path={pages.MISC_OPTIONS} render={routerProps => <MiscOptions {...routerProps} />} />
-            <Route exact path={pages.DEBUGGER} component={routerProps => <Debugger {...routerProps} />} />
-            <Route render={routerProps => <Home {...routerProps} />} />
-          </Switch>
-        </Scrollbars>
-      </Body>
-    )
-  }
+  return (
+    <Body>
+      {/* <Notice>Hello!</Notice> */}
+      <Scrollbars>
+        <Switch>
+          
+          <Route exact path={pages.DASHBOARD} render={routerProps => <Home redirect={redirect} {...routerProps} />}/>
+          <Route exact path={pages.FEEDS} render={routerProps => <Feeds redirect={redirect} {...routerProps} />} />
+          <Route exact path={pages.SERVER_SETTINGS} render={routerProps => <Settings {...routerProps} />} />
+          <Route exact path={pages.MESSAGE} render={routerProps => <Message {...routerProps} />} />
+          <Route exact path={pages.FILTERS} render={routerProps => <Filters {...routerProps} />} />
+          <Route exact path={pages.SUBSCRIPTIONS} render={routerProps => <Subscriptions {...routerProps} />} />
+          <Route exact path={pages.MISC_OPTIONS} render={routerProps => <MiscOptions {...routerProps} />} />
+          <Route exact path={pages.DEBUGGER} component={routerProps => <Debugger {...routerProps} />} />
+          <Route render={routerProps => <Home {...routerProps} />} />
+        </Switch>
+      </Scrollbars>
+    </Body>
+  )
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(ContentBody))
+export default ContentBody
