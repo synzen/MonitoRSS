1864df7a6d05ea89540b030483ea9aa8dd4b12c5
synzen <michael994664@gmail.com>
2020-02-08 16:00:45 -0500

    Auto-lowercase all filter inputs on web

--- src/web/util/validation/filterSchema.js
+++ src/web/util/validation/filterSchema.js
@@ -2,5 +2,5 @@ const Joi = require('@hapi/joi')
 
 module.exports = Joi.object().pattern(/^/, Joi.alternatives(
   Joi.string().valid(''),
-  Joi.array().items(Joi.string())
+  Joi.array().items(Joi.string().lowercase())
 ))
