fca69731bd6daef23c11d3c164b59e397b4bbe9e
synzen <michael994664@gmail.com>
2020-02-09 22:23:05 -0500

    Fix lint, add missing health, fix added on

--- src/web/client/src/js/components/ControlPanel/ContentBody/Server/Feeds/SideBar.js
+++ src/web/client/src/js/components/ControlPanel/ContentBody/Server/Feeds/SideBar.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import PropTypes from 'prop-types'
 import PageHeader from 'js/components/utils/PageHeader'
 import SectionTitle from 'js/components/utils/SectionTitle'
 import SectionSubtitle from 'js/components/utils/SectionSubtitle'
 import PopInButton from '../../../utils/PopInButton'
 import styled from 'styled-components'
-import { Button, Dropdown, Input, Divider } from 'semantic-ui-react';
+import { Button, Dropdown, Input, Divider } from 'semantic-ui-react'
 import { isMobile } from 'react-device-detect'
 import posed from 'react-pose'
 import pages from 'js/constants/pages'
-import colors from 'js/constants/colors';
+import colors from 'js/constants/colors'
 import moment from 'moment-timezone'
-import { Scrollbars } from 'react-custom-scrollbars';
+import { Scrollbars } from 'react-custom-scrollbars'
 import feedSelectors from 'js/selectors/feeds'
 import { setActiveFeed, fetchDeleteFeed, fetchEditFeed } from 'js/actions/feeds'
 
@@ -47,10 +48,19 @@ const PosedDiv = posed.div({
   hide: { opacity: 0, transition: { duration: 200 } }
 })
 
+function calculateHealth (failRecord, hoursUntilFail) {
+  if (!failRecord) {
+    return 100
+  }
+  const hours = (new Date().getTime() - new Date(failRecord.failedAt).getTime()) / 36e5
+  return 100 - Math.ceil(hours / hoursUntilFail * 100)
+}
+
 function SideBar (props) {
   const [inputTitle, setInputTitle] = useState('')
   const [inputChannel, setInputChannel] = useState('')
   const schedules = useSelector(state => state.schedules)
+  const botConfig = useSelector(state => state.botConfig)
   const articlesFetching = useSelector(feedSelectors.articlesFetching)
   const feedRemoving = useSelector(feedSelectors.feedRemoving)
   const feedEditing = useSelector(feedSelectors.feedEditing)
@@ -58,7 +68,6 @@ function SideBar (props) {
   const guildID = useSelector(state => state.activeGuildID)
   const activeGuild = useSelector(state => state.guilds.find(g => g.id === state.activeGuildID))
   const dispatch = useDispatch()
-  // TODO: Do health
   const defaultConfig = useSelector(state => state.botConfig)
   const { selectedFeed, channelDropdownOptions } = props
   const selectedFailRecord = selectedFeed ? failRecords.find(r => r.url === selectedFeed.url) : null
@@ -112,75 +121,80 @@ function SideBar (props) {
   return (
     <Container populated={selectedFeed}>
       <Scrollbars>
-      <Content>
-      <PageHeader heading='Feed Details' subheading={'Select a feed'} />
-      {/* <SectionItemTitle>Feed Details</SectionItemTitle> */}
-      <Divider />
-      <PosedDiv pose={selectedFeed ? 'show' : 'hide'}>
-        <SectionTitle heading='Info' />
-        <SectionSubtitle>Status</SectionSubtitle>
-          { !selectedFeed
-            ? '\u200b'
-            : disabled
-              ? <span style={{ color: colors.discord.yellow }}>Disabled ({selectedFeed.disabled})</span>
-              : hasFailed
-                ? <span style={{ color: colors.discord.red }}>Failed ({moment(selectedFailRecord.failedAt).format('DD MMMM Y')})</span>
-                : <div><span style={{ color: colors.discord.green }}>Normal</span></div>
-        }
-        <EditField>
-          <SectionSubtitle>Refresh Rate</SectionSubtitle>
-      { hasFailed || disabled ? 'None ' : !selectedFeed ? '\u200b' : !refreshRate ? 'Determining... ' : refreshRate < 1 ? `${refreshRate * 60} seconds      ` : `${refreshRate} minutes      `}{ selectedFailRecord ? null : <a href='https://www.patreon.com/discordrss' target='_blank' rel='noopener noreferrer'>－</a> }
-        </EditField>
-        <EditField>
-          <SectionSubtitle>Added On</SectionSubtitle>
-          { !selectedFeed ? '\u200b' : !selectedFeed.addedOn ? 'Unknown' : moment(selectedFeed.addedOn).locale(dateLanguage).tz(dateTimezone).format(dateFormat)}
-        </EditField>
-        <Divider />
-        <SectionTitle heading='Edit' />
-        <EditField>
-          <SectionSubtitle>Title</SectionSubtitle>
-          <Input value={inputTitle || (selectedFeed ? selectedFeed.title : '')} fluid disabled={!selectedFeed} onChange={e => {
-            setInputTitle(e.target.value)
-          }} />
-        </EditField>
-        <EditField>
-          <SectionSubtitle>Channel</SectionSubtitle>
-          <Dropdown value={inputChannel || (selectedFeed ? selectedFeed.channel : '')} options={channelDropdownOptions} disabled={!selectedFeed || channelDropdownOptions.length === 0} search={!isMobile} selection fluid onChange={(e, data) => {
-            setInputChannel(data.value)
-          }} />
-        </EditField>
-        <ApplyField>
-          <PopInButton basic inverted content='Reset' disabled={feedEditing || !differentFromDefault} pose={differentFromDefault ? 'enter' : 'exit'} onClick={e => {
-            setInputTitle('')
-            setInputChannel('')
-          }} />
-          <Button content='Save' color='green' disabled={!differentFromDefault} onClick={edit} />
-        </ApplyField>
-        <Divider />
-        <SectionTitle heading='Customize' subheading='So many options!' />
-        <EditField>
-          <Button fluid content='Message/Embed' icon='angle right' labelPosition='right' onClick={e => redirect(pages.MESSAGE)} loading={articlesFetching} disabled={articlesFetching} />
-        </EditField>
-        <EditField>
-          <Button fluid content='Filters' icon='angle right' labelPosition='right' onClick={e => redirect(pages.FILTERS)} loading={articlesFetching} disabled={articlesFetching} />
-        </EditField>
-        <EditField>
-          <Button fluid content='Subscribers' icon='angle right' labelPosition='right' onClick={e => redirect(pages.SUBSCRIBERS)} loading={articlesFetching} disabled={articlesFetching} />
-        </EditField>
-        <EditField>
-          <Button fluid content='Misc Options' icon='angle right' labelPosition='right' onClick={e => redirect(pages.MISC_OPTIONS)} loading={articlesFetching} disabled={articlesFetching} />
-        </EditField>
-        <Divider />
-        <SectionTitle heading='Remove' />
-        <EditField>
-          <Button fluid content='Remove' basic color='red' loading={feedRemoving} disabled={feedRemoving} onClick={remove}/>
-        </EditField>
-      </PosedDiv>
-      </Content>
+        <Content>
+          <PageHeader heading='Feed Details' subheading={'Select a feed'} />
+          {/* <SectionItemTitle>Feed Details</SectionItemTitle> */}
+          <Divider />
+          <PosedDiv pose={selectedFeed ? 'show' : 'hide'}>
+            <SectionTitle heading='Info' />
+            <SectionSubtitle>Status</SectionSubtitle>
+            { !selectedFeed
+              ? '\u200b'
+              : disabled
+                ? <span style={{ color: colors.discord.yellow }}>Disabled ({selectedFeed.disabled})</span>
+                : hasFailed
+                  ? <span style={{ color: colors.discord.red }}>Failed ({moment(selectedFailRecord.failedAt).format('DD MMMM Y')})</span>
+                  : <div><span style={{ color: colors.discord.green }}>Normal ({calculateHealth(selectedFailRecord, botConfig.hoursUntilFail)}% health)</span></div>
+            }
+            <EditField>
+              <SectionSubtitle>Refresh Rate</SectionSubtitle>
+              { hasFailed || disabled ? 'None ' : !selectedFeed ? '\u200b' : !refreshRate ? 'Determining... ' : refreshRate < 1 ? `${refreshRate * 60} seconds      ` : `${refreshRate} minutes      `}{ selectedFailRecord ? null : <a href='https://www.patreon.com/discordrss' target='_blank' rel='noopener noreferrer'>－</a> }
+            </EditField>
+            <EditField>
+              <SectionSubtitle>Added At</SectionSubtitle>
+              { !selectedFeed ? '\u200b' : !selectedFeed.addedAt ? 'Unknown' : moment(selectedFeed.addedAt).locale(dateLanguage).tz(dateTimezone).format(dateFormat)}
+            </EditField>
+            <Divider />
+            <SectionTitle heading='Edit' />
+            <EditField>
+              <SectionSubtitle>Title</SectionSubtitle>
+              <Input value={inputTitle || (selectedFeed ? selectedFeed.title : '')} fluid disabled={!selectedFeed} onChange={e => {
+                setInputTitle(e.target.value)
+              }} />
+            </EditField>
+            <EditField>
+              <SectionSubtitle>Channel</SectionSubtitle>
+              <Dropdown value={inputChannel || (selectedFeed ? selectedFeed.channel : '')} options={channelDropdownOptions} disabled={!selectedFeed || channelDropdownOptions.length === 0} search={!isMobile} selection fluid onChange={(e, data) => {
+                setInputChannel(data.value)
+              }} />
+            </EditField>
+            <ApplyField>
+              <PopInButton basic inverted content='Reset' disabled={feedEditing || !differentFromDefault} pose={differentFromDefault ? 'enter' : 'exit'} onClick={e => {
+                setInputTitle('')
+                setInputChannel('')
+              }} />
+              <Button content='Save' color='green' disabled={!differentFromDefault} onClick={edit} />
+            </ApplyField>
+            <Divider />
+            <SectionTitle heading='Customize' subheading='So many options!' />
+            <EditField>
+              <Button fluid content='Message' icon='angle right' labelPosition='right' onClick={e => redirect(pages.MESSAGE)} loading={articlesFetching} disabled={articlesFetching} />
+            </EditField>
+            <EditField>
+              <Button fluid content='Filters' icon='angle right' labelPosition='right' onClick={e => redirect(pages.FILTERS)} loading={articlesFetching} disabled={articlesFetching} />
+            </EditField>
+            <EditField>
+              <Button fluid content='Subscribers' icon='angle right' labelPosition='right' onClick={e => redirect(pages.SUBSCRIBERS)} loading={articlesFetching} disabled={articlesFetching} />
+            </EditField>
+            <EditField>
+              <Button fluid content='Misc Options' icon='angle right' labelPosition='right' onClick={e => redirect(pages.MISC_OPTIONS)} loading={articlesFetching} disabled={articlesFetching} />
+            </EditField>
+            <Divider />
+            <SectionTitle heading='Remove' />
+            <EditField>
+              <Button fluid content='Remove' basic color='red' loading={feedRemoving} disabled={feedRemoving} onClick={remove} />
+            </EditField>
+          </PosedDiv>
+        </Content>
       </Scrollbars>
     </Container>
   )
 }
 
-export default SideBar
+SideBar.propTypes = {
+  selectedFeed: PropTypes.object,
+  redirect: PropTypes.func,
+  channelDropdownOptions: PropTypes.array
+}
 
+export default SideBar
