09ccf7994a15e1680265dab86685639ebc3ddaae
synzen <michael994664@gmail.com>
2020-02-11 00:02:03 -0500

    Remove socket status from TopBar, convert TopBar to func

--- src/web/client/src/js/components/ControlPanel/TopBar/index.js
+++ src/web/client/src/js/components/ControlPanel/TopBar/index.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import socketStatus from '../../../constants/socketStatus'
-import { Icon, Button, Popup } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import { darken } from 'polished'
-
-const mapStateToProps = state => {
-  return {
-    guilds: state.guilds,
-    feeds: state.feeds
-  }
-}
 
 const RouterLink = styled(Link)`
   display: flex;
@@ -35,11 +26,8 @@ const BrandTitleContainer = styled.div`
   background: #282b30;
   width: 100vw;
   z-index: 500;
-  /* justify-content: cen; */
   align-items: center;
   user-select: none;
-  justify-content: space-between;
-  /* cursor: pointer; */
   > a {
     display: flex;
     align-items: center;
@@ -67,72 +55,28 @@ const Logo = styled.img`
 const Title = styled.h2`
   color: ${darken(0.1, 'white')};
   margin: 0;
-  
-  /* font-weight: 300; */
 `
 
 const ExpandButton = styled(Button)`
   margin-right: 1em !important;
 `
 
-class App extends React.PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      hideExperimental: window.innerWidth < 500
-    }
-  }
-
-  componentWillMount () {
-    window.addEventListener("resize", this.updateDimensions)
-  }
-
-  componentWillUnmount = () => {
-    window.removeEventListener("resize", this.updateDimensions)
-  }
-
-  updateDimensions = () => {
-    if (!this.state.hideExperimental && window.innerWidth < 500) this.setState({ hideExperimental: true })
-    else if (this.state.hideExperimental && window.innerWidth >= 500) this.setState({ hideExperimental: false })
-  }
-
-  render () {
-    return (
-      <BrandTitleContainer hideExpandButton={this.props.hideExpandButton}>
-      
-        <div>
-          <ExpandButton className='expand-left-menu-btn' icon='list' basic onClick={this.props.toggleLeftMenu} />
-          {/* {this.props.hideExpandButton ? null : } */}
-          <RouterLink to='/'>
-            <Logo src='https://discordapp.com/assets/d36b33903dafb0107bb067b55bdd9cbc.svg' />
-            <Title>Discord.RSS</Title>
-          </RouterLink>
-        </div>
-        <div>
-          {/* {this.state.hideExperimental
-          ? null
-          : <Popup
-            trigger={<h3 style={{ margin: 0, paddingRight: '1em' }}>EXPERIMENTAL</h3>}
-            inverted
-            content='This is a highly experimental UI only for patron use'
-          />
-          } */}
-          {
-            this.props.socketStatus === socketStatus.CONNECTED
-              ? <Popup trigger={<Icon name={'check circle outline'} size='large' color='green' />} content='Server is connected, and all changes are bidirectionally live' position='bottom left' inverted/>
-              : this.props.socketStatus === socketStatus.DISCONNECTED ? <Popup trigger={<Icon name={'x'} size='large' color='red' />} content='Server is disconnected. No changes will be saved!' position='bottom left' inverted/>
-                : <Popup trigger={<Icon name={'question circle outline'} size='large' color='grey' />} content='Attempting to connect to server...' position='bottom left' inverted/>
-          }
-        </div>
-      </BrandTitleContainer>
-    )
-  }
+function TopBar (props) {
+  const { hideExpandButton, toggleLeftMenu } = props
+  return (
+    <BrandTitleContainer hideExpandButton={hideExpandButton}>
+      <ExpandButton className='expand-left-menu-btn' icon='list' basic onClick={toggleLeftMenu} />
+      <RouterLink to='/'>
+        <Logo src='https://discordapp.com/assets/d36b33903dafb0107bb067b55bdd9cbc.svg' />
+        <Title>Discord.RSS</Title>
+      </RouterLink>
+    </BrandTitleContainer>
+  )
 }
 
-App.propTypes = {
+TopBar.propTypes = {
   hideExpandButton: PropTypes.bool,
-  toggleLeftMenu: PropTypes.func,
-  socketStatus: PropTypes.number
+  toggleLeftMenu: PropTypes.func
 }
 
-export default connect(mapStateToProps)(App)
+export default TopBar
