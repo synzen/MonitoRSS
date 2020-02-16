da7414a5e6e476247756bfbf2066c62932feabdd
synzen <michael994664@gmail.com>
2020-02-15 21:39:54 -0500

    Add User-Agent headers to HTTP API requests

--- src/web/constants/discordAPIHeaders.js
+++ src/web/constants/discordAPIHeaders.js
@@ -1,9 +1,24 @@
 const config = require('../../config.js')
+const package = require('../../../package.json')
 
-exports.user = accessToken => {
-  return {
-    headers: { 'Authorization': `Bearer ${accessToken}` }
+exports.common = {
+  headers: {
+    'User-Agent': `DiscordBot (${package.name}, ${package.version}) Node.js/${process.version}`
   }
 }
 
-exports.bot = { headers: { 'Authorization': `Bot ${config.bot.token}` } }
+exports.user = accessToken => {
+  return {
+    headers: {
+      ...exports.common.headers,
+      'Authorization': `Bearer ${accessToken}`
+    }
+  }
+}
+
+exports.bot = {
+  headers: {
+    ...exports.common.headers,
+    'Authorization': `Bot ${config.bot.token}`
+  }
+}
