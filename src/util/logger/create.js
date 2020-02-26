c1b413c89c78c2b74ffff1708521b62ad8add164
synzen <michael994664@gmail.com>
2020-02-26 10:33:39 -0500

    Add pino-pretty, replace logger/log with /create

--- src/util/logger/create.js
+++ src/util/logger/create.js
@@ -1,23 +1,24 @@
 const pino = require('pino')
 const serializers = require('./serializers.js')
 
-const log = pino({
-  base: {
-      shardID: process.env.SHARD_ID
-  },
-  prettyPrint: {
+function createLogger (shardID) {
+  return pino({
+    base: {
+      shardID: String(shardID)
+    },
+    prettyPrint: {
       translateTime: 'yyyy-mm-dd HH:MM:ss',
       messageFormat: `[{shardID}] {msg}`,
       ignore: 'hostname,shardID'
-  },
-  serializers: {
+    },
+    serializers: {
       guild: serializers.guild,
       textChannel: serializers.textChannel,
       role: serializers.textChannel,
       user: serializers.user
-  },
-  enabled: !process.env.TEST_ENV
-})
+    },
+    enabled: !process.env.TEST_ENV
+  })
+}
 
-
-module.exports = log
+module.exports = createLogger
