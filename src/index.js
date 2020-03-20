3e114e4b4af509ae9e2420404a3ae710d0a54576
synzen <michael994664@gmail.com>
2020-03-19 20:02:32 -0400

    Export v6 migration function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const Client = require('./structs/Client.js')
 const ClientManager = require('./structs/ClientManager.js')
+const v6 = require('../scripts/pre_v6.js')
 
 exports.Client = Client
 exports.ClientManager = ClientManager
+exports.migrations = {
+  v6
+}
