fc15f637fd53b89229125f4ca27e4eaf7d9d215d
synzen <michael994664@gmail.com>
2020-03-19 21:25:08 -0400

    Export validateConfig function in index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Client = require('./src/structs/Client.js')
 const ClientManager = require('./src/structs/ClientManager.js')
+const validateConfig = require('./src/util/config/schema').validate
+
 const v6 = require('./scripts/pre_v6.js')
 
 exports.Client = Client
@@ -7,3 +9,4 @@ exports.ClientManager = ClientManager
 exports.migrations = {
   v6
 }
+exports.validateConfig = validateConfig
