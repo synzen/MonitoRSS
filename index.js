b57d634f397fb7e13cf9ffcae7b79988e5d7f8e1
synzen <michael994664@gmail.com>
2020-03-20 12:11:40 -0400

    Export most structs/models in index

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-const Client = require('./src/structs/Client.js')
-const ClientManager = require('./src/structs/ClientManager.js')
-const validateConfig = require('./src/util/config/schema').validate
-
-const v6 = require('./scripts/pre_v6.js')
-
-exports.Client = Client
-exports.ClientManager = ClientManager
-exports.migrations = {
-  v6
+// Models
+exports.models = {
+  Article: require('./src/models/Article.js'),
+  Blacklist: require('./src/models/Blacklist.js'),
+  FailRecord: require('./src/models/FailRecord.js'),
+  Feed: require('./src/models/Feed.js'),
+  Feedback: require('./src/models/Feedback.js'),
+  FilteredFormat: require('./src/models/FilteredFormat.js'),
+  Patron: require('./src/models/Patron.js'),
+  Profile: require('./src/models/Profile.js'),
+  Rating: require('./src/models/Rating.js'),
+  Schedule: require('./src/models/Schedule.js'),
+  ScheduleStats: require('./src/models/ScheduleStats.js'),
+  Subscriber: require('./src/models/Subscriber.js'),
+  Supporter: require('./src/models/Supporter.js')
 }
-exports.validateConfig = validateConfig
+
+// Structures
+exports.Article = require('./src/structs/Article.js')
+exports.Client = require('./src/structs/Client.js')
+exports.ClientManager = require('./src/structs/ClientManager.js')
+exports.FeedData = require('./src/structs/FeedData.js')
+
+// Database Structures
+exports.Blacklist = require('./src/structs/db/Blacklist.js')
+exports.FailRecord = require('./src/structs/db/FailRecord.js')
+exports.Feed = require('./src/structs/db/Feed.js')
+exports.FilteredFormat = require('./src/structs/db/FilteredFormat.js')
+exports.Patron = require('./src/structs/db/Patron.js')
+exports.Profile = require('./src/structs/db/Profile.js')
+exports.Schedule = require('./src/structs/db/Schedule.js')
+exports.ScheduleStats = require('./src/structs/db/ScheduleStats.js')
+exports.Subscriber = require('./src/structs/db/Subscriber.js')
+exports.Supporter = require('./src/structs/db/Supporter.js')
+
+// Utils
+exports.FeedFetcher = require('./src/util/FeedFetcher.js')
+exports.validateConfig = require('./src/util/config/schema').validate
+exports.migrations = {
+  v6: require('./scripts/pre_v6.js')
+}
+
+// Errors
+exports.errors = {
+  FeedParserError: require('./src/structs/errors/FeedParserError.js'),
+  RequestError: require('./src/structs/errors/RequestError.js')
+}
