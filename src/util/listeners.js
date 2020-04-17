712d02a0c2b39c96b03c635834d55f69f827bf02
synzen <michael994664@gmail.com>
2020-04-16 22:00:22 -0400

    Simplify listeners.js

--- src/util/listeners.js
+++ src/util/listeners.js
@@ -1,77 +1,25 @@
 const fs = require('fs')
 const path = require('path')
 const Command = require('../structs/Command.js')
-
+const messageHandler = require('../events/message.js')
 const eventHandlers = []
-const VALID_EVENTS = [
-  'channelCreate',
-  'channelDelete',
-  'channelPinsUpdate',
-  'channelUpdate',
-  'clientUserGuildSettingsUpdate',
-  'clientUserSettingsUpdate',
-  'debug',
-  'disconnect',
-  'emojiCreate',
-  'emojiDelete',
-  'emojiUpdate',
-  'error',
-  'guildBanAdd',
-  'guildBanRemove',
-  'guildCreate',
-  'guildDelete',
-  'guildMemberAdd',
-  'guildMemberAvailable',
-  'guildMemberRemove',
-  'guildMembersChunk',
-  'guildMemberSpeaking',
-  'guildMemberUpdate',
-  'guildUnavailable',
-  'guildUpdate',
-  'message',
-  'messageDelete',
-  'messageDeleteBulk',
-  'messageReactionAdd',
-  'messageReactionRemove',
-  'messageReactionRemoveAll',
-  'messageUpdate',
-  'presenceUpdate',
-  'rateLimit',
-  'ready',
-  'reconnecting',
-  'resume',
-  'roleCreate',
-  'roleDelete',
-  'roleUpdate',
-  'typingStart',
-  'typingStop',
-  'userNoteUpdate',
-  'userUpdate',
-  'voiceStateUpdate',
-  'warn'
-]
-
-const messageHandler = message => {
-  require('../events/message.js')(message)
-}
 
 exports.createManagers = (bot) => {
   const fileNames = fs.readdirSync(path.join(__dirname, '..', 'events'))
   for (const fileName of fileNames) {
     const eventName = fileName.replace('.js', '')
-    if (!VALID_EVENTS.includes(eventName)) {
-      throw new Error('Invalid event file found:', fileName)
+    if (eventName === 'message') {
+      continue
     }
-    if (eventName === 'message') continue
     const eventHandler = require(`../events/${fileName}`)
     eventHandlers.push({ name: eventName, func: eventHandler })
-    bot.on(eventName, eventHandlers[eventHandlers.length - 1].func)
+    bot.on(eventName, eventHandler)
   }
 }
 
 exports.enableCommands = async (bot) => {
   eventHandlers.push({ name: 'message', func: messageHandler })
-  bot.on('message', eventHandlers[eventHandlers.length - 1].func)
+  bot.on('message', messageHandler)
   Command.enable()
 }
 
