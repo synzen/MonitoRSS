1858e43a1be461a664a610d1c7243b69f9e83e27
synzen <michael994664@gmail.com>
2020-05-05 13:30:36 -0400

    Check for invalid encodings

--- src/structs/DecodedFeedParser.js
+++ src/structs/DecodedFeedParser.js
@@ -12,7 +12,7 @@ class DecodedFeedParser extends FeedParser {
   _transform (chunk, encoding, done) {
     const config = getConfig()
     const charset = config.feeds.decode[this.url] || this.charset
-    if (/utf-*8/i.test(charset) || !charset) {
+    if (/utf-*8/i.test(charset) || !charset || !iconv.encodingExists(charset)) {
       this.stream.write(chunk)
     } else {
       // Assumes that the encoding specified is valid, and will not check via iconv.encodingExists()
