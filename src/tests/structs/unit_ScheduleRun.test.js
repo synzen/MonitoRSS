28b962de1ab7cbe0c837435a97542f66a7905948
synzen <michael994664@gmail.com>
2020-05-23 11:28:18 -0400

    Fix tests

--- src/tests/structs/unit_ScheduleRun.test.js
+++ src/tests/structs/unit_ScheduleRun.test.js
@@ -233,106 +233,94 @@ describe('Unit::structs/ScheduleRun', function () {
       }])
     })
   })
-  describe('creaetBatchGroups', function () {
-    it('returns the batch groups', function () {
-      const run = new ScheduleRun(basicSchedule)
-      const batches = [{
-        a: {},
-        b: {}
-      }, {
-        c: {},
-        d: {}
-      }, {
-        e: {}
-      }]
-      const groupSize = 2
-      const returned = run.createBatchGroups(batches, groupSize)
-      expect(returned).toEqual([
-        [batches[0], batches[1]],
-        [batches[2]]
-      ])
-    })
-  })
   describe('createURLRecords', function () {
     it('creates the records correctly', function () {
       const run = new ScheduleRun(basicSchedule)
-      const batchGroups = [
-        [{
+      const batches = [
+        {
           url1: {},
           url2: {}
         }, {
           url3: {},
           url4: {}
-        }], [{
+        }, {
           url5: {},
           url6: {}
         }, {
           url7: {}
-        }]
+        }
       ]
-      run.createURLRecords(batchGroups)
-      expect(run.urlBatchGroups).toEqual([
-        [new Set(['url1', 'url2']), new Set(['url3', 'url4'])],
-        [new Set(['url5', 'url6']), new Set(['url7'])]
+      run.createURLRecords(batches)
+      expect(run.urlBatchRecords).toEqual([
+        new Set(['url1', 'url2']),
+        new Set(['url3', 'url4']),
+        new Set(['url5', 'url6']),
+        new Set(['url7'])
       ])
     })
     it('creates counts correctly', function () {
       const run = new ScheduleRun(basicSchedule)
       const batchGroups = [
-        [{
+        {
           url1: {},
           url2: {}
         }, {
           url3: {},
           url4: {}
-        }], [{
+        }, {
           url5: {},
           url6: {}
         }, {
           url7: {}
-        }]
+        }
       ]
       run.createURLRecords(batchGroups)
-      expect(run.urlSizeGroups).toEqual([
-        [2, 2],
-        [2, 1]
+      expect(run.urlSizeRecords).toEqual([
+        2, 2, 2, 1
       ])
     })
   })
   describe('removeFromBatchRecords', function () {
     it('removes correctly', function () {
       const run = new ScheduleRun(basicSchedule)
-      run.urlBatchGroups = [
-        [new Set(['url1', 'url2']), new Set(['url3', 'url4'])],
-        [new Set(['url5', 'url6']), new Set(['url7'])]
+      run.urlBatchRecords = [
+        new Set(['url1', 'url2']),
+        new Set(['url3', 'url4']),
+        new Set(['url5', 'url6']),
+        new Set(['url7'])
       ]
-      run.removeFromBatchRecords(1, 1, 'url7')
-      expect(run.urlBatchGroups).toEqual([
-        [new Set(['url1', 'url2']), new Set(['url3', 'url4'])],
-        [new Set(['url5', 'url6']), new Set()]
+      run.removeFromBatchRecords(3, 'url7')
+      expect(run.urlBatchRecords).toEqual([
+        new Set(['url1', 'url2']),
+        new Set(['url3', 'url4']),
+        new Set(['url5', 'url6']),
+        new Set()
       ])
-      run.removeFromBatchRecords(0, 0, 'url2')
-      expect(run.urlBatchGroups).toEqual([
-        [new Set(['url1']), new Set(['url3', 'url4'])],
-        [new Set(['url5', 'url6']), new Set()]
+      run.removeFromBatchRecords(1, 'url3')
+      expect(run.urlBatchRecords).toEqual([
+        new Set(['url1', 'url2']),
+        new Set(['url4']),
+        new Set(['url5', 'url6']),
+        new Set()
       ])
     })
   })
   describe('getHungUpURLs', function () {
     it('returns correctly for mid-progress batches', function () {
       const run = new ScheduleRun(basicSchedule)
-      run.urlBatchGroups = [
-        [new Set(['url1', 'url2']), new Set(['url3'])],
-        [new Set(['url5']), new Set(['url7'])]
+      run.urlBatchRecords = [
+        new Set(['url1', 'url2']),
+        new Set(['url3']),
+        new Set(['url5']),
+        new Set(['url7'])
       ]
-      run.urlSizeGroups = [
-        [2, 2],
-        [2, 1]
+      run.urlSizeRecords = [
+        2, 2, 2, 1
       ]
       expect(run.getHungUpURLs()).toEqual({
         summary: [
-          [['url3']],
-          [['url5']]
+          ['url3'],
+          ['url5']
         ],
         total: 5
       })
