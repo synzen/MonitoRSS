797fe70097f307576d6e918d900374d825905cbd
synzen <michael994664@gmail.com>
2020-05-24 17:09:51 -0400

    Fix broken menu commands when no feeds

--- src/commands/prompts/common/noFeedsFound.js
+++ src/commands/prompts/common/noFeedsFound.js
@@ -1,6 +1,6 @@
 const LocalizedPrompt = require('./utils/LocalizedPrompt.js')
 const Translator = require('../../../structs/Translator.js')
-
+const { MessageVisual } = require('discord.js-prompts')
 /**
  * @typedef {Object} Data
  * @property {import('../../../structs/db/Profile.js')} [profile]
@@ -12,9 +12,7 @@ const Translator = require('../../../structs/Translator.js')
  */
 function noFeedsFoundVisual (data) {
   const { locale } = data.profile || {}
-  return {
-    text: Translator.translate('structs.FeedSelector.noFeeds', locale)
-  }
+  return new MessageVisual(Translator.translate('structs.FeedSelector.noFeeds', locale))
 }
 
 const prompt = new LocalizedPrompt(noFeedsFoundVisual)
