ef58b7f0c614a6b9ee31280e5faee0af720523fc
synzen <michael994664@gmail.com>
2020-06-01 12:04:15 -0400

    Update packages

--- src/commands/prompts/common/utils/LocalizedPrompt.js
+++ src/commands/prompts/common/utils/LocalizedPrompt.js
@@ -1,19 +1,7 @@
-const { DiscordPrompt, MessageVisual, Rejection } = require('discord.js-prompts')
+const { DiscordPrompt, Rejection } = require('discord.js-prompts')
 const Translator = require('../../../../structs/Translator.js')
 
 class LocalizedPrompt extends DiscordPrompt {
-  static async getExitVisual (message, discordChannel, data) {
-    const { profile } = data
-    const translate = Translator.createProfileTranslator(profile)
-    return new MessageVisual(translate('structs.MenuUtils.closed'))
-  }
-
-  static async getInactivityVisual (discordChannel, data) {
-    const { profile } = data
-    const translate = Translator.createProfileTranslator(profile)
-    return new MessageVisual(translate('structs.MenuUtils.closedInactivity'))
-  }
-
   static createMenuRejection (message, data) {
     const { profile } = data
     const translate = Translator.createProfileTranslator(profile)
--- src/commands/prompts/runner/run.js
+++ src/commands/prompts/runner/run.js
@@ -1,6 +1,7 @@
-const { PromptNode, DiscordPromptRunner, DiscordChannel } = require('discord.js-prompts')
+const { PromptNode, DiscordPromptRunner, DiscordChannel, Errors } = require('discord.js-prompts')
 const Profile = require('../../../structs/db/Profile.js')
 const Feed = require('../../../structs/db/Feed.js')
+const Translator = require('../../../structs/Translator.js')
 const noFeeds = require('../common/noFeedsFound.js')
 const deleteMessages = require('./util/deleteMessages.js')
 
@@ -29,10 +30,22 @@ async function runWithFeedsProfile (rootNode, message, inputData = {}) {
   const noFeedsNode = new PromptNode(noFeeds.prompt, data => data.feeds.length === 0)
   const runner = new DiscordPromptRunner(author, initialData)
   const channelStore = new DiscordChannel(channel)
-  const finalData = await runner.runArray([
-    noFeedsNode,
-    rootNode
-  ], channelStore)
+  let finalData
+  try {
+    finalData = await runner.runArray([
+      noFeedsNode,
+      rootNode
+    ], channelStore)
+  } catch (err) {
+    const translate = Translator.createProfileTranslator(initialData.profile)
+    if (err instanceof Errors.UserInactivityError) {
+      await message.channel.send(translate('structs.MenuUtils.closedInactivity'))
+    } else if (err instanceof Errors.UserVoluntaryExitError) {
+      await message.channel.send(translate('structs.MenuUtils.closed'))
+    } else {
+      throw err
+    }
+  }
   await deleteMessages(channel, channelStore.messages)
   return finalData
 }
