e24dd67b554e8342ce887ee33200a6be7b3497f1
synzen <michael994664@gmail.com>
2020-06-06 16:40:15 -0400

    Add version schema to GeneralStats

--- src/models/GeneralStats.js
+++ src/models/GeneralStats.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Version = require('./common/Version.js')
 
 const schema = new mongoose.Schema({
   _id: {
@@ -7,6 +8,8 @@ const schema = new mongoose.Schema({
   },
   data: mongoose.Schema.Types.Mixed
 })
+
+schema.add(Version)
 
 exports.schema = schema
 /** @type {import('mongoose').Model} */
