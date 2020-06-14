828b46fb0ec698794259a9d0815cc189bee4a360
synzen <michael994664@gmail.com>
2020-06-13 22:56:05 -0400

    Add feedURL to DeliveryRecord

--- src/models/DeliveryRecord.js
+++ src/models/DeliveryRecord.js
@@ -6,6 +6,10 @@ const schema = new mongoose.Schema({
     type: String,
     required: true
   },
+  feedURL: {
+    type: String,
+    required: true
+  },
   channel: {
     type: String,
     required: true
