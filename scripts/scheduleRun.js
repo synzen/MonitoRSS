1de0c4eebb524f4ca320cfd6a97c1807c267b469
synzen <michael994664@gmail.com>
2021-05-11 22:37:10 -0400

    Make testRuns an optional config in schedule run script

--- scripts/scheduleRun.js
+++ scripts/scheduleRun.js
@@ -3,23 +3,20 @@ const connectDatabase = require('../src/util/connectDatabase.js')
 const ScheduleManager = require('../src/structs/ScheduleManager.js')
 const setConfig = require('../src/config.js').set
 
-async function testScheduleRun (userConfig) {
+async function testScheduleRun (userConfig, runSettings) {
   let config = setConfig(userConfig)
   config = setConfig({
-    ...config,
-    log: {
-      ...config.log,
-      level: 'debug'
-    }
+    ...config
   })
   const con = await connectDatabase(config.database.uri, config.database.connection)
   console.log('Connected to database')
   await initialize.setupModels(con)
   const schedules = await initialize.populateSchedules()
   const scheduleManager = new ScheduleManager()
-  scheduleManager.testRuns = true
+  scheduleManager.testRuns = runSettings.testRuns
   scheduleManager.addSchedules(schedules)
   scheduleManager.beginTimers()
+  return scheduleManager
 }
 
 module.exports = testScheduleRun
