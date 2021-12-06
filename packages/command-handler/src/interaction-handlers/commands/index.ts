586de753aa79b3ebacf342b4378d5fab9adab7a9
gintil <searcher64@gmail.com>
2021-12-05 20:35:19 -0500

    feat(command-handler): Add "locale" slash command

--- packages/command-handler/src/interaction-handlers/commands/index.ts
+++ packages/command-handler/src/interaction-handlers/commands/index.ts
@@ -1,6 +1,7 @@
 import CommandAdd from './add';
 import CommandInterface from './command.interface';
 import CommandInvite from './invite';
+import CommandLocale from './locale';
 import CommandPing from './ping';
 import CommandRemove from './remove';
 import CommandVersion from './version';
@@ -11,6 +12,7 @@ const mapOfCommands = new Map<string, new () => CommandInterface>([
   [CommandRemove.data.name, CommandRemove],
   [CommandVersion.data.name, CommandVersion],
   [CommandInvite.data.name, CommandInvite],
+  [CommandLocale.data.name, CommandLocale],
 ]);
 
 export default mapOfCommands;
