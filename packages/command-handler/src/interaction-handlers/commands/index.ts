3ebde0dfa037735db984f48826ddb204266eacd6
gintil <searcher64@gmail.com>
2021-12-05 19:35:39 -0500

    feat(command-handler): Add version slash command

--- packages/command-handler/src/interaction-handlers/commands/index.ts
+++ packages/command-handler/src/interaction-handlers/commands/index.ts
@@ -2,11 +2,13 @@ import CommandAdd from './add';
 import CommandInterface from './command.interface';
 import CommandPing from './ping';
 import CommandRemove from './remove';
+import CommandVersion from './version';
 
 const mapOfCommands = new Map<string, new () => CommandInterface>([
   [CommandPing.data.name, CommandPing],
   [CommandAdd.data.name, CommandAdd],
   [CommandRemove.data.name, CommandRemove],
+  [CommandVersion.data.name, CommandVersion],
 ]);
 
 export default mapOfCommands;
