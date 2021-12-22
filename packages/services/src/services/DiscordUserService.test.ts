8693f17f8377962532430a510d1c233e8dac348f
gintil <searcher64@gmail.com>
2021-12-22 12:27:36 -0500

    chore(services): Add DiscordUserService Used for connecting logic between supporters and patrons

--- packages/services/src/services/DiscordUserService.test.ts
+++ packages/services/src/services/DiscordUserService.test.ts
@@ -8,6 +8,7 @@ describe('DiscordUserService', () => {
   };
   let patronService = {
     findByDiscordId: jest.fn(),
+    getGuildLimitFromDiscordId: jest.fn(),
   };
 
   beforeEach(() => {
@@ -24,6 +25,9 @@ describe('DiscordUserService', () => {
       expect(result).toBe(true);
     });
     it('returns true if the user is a active patron', async () => {
+      supporterService.findByDiscordId.mockResolvedValue({
+        patron: true,
+      } as any);
       patronService.findByDiscordId.mockResolvedValue({} as any);
 
       const result = await service.isSupporter('test');
@@ -34,6 +38,69 @@ describe('DiscordUserService', () => {
       const result = await service.isSupporter('test');
 
       expect(result).toBe(false);
+    });
+    it('returns false if the supporter is a patron but there is no valid patron', async () => {
+      supporterService.findByDiscordId.mockResolvedValue({
+        patron: true,
+      });
+
+      const result = await service.isSupporter('test');
+
+      expect(result).toBe(false);
+    });
+  });
+  describe('getSupporterGuilds', () => {
+    it('returns the guilds that the user has backed', async () => {
+      supporterService.findByDiscordId.mockResolvedValue({ guilds: ['test'] } as any);
+
+      const result = await service.getSupporterGuilds('test');
+
+      expect(result).toEqual(['test']);
+    });
+
+    it('returns an empty array if the user has no backed guilds', async () => {
+      supporterService.findByDiscordId.mockResolvedValue({} as any);
+
+      const result = await service.getSupporterGuilds('test');
+
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('getMaxSupporterGuildCount', () => {
+    describe('when supporter is not found', () => {
+      it('returns 1', async () => {
+        supporterService.findByDiscordId.mockResolvedValue(null);
+
+        const result = await service.getMaxSupporterGuildCount('test');
+
+        expect(result).toEqual(1);
+      });
+    });
+    describe('when supporter is found', () => {
+      describe('when supporter is a patron', () => {
+        it('returns the max guilds from patron service', async () => {
+          supporterService.findByDiscordId.mockResolvedValue({
+            patron: true,
+          } as any);
+          patronService.getGuildLimitFromDiscordId.mockResolvedValue(2);
+
+          const result = await service.getMaxSupporterGuildCount('test');
+
+          expect(result).toEqual(2);
+        });
+      });
+      describe('when supporter is not a patron', () => {
+        it('returns 1', async () => {
+          supporterService.findByDiscordId.mockResolvedValue({
+            patron: false,
+          } as any);
+
+          const result = await service.getMaxSupporterGuildCount('test');
+
+          expect(result).toEqual(1);
+        });
+      });
     });
   });
 });
--- packages/services/src/services/DiscordUserService.ts
+++ packages/services/src/services/DiscordUserService.ts
@@ -18,7 +18,12 @@ export default class DiscordUserService {
   async isSupporter(discordUserId: string): Promise<boolean> {
     const supporter = await this.supporterService.findByDiscordId(discordUserId);
 
-    if (supporter) {
+    if (!supporter) {
+      return false;
+    }
+
+    // If the supporter is a patron, we must check the patron reference
+    if (!supporter.patron) {
       return true;
     }
 
@@ -26,4 +31,57 @@ export default class DiscordUserService {
     
     return !!patron;
   }
+
+  /**
+   * Get the server IDs that this user has backed.
+   *
+   * @param discordUserId The ID of the Discord user.
+   * @returns 
+   */
+  async getSupporterGuilds(discordUserId: string) {
+    const supporter = await this.supporterService.findByDiscordId(discordUserId);
+    
+    return supporter?.guilds || [];
+  }
+
+  /**
+   * Add a server to the user's supporter list.
+   *
+   * @param discordUserId The ID of the Discord user.
+   * @param guildId The ID of the server.
+   */
+  async addSupporterGuild(discordUserId: string, guildId: string) {
+    await this.supporterService.addGuildToSupporter(discordUserId, guildId);
+  }
+
+  /**
+   * Remove a server from the user's supporter list.
+   * 
+   * @param discordUserId The ID of the Discord user.
+   * @param guildId The ID of the server.
+   */
+  async removeSupporterGuild(discordUserId: string, guildId: string) {
+    await this.supporterService.removeGuildFromSupporter(discordUserId, guildId);
+  }
+
+  /**
+   * Get the maximum number of guilds that the given Discord user can have backed.
+   *
+   * @param discordUserId The ID of the Discord user.
+   * @returns The maximum number of guilds that the user can have backed.
+   */
+  async getMaxSupporterGuildCount(discordUserId: string) {
+    const supporter = await this.supporterService.findByDiscordId(discordUserId);
+
+    if (!supporter) {
+      return 1;
+    }
+
+    if (!supporter.patron) {
+      return supporter.maxGuilds || 1;
+    }
+
+    return this.patronService.getGuildLimitFromDiscordId(discordUserId);
+  }
+  
 }
