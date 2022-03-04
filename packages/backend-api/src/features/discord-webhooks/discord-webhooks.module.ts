596e07f7ec5ccd329274e834838e25f1bbedf938
synzeno <michael994664@gmail.com>
2022-03-04 09:43:07 -0500

    Remove unneeded dependency

--- packages/backend-api/src/features/discord-webhooks/discord-webhooks.module.ts
+++ packages/backend-api/src/features/discord-webhooks/discord-webhooks.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DiscordApiModule } from '../../services/apis/discord/discord-api.module';
 import { DiscordAuthModule } from '../discord-auth/discord-auth.module';
-import { DiscordUserModule } from '../discord-users/discord-users.module';
 import { DiscordWebhooksController } from './discord-webhooks.controller';
 import { DiscordWebhooksService } from './discord-webhooks.service';
 
 @Module({
   providers: [DiscordWebhooksService],
   controllers: [DiscordWebhooksController],
-  imports: [DiscordApiModule, DiscordAuthModule, DiscordUserModule],
+  imports: [DiscordApiModule, DiscordAuthModule],
   exports: [DiscordWebhooksService],
 })
 export class DiscordWebhooksModule {}
