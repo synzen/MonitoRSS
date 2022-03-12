71d8b32f3205660c27152988e75d2325f5c40a1c
synzeno <michael994664@gmail.com>
2022-03-12 12:05:42 -0500

    Extract filters table to separate component for reuse

--- packages/control-panel/src/pages/FeedFilters.tsx
+++ packages/control-panel/src/pages/FeedFilters.tsx
@@ -1,30 +1,15 @@
 /* eslint-disable react/no-unstable-nested-components */
 import {
-  Box,
-  Button,
-  Checkbox,
-  Flex,
-  Heading,
   Stack,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
 } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
 import { useMemo } from 'react';
-import {
-  Column, useRowSelect, useSortBy, useTable,
-} from 'react-table';
-import { ArrowDownIcon, ArrowUpIcon } from '@chakra-ui/icons';
+
 import { DashboardContent } from '@/components';
 import RouteParams from '../types/RouteParams';
 import { useFeed } from '@/features/feed';
-import { AddFilterDialog } from '@/features/feed/components/AddFilterDialog';
 import { useUpdateFeed } from '@/features/feed/hooks/useUpdateFeed';
+import { FiltersTable } from '@/features/feed/components/FiltersTable';
 
 const FeedFilters: React.FC = () => {
   const { feedId } = useParams<RouteParams>();
@@ -33,7 +18,6 @@ const FeedFilters: React.FC = () => {
   } = useFeed({
     feedId,
   });
-  const { t } = useTranslation();
   const {
     mutateAsync,
     status: updatingStatus,
@@ -50,106 +34,15 @@ const FeedFilters: React.FC = () => {
     }));
   }, [feed]);
 
-  const columns = useMemo<Column<{ category: string, value: string }>[]>(() => [{
-    Header: t('pages.filters.tableCategory') as string,
-    accessor: 'category',
-  }, {
-    Header: t('pages.filters.tableValue') as string,
-    accessor: 'value',
-  }], []);
-
-  const tableInstance = useTable(
-    {
-      columns,
-      data: tableData,
-    },
-    useSortBy,
-    useRowSelect,
-    (hooks) => {
-      hooks.visibleColumns.push((cols) => [
-        // Let's make a column for selection
-        {
-          id: 'selection',
-          // The header can use the table's getToggleAllRowsSelectedProps method
-          // to render a checkbox
-          Header: ({ getToggleAllRowsSelectedProps }) => {
-            const {
-              indeterminate, title, checked, onChange,
-            } = getToggleAllRowsSelectedProps();
-
-            return (
-              <Checkbox
-                isIndeterminate={indeterminate}
-                title={title}
-                isChecked={checked}
-                onChange={onChange}
-              />
-            );
-          },
-          // The cell can use the individual row's getToggleRowSelectedProps method
-          // to the render a checkbox
-          // @ts-ignore
-          Cell: ({ row }) => {
-            const {
-              indeterminate, title, checked, onChange,
-            } = row.getToggleRowSelectedProps();
-
-            return (
-              <Checkbox
-                isIndeterminate={indeterminate}
-                title={title}
-                isChecked={checked}
-                onChange={onChange}
-              />
-            );
-          },
-        },
-        ...cols,
-      ]);
-    },
-  );
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    prepareRow,
-    rows,
-    selectedFlatRows,
-  } = tableInstance;
-
-  const onAddFilters = async (data: Array<{ category: string, value: string }>) => {
-    if (!feedId || !feed) {
+  const onFiltersChanged = async (filters: Array<{ category: string, value: string }>) => {
+    if (!feedId) {
       return;
     }
 
     await mutateAsync({
       feedId,
       details: {
-        filters: [
-          ...feed.filters,
-          ...data,
-        ],
-      },
-    });
-  };
-
-  const onRemoveFilters = async () => {
-    if (!feedId || !feed) {
-      return;
-    }
-
-    const newFiltersState = [...feed.filters];
-    const selectedIndexes = selectedFlatRows.map((row) => row.index).sort();
-
-    for (let i = selectedIndexes.length - 1; i >= 0; i -= 1) {
-      newFiltersState.splice(selectedIndexes[i], 1);
-    }
-
-    await mutateAsync({
-      feedId,
-      details: {
-        filters: newFiltersState,
+        filters,
       },
     });
   };
@@ -160,78 +53,12 @@ const FeedFilters: React.FC = () => {
         error={error}
         loading={status === 'loading' || status === 'idle'}
       >
-        <Stack spacing={6}>
-          <Flex justifyContent="space-between">
-            <Heading
-              size="lg"
-              marginRight={4}
-            >
-              {t('pages.filters.title')}
-            </Heading>
-            <AddFilterDialog onSubmit={onAddFilters} />
-          </Flex>
-          <Stack>
-            <Box>
-              <Button
-                disabled={selectedFlatRows.length === 0 || updatingStatus === 'loading'}
-                isLoading={updatingStatus === 'loading'}
-                colorScheme="red"
-                variant="outline"
-                onClick={onRemoveFilters}
-              >
-                {t('pages.filters.removeSelectedFilters')}
-              </Button>
-            </Box>
-            <Table
-              {...getTableProps()}
-              whiteSpace="nowrap"
-              marginBottom="5"
-              background="gray.850"
-              borderColor="gray.700"
-              borderWidth="2px"
-              boxShadow="lg"
-            >
-              <Thead>
-                {headerGroups.map((headerGroup) => (
-                  <Tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map((column) => (
-                      <Th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                        <Flex alignItems="center" userSelect="none">
-                          {column.render('Header')}
-                          {column.isSorted && column.isSortedDesc && (
-                          <ArrowDownIcon
-                            marginLeft="1"
-                          />
-                          )}
-                          {column.isSorted && !column.isSortedDesc && (
-                          <ArrowUpIcon
-                            marginLeft="1"
-                          />
-                          )}
-                        </Flex>
-                      </Th>
-                    ))}
-                  </Tr>
-                ))}
-              </Thead>
-              <Tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
-                  prepareRow(row);
-
-                  return (
-                    <Tr {...row.getRowProps()}>
-                      {row.cells.map((cell) => (
-                        <Td {...cell.getCellProps()}>
-                          {cell.render('Cell')}
-                        </Td>
-                      ))}
-                    </Tr>
-                  );
-                })}
-              </Tbody>
-            </Table>
-          </Stack>
-        </Stack>
+        <FiltersTable
+          data={tableData}
+          onFiltersChanged={onFiltersChanged}
+          isUpdating={updatingStatus === 'loading'}
+          isLoading={status === 'loading' || status === 'idle'}
+        />
       </DashboardContent>
     </Stack>
   );
