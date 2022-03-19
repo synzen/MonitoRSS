55465dffaaed4cc9236f64ae8bf70c6ea6304fc1
synzen <michael994664@gmail.com>
2022-03-19 12:03:57 -0700

    Fix proxy

--- packages/backend-api/client/vite.config.ts
+++ packages/backend-api/client/vite.config.ts
@@ -1,24 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, ProxyOptions } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { resolve } from 'path'
+import { resolve } from 'path'
 
-// const VITE_ENV = process.env.ENV
+const VITE_ENV = process.env.ENV
+
+const proxyOptionsByEnv: Record<string, Record<string, ProxyOptions>> = {
+  'development': {
+    '/api': {
+      target: 'http://localhost:8000',
+      changeOrigin: true,
+    }
+  },
+  'production': {
+    '/api': {
+      target: 'https://cp.monitorss.xyz',
+      changeOrigin: true,
+    }
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // publicDir: './public',
-  // resolve: {
-  //   alias: {
-  //     '@': resolve(__dirname, 'src'),
-  //   }
-  // },
-  // server: {
-  //   proxy: VITE_ENV === 'development-mockapi' ? {} : {
-  //     '/api': {
-  //       target: 'http://localhost:8000',
-  //       changeOrigin: true,
-  //     }
-  //   }
-  // }
+  publicDir: './public',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    }
+  },
+  server: {
+    proxy: proxyOptionsByEnv[VITE_ENV] || {}
+  }
 })
