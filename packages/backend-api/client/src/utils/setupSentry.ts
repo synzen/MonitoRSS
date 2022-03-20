aa7a0dc168a647e53de9ee60fd941ebd067f7c93
synzeno <michael994664@gmail.com>
2022-03-20 17:53:49 -0400

    Add trace sample rate to sentry

--- packages/backend-api/client/src/utils/setupSentry.ts
+++ packages/backend-api/client/src/utils/setupSentry.ts
@@ -3,10 +3,10 @@ import { BrowserTracing } from '@sentry/tracing';
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN as string,
-  // integrations: [new BrowserTracing()],
-  debug: true,
+  integrations: [new BrowserTracing()],
+  // debug: true,
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  // tracesSampleRate: 0.1,
+  tracesSampleRate: 0.1,
 });
