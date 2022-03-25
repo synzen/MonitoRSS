5c4f74714413dd0aede8cb5652e76195298eac4d
synzeno <michael994664@gmail.com>
2022-03-24 21:00:29 -0400

    Add feed limit display on feeds page

--- packages/backend-api/client/src/pages/Feeds.tsx
+++ packages/backend-api/client/src/pages/Feeds.tsx
@@ -8,20 +8,26 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
+  Text,
+  IconButton,
 } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ArrowLeftIcon } from '@chakra-ui/icons';
 import RouteParams from '../types/RouteParams';
-import { RequireServerBotAccess } from '@/features/discordServers';
+import { RequireServerBotAccess, useDiscordServer } from '@/features/discordServers';
 import { FeedSidebar } from '@/features/feed/components/FeedSidebar';
 import { FeedsTable } from '@/features/feed/components/FeedsTable';
+import { useFeeds } from '@/features/feed';
 
 const Feeds: React.FC = () => {
   const { serverId } = useParams<RouteParams>();
   const sidebarEnabled = useBreakpointValue<boolean>({ base: true, '2xl': false });
   const [focusedFeedId, setFocusedFeedId] = useState('');
   const { t } = useTranslation();
+  const { data: serverData } = useDiscordServer({ serverId });
+  const { data: feedsData } = useFeeds({ serverId });
 
   useEffect(() => {
     setFocusedFeedId('');
@@ -30,6 +36,10 @@ const Feeds: React.FC = () => {
   const onFeedDeleted = () => {
     setFocusedFeedId('');
   };
+
+  const currentFeedCount = feedsData?.total || 0;
+  const maxFeedsCount = serverData?.benefits.maxFeeds || 0;
+  const feedCountIsAccessible = feedsData && serverData;
 
   return (
     <RequireServerBotAccess
@@ -47,7 +57,36 @@ const Feeds: React.FC = () => {
           paddingBottom="12"
           width="100%"
         >
-          <Heading size="lg" paddingTop="8">{t('pages.feeds.title')}</Heading>
+          <Flex
+            paddingTop="8"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Heading size="lg">{t('pages.feeds.title')}</Heading>
+            {feedCountIsAccessible && (
+            <Flex alignItems="center">
+              <Text fontSize="xl" fontWeight={600}>
+                {currentFeedCount}
+                {' '}
+                /
+                {' '}
+                {maxFeedsCount}
+              </Text>
+              <IconButton
+                as="a"
+                href="https://www.patreon.com/monitorss"
+                target="_blank"
+                rel="noreferrer noopener"
+                marginLeft="4"
+                aria-label="Increase feed limit"
+                variant="outline"
+                icon={<ArrowLeftIcon />}
+                size="sm"
+                transform="rotate(90deg)"
+              />
+            </Flex>
+            )}
+          </Flex>
           <FeedsTable
             onSelectedFeedId={setFocusedFeedId}
             selectedFeedId={focusedFeedId}
