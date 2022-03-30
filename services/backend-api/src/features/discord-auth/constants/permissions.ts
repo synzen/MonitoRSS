8fc836ef72c47c094d908e3aedfa9df7f390db0f
gintil <michael994664@gmail.com>
2022-03-30 16:49:07 -0400

    Fix wrong permission constants

--- services/backend-api/src/features/discord-auth/constants/permissions.ts
+++ services/backend-api/src/features/discord-auth/constants/permissions.ts
@@ -1,5 +1,5 @@
-export const MANAGE_CHANNEL = BigInt(0x16);
-export const SEND_CHANNEL_MESSAGE = BigInt(0x8);
+export const MANAGE_CHANNEL = BigInt(0x10);
+export const SEND_CHANNEL_MESSAGE = BigInt(0x800);
 export const NONE = BigInt(0x0);
 export const ADMINISTRATOR = BigInt(0x8);
 export const VIEW_CHANNEL = BigInt(0x400);
