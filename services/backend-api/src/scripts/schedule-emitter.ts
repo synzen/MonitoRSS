0e0645f26b091521f5a96c968ee588ee7a57dddb
gintil <michael994664@gmail.com>
2022-04-30 18:47:50 -0400

    Add error logs for schedule event emitter/handler

--- services/backend-api/src/scripts/schedule-emitter.ts
+++ services/backend-api/src/scripts/schedule-emitter.ts
@@ -6,26 +6,32 @@ import logger from '../utils/logger';
 bootstrap();
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule.forRoot());
-  await app.init();
+  try {
+    const app = await NestFactory.createApplicationContext(AppModule.forRoot());
+    await app.init();
 
-  const scheduleEmitterService = app.get(ScheduleEmitterService);
+    const scheduleEmitterService = app.get(ScheduleEmitterService);
 
-  setInterval(async () => {
-    try {
-      logger.info(`Syncing timer states`);
-      await scheduleEmitterService.syncTimerStates(
-        async (refreshRateSeconds) => {
-          logger.info(`Refreshing at ${refreshRateSeconds}s`);
-          await scheduleEmitterService.emitScheduleEvent({
-            refreshRateSeconds,
-          });
-        },
-      );
-    } catch (err) {
-      logger.error(`Failed to sync timer states`, {
-        stack: err.stack,
-      });
-    }
-  }, 1000 * 60);
+    setInterval(async () => {
+      try {
+        logger.info(`Syncing timer states`);
+        await scheduleEmitterService.syncTimerStates(
+          async (refreshRateSeconds) => {
+            logger.info(`Refreshing at ${refreshRateSeconds}s`);
+            await scheduleEmitterService.emitScheduleEvent({
+              refreshRateSeconds,
+            });
+          },
+        );
+      } catch (err) {
+        logger.error(`Failed to sync timer states`, {
+          stack: err.stack,
+        });
+      }
+    }, 1000 * 60);
+  } catch (err) {
+    logger.error(`Failed to initialize schedule emitter`, {
+      stack: err.stack,
+    });
+  }
 }
--- services/backend-api/src/scripts/schedule-handler.ts
+++ services/backend-api/src/scripts/schedule-handler.ts
@@ -2,20 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app.module';
 
 import { ScheduleHandlerService } from '../features/schedule-handler/schedule-handler.service';
+import logger from '../utils/logger';
 
 bootstrap();
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule.forRoot());
-  const scheduleHandlerService = app.get(ScheduleHandlerService);
-  await app.init();
+  try {
+    const app = await NestFactory.createApplicationContext(AppModule.forRoot());
+    const scheduleHandlerService = app.get(ScheduleHandlerService);
+    await app.init();
 
-  await scheduleHandlerService.pollForScheduleEvents(
-    async ({ refreshRateSeconds }) => {
-      const urls = await scheduleHandlerService.getUrlsMatchingRefreshRate(
-        refreshRateSeconds,
-      );
-      console.log(urls);
-    },
-  );
+    await scheduleHandlerService.pollForScheduleEvents(
+      async ({ refreshRateSeconds }) => {
+        const urls = await scheduleHandlerService.getUrlsMatchingRefreshRate(
+          refreshRateSeconds,
+        );
+        console.log(urls);
+      },
+    );
+  } catch (err) {
+    logger.error(`Failed to initialize schedule handler`, {
+      stack: err.stack,
+    });
+  }
 }
