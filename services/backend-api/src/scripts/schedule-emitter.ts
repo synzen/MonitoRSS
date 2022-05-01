6c7bbc4f862ecfb6e50e37a20d3af93dc6c6551d
gintil <michael994664@gmail.com>
2022-05-01 10:07:30 -0400

    Add emitter service to docker-compose

--- services/backend-api/src/scripts/schedule-emitter.ts
+++ services/backend-api/src/scripts/schedule-emitter.ts
@@ -1,3 +1,4 @@
+import { INestApplicationContext } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app.module';
 import { ScheduleEmitterService } from '../features/schedule-emitter/schedule-emitter.service';
@@ -7,30 +8,45 @@ bootstrap();
 
 async function bootstrap() {
   try {
+    logger.info('Starting schedule emitter service...');
     const app = await NestFactory.createApplicationContext(AppModule.forRoot());
     await app.init();
 
-    const scheduleEmitterService = app.get(ScheduleEmitterService);
+    setInterval(() => {
+      runTimerSync(app)
+    }, 1000 * 60);
 
-    setInterval(async () => {
-      try {
-        logger.info(`Syncing timer states`);
-        await scheduleEmitterService.syncTimerStates(
-          async (refreshRateSeconds) => {
-            logger.info(`Refreshing at ${refreshRateSeconds}s`);
-            await scheduleEmitterService.emitScheduleEvent({
-              refreshRateSeconds,
-            });
-          },
-        );
+    await runTimerSync(app)
+
+    logger.info('Initiailized schedule emitter service');
+  } catch (err) {
+    logger.error(`Failed to initialize schedule emitter`, {
+      stack: err.stack,
+    });
+  }
+}
+
+async function runTimerSync(app: INestApplicationContext) {
+  const scheduleEmitterService = app.get(ScheduleEmitterService);
+
+  try {
+    logger.info(`Syncing timer states`);
+    await scheduleEmitterService.syncTimerStates(
+      async (refreshRateSeconds) => {
+        try {
+          await scheduleEmitterService.emitScheduleEvent({
+            refreshRateSeconds,
+          });
+          logger.info(`Refreshed at ${refreshRateSeconds}s`);
       } catch (err) {
-        logger.error(`Failed to sync timer states`, {
+        logger.error(`Failed to emit schedule event`, {
           stack: err.stack,
         });
       }
-    }, 1000 * 60);
+      },
+    );
   } catch (err) {
-    logger.error(`Failed to initialize schedule emitter`, {
+    logger.error(`Failed to sync timer states`, {
       stack: err.stack,
     });
   }
