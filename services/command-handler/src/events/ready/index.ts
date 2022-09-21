f59de82efbda295f88e12a9639f6a290410d1f35
gintil <michael994664@gmail.com>
2022-09-20 21:28:33 -0400

    Delete command handler service

--- services/command-handler/src/events/ready/index.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from 'discord.js';
-import Logger from '../../utils/logger';
-
-export default async function readyEvent(
-  client: Client,
-) {
-  new Logger().info(`Client shard ${client.shard?.ids[0]} ready`);
-}
--- services/command-handler/src/interaction-handlers/commands/command.interface.ts
+++
@@ -1,7 +0,0 @@
-import { CommandInteraction } from 'discord.js';
-
-interface CommandInterface {
-  execute(interaction: CommandInteraction): Promise<void>
-}
-
-export default CommandInterface;
