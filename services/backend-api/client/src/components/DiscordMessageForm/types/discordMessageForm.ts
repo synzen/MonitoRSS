1fd438ca29df07771067798c859fda4358645ecb
gintil <michael994664@gmail.com>
2022-10-02 10:18:27 -0400

    Update yup to fix typescript errors with "when"

--- services/backend-api/client/src/components/DiscordMessageForm/types/discordMessageForm.ts
+++ services/backend-api/client/src/components/DiscordMessageForm/types/discordMessageForm.ts
@@ -3,20 +3,47 @@ import {
   InferType, number, object, string,
 } from 'yup';
 
-export const discordMessageEmbedFormSchema = object({
+export const discordMessageEmbedFormSchema = object().shape({
   embedColor: number().positive().max(16777215)
     .typeError('Must be a positive number less than 16777216'),
   embedAuthorTitle: string().max(256),
-  embedAuthorUrl: string(),
-  embedAuthorIconUrl: string(),
+  embedAuthorUrl: string().when('embedAuthorTitle', ([embedAuthorTitle], schema) => {
+    console.log('EMBEF AUTHOR URL', embedAuthorTitle);
+
+    if (!embedAuthorTitle) {
+      return schema.oneOf([''], 'Must be empty if there is no author title');
+    }
+
+    return schema;
+  }),
+  embedAuthorIconUrl: string().when('embedAuthorTitle', ([embedAuthorTitle], schema) => {
+    if (!embedAuthorTitle) {
+      return schema.oneOf([''], 'Must be empty if there is no author title');
+    }
+
+    return schema;
+  }),
   embedTitle: string().max(256),
-  embedUrl: string(),
+  embedUrl: string().when('embedTitle', ([embedTitle], schema) => {
+    if (!embedTitle) {
+      return schema.oneOf([''], 'Must be empty if there is no title');
+    }
+
+    return schema;
+  }),
   embedDescription: string().max(4096),
   embedThumbnailUrl: string(),
   embedImageUrl: string(),
   embedFooterText: string().max(2048),
-  embedFooterIconUrl: string(),
+  embedFooterIconUrl: string().when('embedFooterText', ([embedFooterText], schema) => {
+    if (!embedFooterText) {
+      return schema.oneOf([''], 'Must be empty if there is no footer text');
+    }
+
+    return schema;
+  }),
 });
+
 export const discordMessageFormSchema = object({
   content: string().max(2000),
   embeds: array().of(discordMessageEmbedFormSchema),
