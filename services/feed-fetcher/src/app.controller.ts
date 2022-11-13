74892b97b0c2d3733541866d3b2ff971d7dfbb14
gintil <michael994664@gmail.com>
2022-11-13 11:07:21 -0500

    Add health endpoint

--- services/feed-fetcher/src/app.controller.ts
+++ services/feed-fetcher/src/app.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  version: '1',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('health')
+  getHealth() {
+    return {
+      ok: 1,
+    };
+  }
 }
