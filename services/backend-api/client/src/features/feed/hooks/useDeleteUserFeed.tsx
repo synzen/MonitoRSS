cc261e88399b9f85ae104b3e2bdc36eadf3165da
gintil <michael994664@gmail.com>
2022-11-19 18:10:52 -0500

    Fix user feeds not updating after deleting one

--- services/backend-api/client/src/features/feed/hooks/useDeleteUserFeed.tsx
+++ services/backend-api/client/src/features/feed/hooks/useDeleteUserFeed.tsx
@@ -1,15 +1,27 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import ApiAdapterError from '@/utils/ApiAdapterError';
 import { deleteUserFeed, DeleteUserFeedInput } from '../api';
 
 export const useDeleteUserFeed = () => {
+  const queryClient = useQueryClient();
+
   const {
     mutateAsync,
     status,
     error,
-  } = useMutation<void, ApiAdapterError, DeleteUserFeedInput>(
-    (details) => deleteUserFeed(details),
-  );
+  } = useMutation<
+  void,
+  ApiAdapterError,
+  DeleteUserFeedInput
+  >((details) => deleteUserFeed(details), {
+    onSuccess: () => queryClient.invalidateQueries({
+      queryKey: ['user-feeds'],
+      refetchType: 'all',
+      exact: false,
+    }, {
+      throwOnError: true,
+    }),
+  });
 
   return {
     mutateAsync,
