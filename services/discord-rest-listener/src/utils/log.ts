f190df559c85a2f8a0479cb60457c612c955b6f6
gintil <michael994664@gmail.com>
2022-12-24 18:52:42 -0500

    Disable file logs for discord-rest-listener

--- services/discord-rest-listener/src/utils/log.ts
+++ services/discord-rest-listener/src/utils/log.ts
@@ -1,37 +1,7 @@
 import winston from 'winston'
-import isProductionEnv from './isProductionEnv'
 import config from './config'
 
 const timestampFormat = winston.format.timestamp()
-
-const productionLog = winston.createLogger({
-  transports: [
-    new winston.transports.File({
-      filename: 'logs/combined.log',
-      level: 'info',
-      format: winston.format.combine(
-        timestampFormat,
-        winston.format.json()
-      )
-    }),
-    new winston.transports.File({
-      filename: 'logs/error.log',
-      level: 'error',
-      format: winston.format.combine(
-        timestampFormat,
-        winston.format.json()
-      )
-    }),
-    // new winston.transports.Console({
-    //   level: 'info',
-    //   format: winston.format.combine(
-    //     winston.format.colorize(),
-    //     timestampFormat,
-    //     winston.format.simple(),
-    //   )
-    // }),
-  ]
-})
 
 const devLog = winston.createLogger({
   transports: [
@@ -71,6 +41,6 @@ export const logDatadog = (level: string, message: string, meta?: any) => {
   }
 }
 
-const log = isProductionEnv ? productionLog : devLog
+const log = devLog
 
 export default log
