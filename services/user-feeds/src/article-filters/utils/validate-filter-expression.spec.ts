ccf1da754382aa838178d9e5f3e008f2974b6aad
gintil <michael994664@gmail.com>
2022-12-27 20:35:19 -0500

    Add filter validation functions

---
+++ services/user-feeds/src/article-filters/utils/validate-filter-expression.spec.ts
@@ -0,0 +1,531 @@
+import {
+  ExpressionType,
+  LogicalExpression,
+  LogicalExpressionOperator,
+  RelationalExpressionLeft,
+  RelationalExpressionOperator,
+  RelationalExpressionRight,
+} from "../types";
+import {
+  validateLogicalExpression,
+  validateRelationalExpression,
+  validateRelationalLeft,
+  validateRelationalRight,
+} from "./validate-filter-expression";
+
+describe("validateRelationalLeft", () => {
+  it("should return an error if left is not an object", () => {
+    const left = 123;
+
+    const errors = validateRelationalLeft(left as never);
+
+    expect(errors).toEqual(["Expected root.left to be an object but got 123"]);
+  });
+
+  it("should return an error if type is not article", () => {
+    const left = {
+      type: "notArticle",
+      value: "value",
+    };
+
+    const errors = validateRelationalLeft(left);
+
+    expect(errors).toEqual([
+      `Expected root.type to be one of ARTICLE but got notArticle`,
+    ]);
+  });
+
+  describe("ARTICLE type", () => {
+    it("should return an error if value is not a string", () => {
+      const left = {
+        type: RelationalExpressionLeft.Article,
+        value: 123,
+      };
+
+      const errors = validateRelationalLeft(left);
+
+      expect(errors).toEqual([
+        "Expected root.value to be a string but got 123",
+      ]);
+    });
+
+    it("should return an empty array if type is ARTICLE and value is a string", () => {
+      const left = {
+        type: RelationalExpressionLeft.Article,
+        value: "value",
+      };
+
+      const errors = validateRelationalLeft(left);
+
+      expect(errors).toEqual([]);
+    });
+  });
+});
+
+describe("validateRelationalRight", () => {
+  it("should return an error if right is not an object", () => {
+    const right = 123;
+
+    const errors = validateRelationalRight(right as never);
+
+    expect(errors).toEqual(["Expected root.right to be an object but got 123"]);
+  });
+
+  it("should return an error if type is not String or RegExp", () => {
+    const right = {
+      type: "notStringOrRegExp",
+      value: "value",
+    };
+
+    const errors = validateRelationalRight(right);
+
+    expect(errors).toEqual([
+      "Expected root.type to be one of STRING,REGEXP but got notStringOrRegExp",
+    ]);
+  });
+
+  describe("STRING type", () => {
+    it("should return an error if value is not a string", () => {
+      const right = {
+        type: RelationalExpressionRight.String,
+        value: 123,
+      };
+
+      const errors = validateRelationalRight(right);
+
+      expect(errors).toEqual([
+        "Expected root.value to be a string but got 123",
+      ]);
+    });
+
+    it("should return an empty array if type is String and value is a string", () => {
+      const right = {
+        type: RelationalExpressionRight.String,
+        value: "value",
+      };
+
+      const errors = validateRelationalRight(right);
+
+      expect(errors).toEqual([]);
+    });
+  });
+
+  describe("REGEXP type", () => {
+    it("should return an error if value is not a string", () => {
+      const right = {
+        type: RelationalExpressionRight.RegExp,
+        value: 123,
+      };
+
+      const errors = validateRelationalRight(right);
+
+      expect(errors).toEqual([
+        "Expected root.value to be a string but got 123",
+      ]);
+    });
+
+    it("should return an empty array if type is RegExp and value is a string", () => {
+      const right = {
+        type: RelationalExpressionRight.RegExp,
+        value: "value",
+      };
+
+      const errors = validateRelationalRight(right);
+
+      expect(errors).toEqual([]);
+    });
+  });
+});
+
+describe("validateRelationalExpression", () => {
+  it("should return an error if input is not an object", () => {
+    const expression = 123;
+
+    const errors = validateRelationalExpression(expression as never);
+
+    expect(errors).toEqual(["Expected root. to be an object but got 123"]);
+  });
+
+  it("should return an error if type is not Relational", () => {
+    const expression = {
+      type: "notRelational",
+      left: {
+        type: RelationalExpressionLeft.Article,
+        value: "value",
+      },
+      op: "==",
+      right: {
+        type: RelationalExpressionRight.String,
+        value: "value",
+      },
+    };
+
+    const errors = validateRelationalExpression(expression);
+
+    expect(errors).toEqual([
+      "Expected root.type to be RELATIONAL but got notRelational",
+    ]);
+  });
+
+  it("should return an error if left is not an object", () => {
+    const expression = {
+      type: ExpressionType.Relational,
+      left: 123,
+      op: RelationalExpressionOperator.Eq,
+      right: {
+        type: RelationalExpressionRight.String,
+        value: "value",
+      },
+    };
+
+    const errors = validateRelationalExpression(expression);
+
+    expect(errors).toEqual(["Expected root.left to be an object but got 123"]);
+  });
+
+  it("should return an error if there is no left", () => {
+    const expression = {
+      type: ExpressionType.Relational,
+      op: RelationalExpressionOperator.Eq,
+      right: {
+        type: RelationalExpressionRight.String,
+        value: "value",
+      },
+    };
+
+    const errors = validateRelationalExpression(expression);
+
+    expect(errors).toEqual([
+      "Expected root.left to be an object but got undefined",
+    ]);
+  });
+
+  it("should return an error if op is not a valid operator", () => {
+    const expression = {
+      type: ExpressionType.Relational,
+      left: {
+        type: RelationalExpressionLeft.Article,
+        value: "value",
+      },
+      op: "notAnOperator",
+      right: {
+        type: RelationalExpressionRight.String,
+        value: "value",
+      },
+    };
+
+    const errors = validateRelationalExpression(expression);
+
+    expect(errors).toEqual([
+      `Expected root.op to be one of ${Object.values(
+        RelationalExpressionOperator
+      )} but got notAnOperator`,
+    ]);
+  });
+
+  it("should return an error if right is invalid", () => {
+    const expression = {
+      type: ExpressionType.Relational,
+      left: {
+        type: RelationalExpressionLeft.Article,
+        value: "value",
+      },
+      op: RelationalExpressionOperator.Eq,
+      right: 123,
+    };
+
+    const errors = validateRelationalExpression(expression);
+
+    expect(errors).toEqual(["Expected root.right to be an object but got 123"]);
+  });
+
+  it("should return an error if there is no right", () => {
+    const expression = {
+      type: ExpressionType.Relational,
+      left: {
+        type: RelationalExpressionLeft.Article,
+        value: "value",
+      },
+      op: RelationalExpressionOperator.Eq,
+    };
+
+    const errors = validateRelationalExpression(expression);
+
+    expect(errors).toEqual([
+      "Expected root.right to be an object but got undefined",
+    ]);
+  });
+
+  it("should return an empty array if expression is valid", () => {
+    const expression = {
+      type: ExpressionType.Relational,
+      left: {
+        type: RelationalExpressionLeft.Article,
+        value: "value",
+      },
+      op: RelationalExpressionOperator.Eq,
+      right: {
+        type: RelationalExpressionRight.String,
+        value: "value",
+      },
+    };
+
+    const errors = validateRelationalExpression(expression);
+
+    expect(errors).toEqual([]);
+  });
+});
+
+describe("validateLogicalExpression", () => {
+  it("should return an error if type is not Logical", () => {
+    const expression = {
+      type: "notLogical",
+      op: LogicalExpressionOperator.And,
+      children: [
+        {
+          type: ExpressionType.Relational,
+          left: {
+            type: RelationalExpressionLeft.Article,
+            value: "value",
+          },
+          op: RelationalExpressionOperator.Eq,
+          right: {
+            type: RelationalExpressionRight.String,
+            value: "value",
+          },
+        },
+      ],
+    };
+
+    const errors = validateLogicalExpression(expression);
+
+    expect(errors).toEqual([
+      "Expected root.type to be LOGICAL but got notLogical",
+    ]);
+  });
+
+  it("should return an error if op is not a valid operator", () => {
+    const expression = {
+      type: ExpressionType.Logical,
+      op: "notAnOperator",
+      children: [
+        {
+          type: ExpressionType.Relational,
+          left: {
+            type: RelationalExpressionLeft.Article,
+            value: "value",
+          },
+          op: RelationalExpressionOperator.Eq,
+          right: {
+            type: RelationalExpressionRight.String,
+            value: "value",
+          },
+        },
+      ],
+    };
+
+    const errors = validateLogicalExpression(expression);
+
+    expect(errors).toEqual([
+      `Expected root.op to be one of ${Object.values(
+        LogicalExpressionOperator
+      )} but got notAnOperator`,
+    ]);
+  });
+
+  it("should return an error if children is not an array", () => {
+    const expression = {
+      type: ExpressionType.Logical,
+      op: LogicalExpressionOperator.And,
+      children: 123,
+    };
+
+    const errors = validateLogicalExpression(expression);
+
+    expect(errors).toEqual([
+      "Expected root.children to be an array but got 123",
+    ]);
+  });
+
+  it("should return an error if there is no children", () => {
+    const expression = {
+      type: ExpressionType.Logical,
+      op: LogicalExpressionOperator.And,
+    };
+
+    const errors = validateLogicalExpression(expression);
+
+    expect(errors).toEqual([
+      "Expected root.children to be an array but got undefined",
+    ]);
+  });
+
+  it("should return an error if children is empty", () => {
+    const expression = {
+      type: ExpressionType.Logical,
+      op: LogicalExpressionOperator.And,
+      children: [],
+    };
+
+    const errors = validateLogicalExpression(expression);
+
+    expect(errors).toEqual([
+      "Expected root.children to have at least one item",
+    ]);
+  });
+
+  it("should return an error if children is not an array of valid expressions", () => {
+    const expression = {
+      type: ExpressionType.Logical,
+      op: LogicalExpressionOperator.And,
+      children: [
+        {
+          type: "notLogical",
+          op: LogicalExpressionOperator.And,
+          children: [
+            {
+              type: ExpressionType.Relational,
+              left: {
+                type: RelationalExpressionLeft.Article,
+                value: "value",
+              },
+              op: RelationalExpressionOperator.Eq,
+              right: {
+                type: RelationalExpressionRight.String,
+                value: "value",
+              },
+            },
+          ],
+        },
+      ],
+    };
+
+    const errors = validateLogicalExpression(expression);
+
+    expect(errors).toEqual([
+      "Expected root.children[0].type to be one of RELATIONAL,LOGICAL but got notLogical",
+    ]);
+  });
+
+  it("should return an error if input record is not a object", () => {
+    const errors = validateLogicalExpression(null as never);
+
+    expect(errors).toEqual(["Expected root. to be an object but got null"]);
+  });
+
+  it("should return an empty array if expression is valid", () => {
+    const expression = {
+      type: ExpressionType.Logical,
+      op: LogicalExpressionOperator.And,
+      children: [
+        {
+          type: ExpressionType.Relational,
+          left: {
+            type: RelationalExpressionLeft.Article,
+            value: "value",
+          },
+          op: RelationalExpressionOperator.Eq,
+          right: {
+            type: RelationalExpressionRight.String,
+            value: "value",
+          },
+        },
+      ],
+    };
+
+    const errors = validateLogicalExpression(expression);
+
+    expect(errors).toEqual([]);
+  });
+
+  it("should return a flat array of errors if there are nested errors", () => {
+    const expression = {
+      type: ExpressionType.Logical,
+      op: LogicalExpressionOperator.And,
+      children: [
+        {
+          type: "TEST",
+          op: LogicalExpressionOperator.And,
+          children: [
+            {
+              type: ExpressionType.Relational,
+              left: {
+                type: RelationalExpressionLeft.Article,
+                value: "value",
+              },
+              op: RelationalExpressionOperator.Eq,
+              right: {
+                type: RelationalExpressionRight.String,
+                value: "value",
+              },
+            },
+          ],
+        },
+        {
+          type: "TEST",
+          op: LogicalExpressionOperator.And,
+          children: [
+            {
+              type: ExpressionType.Relational,
+              left: {
+                type: RelationalExpressionLeft.Article,
+                value: "value",
+              },
+              op: RelationalExpressionOperator.Eq,
+              right: {
+                type: RelationalExpressionRight.String,
+                value: "value",
+              },
+            },
+          ],
+        },
+      ],
+    };
+
+    const errors = validateLogicalExpression(expression);
+
+    expect(errors).toEqual([
+      "Expected root.children[0].type to be one of RELATIONAL,LOGICAL but got TEST",
+      "Expected root.children[1].type to be one of RELATIONAL,LOGICAL but got TEST",
+    ]);
+  });
+
+  it("returns an error if the depth is greater than 10", () => {
+    const baseExpression: LogicalExpression = {
+      type: ExpressionType.Logical,
+      op: LogicalExpressionOperator.And,
+      children: [],
+    };
+
+    let latestExpression = baseExpression;
+    let currentDepth = 0;
+
+    while (currentDepth < 10) {
+      latestExpression.children.push({
+        type: ExpressionType.Logical,
+        op: LogicalExpressionOperator.And,
+        children: [],
+      });
+
+      latestExpression = latestExpression.children[0] as LogicalExpression;
+      currentDepth++;
+    }
+
+    latestExpression.children.push({
+      type: ExpressionType.Relational,
+      left: {
+        type: RelationalExpressionLeft.Article,
+        value: "value",
+      },
+      op: RelationalExpressionOperator.Eq,
+      right: {
+        type: RelationalExpressionRight.String,
+        value: "value",
+      },
+    });
+
+    const errors = validateLogicalExpression(baseExpression);
+
+    expect(errors).toEqual([`Depth of logical expression is too deep.`]);
+  });
+});
---
+++ services/user-feeds/src/article-filters/utils/validate-filter-expression.ts
@@ -0,0 +1,174 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  ExpressionType,
+  LogicalExpressionOperator,
+  RelationalExpressionLeft,
+  RelationalExpressionOperator,
+  RelationalExpressionRight,
+} from "../types";
+
+export function validateRelationalLeft(
+  left: Record<string, any>,
+  currentPath = "root."
+): string[] {
+  if (!left || left.constructor !== {}.constructor) {
+    return [`Expected ${currentPath}left to be an object but got ${left}`];
+  }
+
+  if (left.type === RelationalExpressionLeft.Article) {
+    if (typeof left.value !== "string") {
+      return [
+        `Expected ${currentPath}value to be a string but got ${left.value}`,
+      ];
+    }
+  } else {
+    return [
+      `Expected ${currentPath}type to be one of ${Object.values(
+        RelationalExpressionLeft
+      ).join(",")} but got ${left.type}`,
+    ];
+  }
+
+  return [];
+}
+
+export function validateRelationalRight(
+  right: Record<string, any>,
+  currentPath = "root."
+): string[] {
+  if (!right || right.constructor !== {}.constructor) {
+    return [`Expected ${currentPath}right to be an object but got ${right}`];
+  }
+
+  if (right.type === RelationalExpressionRight.String) {
+    if (typeof right.value !== "string") {
+      return [
+        `Expected ${currentPath}value to be a string but got ${right.value}`,
+      ];
+    }
+  } else if (right.type === RelationalExpressionRight.RegExp) {
+    if (typeof right.value !== "string") {
+      return [
+        `Expected ${currentPath}value to be a string but got ${right.value}`,
+      ];
+    }
+  } else {
+    return [
+      `Expected ${currentPath}type to be one of ${Object.values(
+        RelationalExpressionRight
+      )} but got ${right.type}`,
+    ];
+  }
+
+  return [];
+}
+
+export function validateRelationalExpression(
+  data: Record<string, any>,
+  currentPath = "root."
+): string[] {
+  if (!data || data.constructor !== {}.constructor) {
+    return [`Expected ${currentPath} to be an object but got ${data}`];
+  }
+
+  const { type, op, left, right } = data;
+
+  if (type !== ExpressionType.Relational) {
+    return [
+      `Expected ${currentPath}type to be ${ExpressionType.Relational} but got ${type}`,
+    ];
+  }
+
+  if (
+    !Object.values(RelationalExpressionOperator).includes(
+      op as RelationalExpressionOperator
+    )
+  ) {
+    return [
+      `Expected ${currentPath}op to be one of ${Object.values(
+        RelationalExpressionOperator
+      )} but got ${op}`,
+    ];
+  }
+
+  if (!left || left.constructor !== {}.constructor) {
+    return [`Expected ${currentPath}left to be an object but got ${left}`];
+  }
+
+  if (!right || right.constructor !== {}.constructor) {
+    return [`Expected ${currentPath}right to be an object but got ${right}`];
+  }
+
+  const leftErrors = validateRelationalLeft(
+    left as Record<string, unknown>,
+    `${currentPath}left.`
+  );
+  const rightErrors = validateRelationalRight(
+    right as Record<string, unknown>,
+    `${currentPath}right.`
+  );
+
+  return [...leftErrors, ...rightErrors];
+}
+
+export function validateLogicalExpression(
+  input: Record<string, any>,
+  currentPath = "root.",
+  depth = 0
+): string[] {
+  if (!input || input.constructor !== {}.constructor) {
+    return [`Expected ${currentPath} to be an object but got ${input}`];
+  }
+
+  const { type, children, op } = input;
+
+  if (depth > 9) {
+    return [`Depth of logical expression is too deep.`];
+  }
+
+  if (type !== ExpressionType.Logical) {
+    return [
+      `Expected ${currentPath}type to be ${ExpressionType.Logical} but got ${type}`,
+    ];
+  }
+
+  if (
+    !Object.values(LogicalExpressionOperator).includes(
+      op as LogicalExpressionOperator
+    )
+  ) {
+    return [
+      `Expected ${currentPath}op to be one of ${Object.values(
+        LogicalExpressionOperator
+      )} but got ${op}`,
+    ];
+  }
+
+  if (!Array.isArray(children)) {
+    return [
+      `Expected ${currentPath}children to be an array but got ${children}`,
+    ];
+  }
+
+  if (children.length === 0) {
+    return [`Expected ${currentPath}children to have at least one item`];
+  }
+
+  const errors = children.flatMap((child, index) => {
+    const childPath = `${currentPath}children[${index}].`;
+
+    if (child.type === ExpressionType.Logical) {
+      return validateLogicalExpression(child, childPath, depth + 1);
+    } else if (child.type === ExpressionType.Relational) {
+      return validateRelationalExpression(child, childPath);
+    } else {
+      return [
+        `Expected ${childPath}type to be one of ${Object.values(
+          ExpressionType
+        )} but got ${child.type}`,
+      ];
+    }
+  });
+
+  return errors;
+}
