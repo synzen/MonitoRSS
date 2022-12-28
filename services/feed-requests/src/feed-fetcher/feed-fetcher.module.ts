d7df21b4ed68d595c81c72f3dfda0285a89b8a00
gintil <michael994664@gmail.com>
2022-12-28 08:47:12 -0500

    Create message broker module in feed fetcher service

--- services/feed-requests/src/feed-fetcher/feed-fetcher.module.ts
+++ services/feed-requests/src/feed-fetcher/feed-fetcher.module.ts
@@ -2,12 +2,8 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { Request, Response } from './entities';
 import { FeedFetcherController } from './feed-fetcher.controller';
 import { FeedFetcherService } from './feed-fetcher.service';
-import {
-  MessageHandlerErrorBehavior,
-  RabbitMQModule,
-} from '@golevelup/nestjs-rabbitmq';
-import config from '../config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { MessageBrokerModule } from '../message-broker/message-broker.module';
 
 @Module({
   controllers: [FeedFetcherController],
@@ -17,18 +13,9 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 })
 export class FeedFetcherModule {
   static forRoot(): DynamicModule {
-    const configValues = config();
-
     return {
       module: FeedFetcherModule,
-      imports: [
-        RabbitMQModule.forRoot(RabbitMQModule, {
-          uri: configValues.FEED_REQUESTS_RABBITMQ_BROKER_URL,
-          defaultExchangeType: 'direct',
-          defaultSubscribeErrorBehavior: MessageHandlerErrorBehavior.NACK,
-        }),
-      ],
-      exports: [RabbitMQModule],
+      imports: [MessageBrokerModule.forRoot()],
     };
   }
 }
