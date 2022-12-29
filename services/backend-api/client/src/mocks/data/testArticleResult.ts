9184af9279f7c01857cbadcf68d6811996a2d27b
gintil <michael994664@gmail.com>
2022-12-28 19:59:44 -0500

    Fix client build

--- services/backend-api/client/src/mocks/data/testArticleResult.ts
+++ services/backend-api/client/src/mocks/data/testArticleResult.ts
@@ -2,4 +2,6 @@ import { SendTestArticleDeliveryStatus, SendTestArticleResult } from '@/types';
 
 export const mockSendTestArticleResult: SendTestArticleResult = {
   status: SendTestArticleDeliveryStatus.Success,
+  apiPayload: {},
+  apiResponse: {},
 };
