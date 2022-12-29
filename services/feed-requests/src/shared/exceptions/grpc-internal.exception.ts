303a88b347ff39c3678b64a98b41b3eaa43f64be
gintil <michael994664@gmail.com>
2022-12-28 21:16:27 -0500

    Fix feed requests service build

--- services/feed-requests/src/shared/exceptions/grpc-internal.exception.ts
+++
@@ -1,9 +0,0 @@
-import { RpcException } from '@nestjs/microservices';
-
-export class GrpcInternalException extends RpcException {
-  constructor() {
-    super({
-      code: 13,
-    });
-  }
-}
--- services/feed-requests/src/shared/exceptions/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './grpc-internal.exception';
-export * from './grpc-invalid-argument.exception';
