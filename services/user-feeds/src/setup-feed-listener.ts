7e3171dbc41d865879e3f63e3f9ff9d8bd1ae93c
gintil <michael994664@gmail.com>
2023-06-27 19:40:14 -0400

    Ping database on user feeds feed listener

--- services/user-feeds/src/setup-feed-listener.ts
+++ services/user-feeds/src/setup-feed-listener.ts
@@ -1,11 +1,38 @@
+import { MikroORM } from "@mikro-orm/core";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import logger from "./shared/utils/logger";
 
 export async function setupFeedListener() {
-  await NestFactory.createApplicationContext(
+  const app = await NestFactory.createApplicationContext(
     AppModule.forFeedListenerService()
   );
 
+  const orm = app.get(MikroORM);
+
+  setInterval(() => {
+    tryDbConnection(orm).catch(() => process.exit(1));
+  }, 60000);
+
   logger.info("Feed handler service initialized");
+}
+
+async function tryDbConnection(orm: MikroORM, currentTries = 0) {
+  if (currentTries >= 10) {
+    logger.error("Failed to connect to database after 10 tries. Exiting...");
+
+    process.exit(1);
+  }
+
+  await orm.em
+    .getDriver()
+    .getConnection()
+    .execute("SELECT 1")
+    .catch((err) => {
+      logger.error("Failed to ping database", {
+        error: (err as Error).stack,
+      });
+
+      return tryDbConnection(orm, currentTries + 1);
+    });
 }
