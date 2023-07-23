e734bdbf5c798b68573256ad29e0abca7dfd4143
gintil <michael994664@gmail.com>
2023-07-23 19:13:12 -0400

    Use new status tags

--- services/backend-api/client/src/features/feed/components/UserFeedsTable/UserFeedStatusTag.tsx
+++ services/backend-api/client/src/features/feed/components/UserFeedsTable/UserFeedStatusTag.tsx
@@ -1,5 +1,7 @@
-import { CheckCircleIcon, NotAllowedIcon, WarningIcon } from "@chakra-ui/icons";
+import { FaCircleExclamation, FaPause } from "react-icons/fa6";
+import { FaCheckCircle } from "react-icons/fa";
 import { UserFeed, UserFeedDisabledCode } from "../../types";
+import getChakraColor from "../../../../utils/getChakraColor";
 
 interface Props {
   disabledCode?: UserFeed["disabledCode"];
@@ -10,12 +12,12 @@ export const UserFeedStatusTag: React.FC<Props> = ({ disabledCode }) => {
     disabledCode === UserFeedDisabledCode.FailedRequests ||
     disabledCode === UserFeedDisabledCode.BadFormat
   ) {
-    return <WarningIcon boxSize={5} color="red.300" />;
+    return <FaCircleExclamation fontSize={18} color={getChakraColor("red.300")} />;
   }
 
   if (disabledCode === UserFeedDisabledCode.Manual) {
-    return <NotAllowedIcon boxSize={5} opacity="0.5" />;
+    return <FaPause opacity="0.5" fontSize={18} />;
   }
 
-  return <CheckCircleIcon boxSize={5} color="green.500" />;
+  return <FaCheckCircle color={getChakraColor("green.500")} fontSize={18} />;
 };
