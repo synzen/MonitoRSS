4f62716ddf534dbfd5173cdd344a22da4c67a1ff
gintil <michael994664@gmail.com>
2023-08-15 19:46:20 -0400

    Fix empty role names throwing error

--- services/backend-api/client/src/features/discordServers/types/DiscordRole.ts
+++ services/backend-api/client/src/features/discordServers/types/DiscordRole.ts
@@ -2,7 +2,7 @@ import { InferType, object, string } from "yup";
 
 export const DiscordRoleSchema = object({
   id: string().required(),
-  name: string().required(),
+  name: string().typeError("Must be a string").strict(true),
   color: string().required(),
 });
 
