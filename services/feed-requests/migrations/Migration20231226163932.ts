55c96dde2761e51f320df46719f3f5ba42f07ac8
gintil <michael994664@gmail.com>
2023-12-26 15:34:53 -0500

    Remove update from migration

--- services/feed-requests/migrations/Migration20231226163932.ts
+++ services/feed-requests/migrations/Migration20231226163932.ts
@@ -4,7 +4,6 @@ export class Migration20231226163932 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('alter table "request" add column "lookup_key" text null;');
-    this.addSql('update "request" set lookup_key = url;');
   }
 
   async down(): Promise<void> {
