b2dd6c08de367470a467e10ca6476ba23419e46c
gintil <michael994664@gmail.com>
2024-02-01 08:04:18 -0500

    fix: allow no messages in preview result schema

--- services/backend-api/client/src/types/CreatePreviewResult.ts
+++ services/backend-api/client/src/types/CreatePreviewResult.ts
@@ -4,7 +4,7 @@ import { DiscordMessageApiPayloadSchema } from "./discord/DiscordApiPayload";
 
 export const CreatePreviewResultSchema = object({
   status: string().oneOf(Object.values(SendTestArticleDeliveryStatus)).required(),
-  messages: array(DiscordMessageApiPayloadSchema).required(),
+  messages: array(DiscordMessageApiPayloadSchema).default([]),
 }).required();
 
 export type CreatePreviewResult = InferType<typeof CreatePreviewResultSchema>;
