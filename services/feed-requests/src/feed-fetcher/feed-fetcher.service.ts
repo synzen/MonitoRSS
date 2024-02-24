afa86a9aa9690383bb62cf7b7855d7527a2866e5
gintil <michael994664@gmail.com>
2024-02-24 10:58:06 -0500

    Add text/html to accept headers

--- services/feed-requests/src/feed-fetcher/feed-fetcher.service.ts
+++ services/feed-requests/src/feed-fetcher/feed-fetcher.service.ts
@@ -382,7 +382,7 @@ export class FeedFetcherService {
       headers: {
         ...options?.headers,
         'user-agent': options?.userAgent || this.defaultUserAgent,
-        accept: 'text/xml,application/xml,application/rss+xml',
+        accept: 'text/html,text/xml,application/xml,application/rss+xml',
         /**
          * Currently required for https://developer.oculus.com/blog/rss/ that returns 400 otherwise
          * Appears to be temporary error given that the page says they're working on fixing it
