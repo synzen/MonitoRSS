a8f7a944660c18116cb7e31accb7fe76fc9e5aff
gintil <michael994664@gmail.com>
2024-04-09 19:19:46 -0400

    Add browser tracing integration

--- services/backend-api/client/src/main.tsx
+++ services/backend-api/client/src/main.tsx
@@ -23,11 +23,13 @@ async function prepare() {
       Sentry.init({
         dsn: DSN,
         integrations: [
+          Sentry.browserTracingIntegration(),
           Sentry.replayIntegration({
             maskAllText: false,
             blockAllMedia: false,
           }),
         ],
+        tracesSampleRate: 0.2,
         // Session Replay
         replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
         replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
