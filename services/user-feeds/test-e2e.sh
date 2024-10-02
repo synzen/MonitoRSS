cd76e55d474c489d38aaef7694128efea0bc29f4
gintil <michael994664@gmail.com>
2024-10-02 17:57:28 -0400

    Add test shell scripts

---
+++ services/user-feeds/test-e2e.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+alias cleanup='docker-compose -f docker-compose.test.yml down --volumes --remove-orphans'
+export TEST_COMMAND="sh -c \"npx mikro-orm migration:up && npm run test:e2e\""
+(docker-compose -f docker-compose.test.yml up --no-log-prefix --build --force-recreate --abort-on-container-exit --exit-code-from tests --attach tests)
+
+cleanup
