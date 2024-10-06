c7b2c2826b143509894de1ac6260279e6c035789
gintil <michael994664@gmail.com>
2024-10-06 11:53:23 -0400

    Use project name in tests

--- services/user-feeds/test.sh
+++ services/user-feeds/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 alias cleanup='docker-compose -f docker-compose.test.yml down --volumes --remove-orphans'
 export TEST_COMMAND="sh -c \"npx mikro-orm migration:up && npm run test\""
-(docker-compose -f docker-compose.test.yml up --no-log-prefix --build --force-recreate --abort-on-container-exit --exit-code-from tests --attach tests)
+(docker-compose -f docker-compose.test.yml -p user-feeds-test up --no-log-prefix --build --force-recreate --abort-on-container-exit --exit-code-from tests --attach tests)
 
 cleanup
