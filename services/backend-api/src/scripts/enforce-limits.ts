78267bb7a61c29d9f78ab2dee68be34df9c1c544
gintil <michael994664@gmail.com>
2024-10-11 19:22:47 -0400

    Exit script after enforcing

--- services/backend-api/src/scripts/enforce-limits.ts
+++ services/backend-api/src/scripts/enforce-limits.ts
@@ -1,26 +1,37 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "../app.module";
-import { ScheduleHandlerService } from "../features/schedule-handler/schedule-handler.service";
 import logger from "../utils/logger";
+import { SupportersService } from "../features/supporters/supporters.service";
+import { UserFeedsService } from "../features/user-feeds/user-feeds.service";
 
 bootstrap();
 
 async function bootstrap() {
   try {
-    logger.info("Enforcing limits");
-    const app = await NestFactory.createApplicationContext(
-      AppModule.forScheduleEmitter()
-    );
+    logger.info("Enforcing limits...");
+    const app = await NestFactory.createApplicationContext(AppModule.forRoot());
     await app.init();
 
-    const scheduleHandlerService = app.get(ScheduleHandlerService);
+    const supportersService = app.get(SupportersService);
+    const userFeedsService = app.get(UserFeedsService);
 
-    await scheduleHandlerService.enforceUserFeedLimits();
+    const benefits = await supportersService.getBenefitsOfAllDiscordUsers();
+
+    await userFeedsService.enforceUserFeedLimits(
+      benefits.map(({ discordUserId, maxUserFeeds, refreshRateSeconds }) => ({
+        discordUserId,
+        maxUserFeeds,
+        refreshRateSeconds,
+      }))
+    );
 
     logger.info("Completed");
+    await app.close();
+    process.exit(0);
   } catch (err) {
-    logger.error(`Error`, {
+    logger.error(`Failed to enforce limits`, {
       stack: err.stack,
     });
+    process.exit(1);
   }
 }
