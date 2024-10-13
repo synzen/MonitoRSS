d413c800660b57ff203c0e265ddffd188f339a14
gintil <michael994664@gmail.com>
2024-10-13 11:09:43 -0400

    Remove primary key

--- services/user-feeds/migrations/Migration20240924220729.ts
+++ services/user-feeds/migrations/Migration20240924220729.ts
@@ -14,7 +14,6 @@ export class Migration20240924220729 extends Migration {
     this.addSql(`CREATE TABLE feed_article_field_partitioned_oldvalues PARTITION OF feed_article_field_partitioned FOR VALUES FROM ('2022-09-24 22:07:29') TO ('2024-10-01T00:00:00.000Z');`);
     this.addSql(`CREATE TABLE feed_article_field_partitioned_y2024m10 PARTITION OF feed_article_field_partitioned FOR VALUES FROM ('2024-10-01T00:00:00.000Z') TO ('2024-11-01T00:00:00.000Z');`);
 
-    this.addSql(`ALTER TABLE feed_article_field_partitioned ADD PRIMARY KEY (id, created_at);`);
     this.addSql(`CREATE INDEX feed_article_field_partitioned_createdat_index ON feed_article_field_partitioned (created_at);`);
     this.addSql('CREATE INDEX feed_article_field_partitioned_feedid_fieldname_fieldvalue_index ON feed_article_field_partitioned (feed_id, field_name, field_hashed_value);');
   }
