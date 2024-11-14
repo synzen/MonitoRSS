f8a6b5a32bbb6a9d5a093576e426996d81cd9d3d
gintil <michael994664@gmail.com>
2024-11-14 07:57:28 -0500

    Fix wrong cipher algorithm

--- services/backend-api/src/utils/decrypt.ts
+++ services/backend-api/src/utils/decrypt.ts
@@ -3,7 +3,7 @@ import { createDecipheriv } from "crypto";
 const decrypt = (input: string, hexKey: string) => {
   const [iv, text, authtag] = input.split(".");
   const key = Buffer.from(hexKey, "hex");
-  const decipher = createDecipheriv("aes-128-gcm", key, Buffer.from(iv, "hex"));
+  const decipher = createDecipheriv("aes-256-gcm", key, Buffer.from(iv, "hex"));
   decipher.setAuthTag(Buffer.from(authtag, "hex"));
   const decrpyted =
     decipher.update(text, "hex", "utf8") + decipher.final("utf8");
--- services/backend-api/src/utils/encrypt.ts
+++ services/backend-api/src/utils/encrypt.ts
@@ -3,7 +3,7 @@ import { createCipheriv, randomBytes } from "crypto";
 const encrypt = (data: string, hexKey: string) => {
   const iv = randomBytes(16);
   const key = Buffer.from(hexKey, "hex");
-  const cipher = createCipheriv("aes-128-gcm", key, iv);
+  const cipher = createCipheriv("aes-256-gcm", key, iv);
   const encrypted = cipher.update(data, "utf8", "hex") + cipher.final("hex");
   const authtag = cipher.getAuthTag().toString("hex");
 
