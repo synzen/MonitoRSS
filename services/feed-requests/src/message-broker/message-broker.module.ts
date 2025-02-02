32120d4648beb8c3483c4e01a618bd7997b90f33
gintil <michael994664@gmail.com>
2025-02-02 17:40:05 -0500

    Use ack instead of nack on error behavior
    Nack creates far too many attempts

--- services/feed-requests/src/message-broker/message-broker.module.ts
+++ services/feed-requests/src/message-broker/message-broker.module.ts
@@ -21,7 +21,7 @@ export class MessageBrokerModule implements OnApplicationShutdown {
         RabbitMQModule.forRoot(RabbitMQModule, {
           uri: configValues.FEED_REQUESTS_RABBITMQ_BROKER_URL,
           defaultExchangeType: 'direct',
-          defaultSubscribeErrorBehavior: MessageHandlerErrorBehavior.NACK,
+          defaultSubscribeErrorBehavior: MessageHandlerErrorBehavior.ACK,
           channels: {
             default: {
               prefetchCount: 10,
