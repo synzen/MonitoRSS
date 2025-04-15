ab6a2261a42379ada9827f07bbd44983a791b496
gintil <michael994664@gmail.com>
2025-04-15 18:59:41 -0400

    Extend dayjs advanced format for date previews

--- services/backend-api/src/common/validations/is-valid-date-locale.ts
+++ services/backend-api/src/common/validations/is-valid-date-locale.ts
@@ -4,10 +4,12 @@ import {
 } from "class-validator";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+import advancedFormat from "dayjs/plugin/advancedFormat";
 import timezone from "dayjs/plugin/timezone";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(advancedFormat);
 
 const SUPPORTED_LOCALES = [
   "af",
