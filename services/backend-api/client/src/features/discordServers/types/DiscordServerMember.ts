bda8b2e315e87f3b1b6cfc165833461b0abb368e
gintil <michael994664@gmail.com>
2026-01-17 13:16:56 -0500

    Remove avatar url

--- services/backend-api/client/src/features/discordServers/types/DiscordServerMember.ts
+++ services/backend-api/client/src/features/discordServers/types/DiscordServerMember.ts
@@ -4,7 +4,6 @@ export const DiscordServerMemberSchema = object({
   id: string().required(),
   username: string().required(),
   displayName: string().required(),
-  avatarUrl: string().nullable(),
 });
 
 export type DiscordServerMember = InferType<typeof DiscordServerMemberSchema>;
--- services/backend-api/src/features/discord-servers/dto/GetServerMemberOutput.dto.ts
+++ services/backend-api/src/features/discord-servers/dto/GetServerMemberOutput.dto.ts
@@ -1,31 +1,20 @@
 import { DiscordGuildMember } from "../../../common";
-import { DISCORD_CDN_BASE_URL } from "../../../constants/discord";
 
 interface MemberResult {
   id: string;
   username: string;
   displayName: string;
-  avatarUrl: string | null;
 }
 
 export class GetServerMemberOutputDto {
   result: MemberResult;
 
   static fromEntity(member: DiscordGuildMember): GetServerMemberOutputDto {
-    let extension = ".png";
-
-    if (member.user.avatar?.startsWith("a_")) {
-      extension = ".gif";
-    }
-
     return {
       result: {
         id: member.user.id,
         username: member.user.username,
         displayName: member.nick || member.user.username,
-        avatarUrl: member.user.avatar
-          ? `${DISCORD_CDN_BASE_URL}/avatars/${member.user.id}/${member.user.avatar}${extension}`
-          : null,
       },
     };
   }
