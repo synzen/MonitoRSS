025365409e381c35fc478d73c693e4177a7df25b
gintil <michael994664@gmail.com>
2026-02-11 17:55:12 -0500

    Add datadog

--- services/bot-presence/src/logger.ts
+++ services/bot-presence/src/logger.ts
@@ -3,6 +3,11 @@ import setupLogger from "@monitorss/logger";
 const logger = setupLogger({
   env: process.env.NODE_ENV as string,
   enableDebugLogs: process.env.LOG_LEVEL === "debug",
+  datadog: {
+    service: process.env.SERVICE_NAME || "bot-presence",
+    apiKey: process.env.BOT_PRESENCE_DATADOG_API_KEY || undefined,
+  },
+  disableConsole: !!process.env.BOT_PRESENCE_DATADOG_API_KEY,
 });
 
 export default logger;
