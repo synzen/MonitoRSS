991a88805522f37e1eccc4c813de2976a5874ca6
gintil <michael994664@gmail.com>
2026-05-27 19:38:40 -0400

    Revert "Build E2E Docker images once and share across CI shards"
    This reverts commit 58c79747a992219b4d6e8ffd83366e87ca6703d1.

--- e2e/e2e-mock.sh
+++ e2e/e2e-mock.sh
@@ -13,19 +13,7 @@ COMPOSE_FILE="$COMPOSE_PROJECT_DIR/docker-compose.e2e.yml"
 COMPOSE_PROJECT_NAME="monitorss-e2e"
 BACKEND_URL="http://127.0.0.1:8100"
 FRONTEND_URL="http://localhost:3100"
-
-BUILD_FLAG="--build"
-PLAYWRIGHT_ARGS=()
-for arg in "$@"; do
-  if [ "$arg" = "--no-build" ]; then
-    BUILD_FLAG=""
-  else
-    PLAYWRIGHT_ARGS+=("$arg")
-  fi
-done
-if [ ${#PLAYWRIGHT_ARGS[@]} -eq 0 ]; then
-  PLAYWRIGHT_ARGS=("--project=e2e-web")
-fi
+PLAYWRIGHT_ARGS="${@:---project=e2e-web}"
 
 cleanup() {
   echo "Tearing down E2E Docker stack..."
@@ -36,8 +24,8 @@ trap cleanup EXIT
 rm -rf test-results/
 
 echo "Starting E2E Docker stack (project: $COMPOSE_PROJECT_NAME)..."
-docker compose -f "$COMPOSE_FILE" -p "$COMPOSE_PROJECT_NAME" up -d $BUILD_FLAG --wait
+docker compose -f "$COMPOSE_FILE" -p "$COMPOSE_PROJECT_NAME" up -d --build --wait
 
 echo "Running E2E tests..."
 E2E_BACKEND_URL="$BACKEND_URL" E2E_BASE_URL="$FRONTEND_URL" \
-  npx playwright test "${PLAYWRIGHT_ARGS[@]}"
+  npx playwright test $PLAYWRIGHT_ARGS
